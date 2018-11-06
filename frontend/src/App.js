8ce7c15571fc7b91a7a5c4edcba6a27698a0f008
Steve Astels <sastels@gmail.com>
2018-11-06 13:49:29 -0500

    prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import Home from './Home';
+import React from "react";
+import Home from "./Home";
 
-const App = () => (
-  <Home/>
-);
+const App = () => <Home />;
 
 export default App;
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,25 +4,25 @@ import { Router, Link } from "@reach/router";
 const Home = () => (
   <div>
     <nav>
-      <Link to="/">Main</Link> <Link to="p2">Page 2</Link>
+      <Link to="/">Home</Link> <Link to="moreInfo">More Information</Link>
     </nav>
     <Router>
       <Main path="/" />
-      <Dashboard path="p2" />
+      <MoreInfo path="moreInfo" />
     </Router>
   </div>
 );
 
 const Main = () => (
   <div>
-    <h2>Main Page</h2>
+    <h2>Report a cybercrime</h2>
   </div>
 );
 
-const Dashboard = () => (
+const MoreInfo = () => (
   <div>
-    <h2>Page 2</h2>
+    <h2>More information</h2>
   </div>
 );
 
-export default Home
+export default Home;
