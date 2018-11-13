157be25952d1a6127aca47049b394d461d255026
Steve Astels <sastels@users.noreply.github.com>
2018-11-13 18:04:19 -0500

    init report screen (#12)
    * init report screen
    
    * add translations
    
    * add prettier config

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,32 +1,29 @@
 import React from "react";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import { Trans } from "@lingui/macro";
+import { css } from "emotion";
+import PhaseBanner from "@govuk-react/phase-banner";
+import { ReportScreen } from "./report_screen";
+
+const root = css`
+  margin: 20pt;
+`;
+
+const screen = css`
+  margin-top: 30pt;
+`;
 
 const Home = () => (
-  <div>
-    <nav>
-      <Link to="/">Home</Link> <Link to="moreInfo">More Information</Link>
-    </nav>
-    <Router>
-      <Main path="/" />
-      <MoreInfo path="moreInfo" />
-    </Router>
-  </div>
-);
+  <div className={root}>
+    <PhaseBanner level="alpha">
+      <Trans>For internal use only</Trans>
+    </PhaseBanner>
 
-const Main = () => (
-  <div>
-    <h2>
-      <Trans>Report a cybercrime</Trans>
-    </h2>
-  </div>
-);
-
-const MoreInfo = () => (
-  <div>
-    <h2>
-      <Trans>More information</Trans>
-    </h2>
+    <div className={screen}>
+      <Router>
+        <ReportScreen path="/" />
+      </Router>
+    </div>
   </div>
 );
 
---
+++ frontend/src/report_screen.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { css } from "react-emotion";
+import { Trans } from "@lingui/macro";
+import TextArea from "@govuk-react/text-area";
+import Button from "@govuk-react/button";
+import Header from "@govuk-react/header";
+
+const submitButton = css`
+  margin-top: 20pt;
+`;
+
+export class ReportScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      whatHappened: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ whatHappened: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("What happened: " + this.state.whatHappened);
+    event.preventDefault();
+  }
+
+  render() {
+    const input = {
+      id: "what happened",
+      onChange: this.handleChange,
+      value: this.state.whatHappened
+    };
+
+    return (
+      <div>
+        <Header level={1}>
+          <Trans>Report a cybercrime</Trans>
+        </Header>
+
+        <form onSubmit={this.handleSubmit}>
+          <TextArea name="group1" input={input}>
+            <Trans>What happened?</Trans>
+          </TextArea>
+          <Button className={submitButton} type="submit">
+            <Trans>Submit</Trans>
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
