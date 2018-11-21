c5bd9e3b043c0e8c34d976b9a36f1900c4d1ac81
Steve Astels <sastels@gmail.com>
2018-11-21 11:16:42 -0500

    add some tests

---
+++ frontend/src/__tests__/LandingPage.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { LandingPage } from '../LandingPage'
+
+describe('ReportScreen', () => {
+  it('renders', () => {
+    mount(<LandingPage />)
+  })
+})
--- frontend/src/__tests__/server.test.js
+++ frontend/src/__tests__/server.test.js
@@ -5,7 +5,7 @@ describe('Server Side Rendering', () => {
   describe('with Accept-Language: fr-CA', () => {
     it('returns a page in French', async () => {
       let response = await request(server)
-        .get('/')
+        .get('/test')
         .set('Accept-Language', 'fr-CA')
 
       let { text } = response
@@ -17,7 +17,7 @@ describe('Server Side Rendering', () => {
   describe('with Accept-Language: en-US', () => {
     it('returns a page in English', async () => {
       let response = await request(server)
-        .get('/')
+        .get('/test')
         .set('Accept-Language', 'en-US')
 
       let { text } = response
