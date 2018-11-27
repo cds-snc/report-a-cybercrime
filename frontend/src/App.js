463102d4c3d780439b3482da174ea54ef6a753d0
Steve Astels <sastels@gmail.com>
2018-11-27 14:53:25 -0500

    add fip banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { css } from 'react-emotion'
+import { GoCSignature } from '@cdssnc/gcui'
 import Home from './Home'
 import { LanguageSwitcher } from './LanguageSwitcher'
 
+const fipBanner = css`
+  background-color: black;
+  margin: auto;
+  padding: 10px;
+  height: 40px;
+`
+const flag = css`
+  float: left;
+  padding: 5px;
+  width: 300px;
+`
+const languageButton = css`
+  display: inline-block;
+  float: right;
+  margin-top: 10px;
+`
+
 const App = () => (
   <div>
-    <LanguageSwitcher />
+    <div className={fipBanner}>
+      <div className={flag}>
+        <GoCSignature width="100%" lang={'en'} flag="#fff" text="#fff" />
+      </div>
+      <div className={languageButton}>
+        <LanguageSwitcher />
+      </div>
+    </div>
     <Home />
   </div>
 )
