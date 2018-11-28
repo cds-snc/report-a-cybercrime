485eaddcd9c4e771b82ea22ba4b8e630630a808d
Steve Astels <sastels@users.noreply.github.com>
2018-11-28 09:10:21 -0500

    Add fip banner (#45)

--- frontend/src/LanguageSwitcher.js
+++ frontend/src/LanguageSwitcher.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Query, Mutation } from 'react-apollo'
 import styled, { css } from 'react-emotion'
-import { Trans } from '@lingui/react'
 
 import gql from 'graphql-tag'
 
@@ -15,34 +14,21 @@ export const CHANGE_LANGUAGE_MUTATION = gql`
     switchLanguage @client
   }
 `
-
 const A = styled('a')`
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
   text-decoration: underline;
-  &:visited {
-    color: #7834bc;
-  }
   &:hover {
     cursor: pointer;
   }
 `
 const thing = css`
   align-content: right;
-`
-const langHeader = css`
-  clip: rect(1px, 1px, 1px, 1px);
-  height: 1px;
-  width: 1px;
-  margin: 0;
-  overflow: hidden;
-  position: absolute;
-  align-content: right;
+  display: block-inline;
 `
 
 export const LanguageSwitcher = () => (
   <section className={thing}>
-    <h2 className={langHeader}>
-      <Trans>Language Selection</Trans>
-    </h2>
     <Query query={GET_LANGUAGE_QUERY}>
       {({ data: { language } }) => (
         <Mutation mutation={CHANGE_LANGUAGE_MUTATION}>
