22476493fa49a6a2e693739c163c5e4c1bb7d318
Mike Williamson <mike@korora.ca>
2018-12-11 16:57:32 -0500

    Add error handling to <Thanks/>

--- frontend/src/thanks.js
+++ frontend/src/thanks.js
@@ -21,49 +21,61 @@ const paragraph = css`
 
 export const Thanks = () => (
   <Query query={GET_LANGUAGE_QUERY}>
-    {({ data: { language } }) => (
-      <div className={centercontent}>
-        <H1>
-          <Trans>Thank you for sharing your story.</Trans>
-        </H1>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading...'
+      if (error) return `Error! ${error.message}`
 
-        <Query query={GET_STATS_QUERY}>
-          {({ data: { stats } }) => (
-            <H3>
-              <Trans>
-                You are the {stats ? stats.reportCount : ''}th person to use
-                this tool to share a cybercrime story.
-              </Trans>
-            </H3>
-          )}
-        </Query>
+      let { language } = data
+      return (
+        <div className={centercontent}>
+          <H1>
+            <Trans>Thank you for sharing your story.</Trans>
+          </H1>
 
-        <TrackPageViews />
-        <div className={paragraph}>
-          <Trans>
-            For more information on how to stay safe online, you can visit{' '}
-            <a
-              href={
-                language === 'en'
-                  ? 'https://www.getcybersafe.gc.ca/index-en.aspx'
-                  : 'https://www.pensezcybersecurite.gc.ca/index-fr.aspx'
-              }
-            >
-              GetCyberSafe
-            </a>{' '}
-            and the{' '}
-            <a
-              href={
-                language === 'en'
-                  ? 'http://www.rcmp-grc.gc.ca/to-ot/tis-set/cyber-tips-conseils-eng.htm'
-                  : 'http://www.rcmp-grc.gc.ca/to-ot/tis-set/cyber-tips-conseils-fra.htm'
-              }
-            >
-              Top 10 Cyber Crime Prevention Tips.
-            </a>
-          </Trans>
+          <Query query={GET_STATS_QUERY}>
+            {({ loading, error, data }) => {
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
+
+              let { stats } = data
+              return (
+                <H3>
+                  <Trans>
+                    You are the {stats ? stats.reportCount : ''}th person to use
+                    this tool to share a cybercrime story.
+                  </Trans>
+                </H3>
+              )
+            }}
+          </Query>
+
+          <TrackPageViews />
+          <div className={paragraph}>
+            <Trans>
+              For more information on how to stay safe online, you can visit{' '}
+              <a
+                href={
+                  language === 'en'
+                    ? 'https://www.getcybersafe.gc.ca/index-en.aspx'
+                    : 'https://www.pensezcybersecurite.gc.ca/index-fr.aspx'
+                }
+              >
+                GetCyberSafe
+              </a>{' '}
+              and the{' '}
+              <a
+                href={
+                  language === 'en'
+                    ? 'http://www.rcmp-grc.gc.ca/to-ot/tis-set/cyber-tips-conseils-eng.htm'
+                    : 'http://www.rcmp-grc.gc.ca/to-ot/tis-set/cyber-tips-conseils-fra.htm'
+                }
+              >
+                Top 10 Cyber Crime Prevention Tips.
+              </a>
+            </Trans>
+          </div>
         </div>
-      </div>
-    )}
+      )
+    }}
   </Query>
 )
