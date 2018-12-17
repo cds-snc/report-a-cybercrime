b4fa5b332672e909e7005a9d35cfb2d8f36ae711
Mike Williamson <mike@korora.ca>
2018-12-17 17:06:37 -0500

    Simplify headers
    These components ended up more complicated than necessary. This commit slims
    things down by replacing them with styled.whatevers.

--- frontend/src/utils/headers.js
+++ frontend/src/utils/headers.js
@@ -1,50 +1,23 @@
-import React from 'react'
-import { cx, css } from 'react-emotion'
-import PropTypes from 'prop-types'
+import styled from 'react-emotion'
 
-const h1Style = css`
+export const H1 = styled.h1`
   font-size: 48px;
   line-height: 1.0416666666666667;
   font-weight: bold;
   margin: 0;
   margin-bottom: 30px;
 `
-const h3Style = css`
+
+export const H3 = styled.h3`
   font-size: 24px;
   line-height: 1.25;
   font-weight: bold;
   margin: 0;
   margin-bottom: 20px;
 `
-const h4Style = css`
+
+export const H4 = styled.h4`
   font-size: 19px;
   line-height: 1.31579;
   font-weight: bold;
 `
-
-export const H1 = props => (
-  <h1 className={cx(h1Style, props.className)}>{props.children}</h1>
-)
-
-H1.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  className: PropTypes.string,
-}
-
-export const H3 = props => (
-  <h3 className={cx(h3Style, props.className)}>{props.children}</h3>
-)
-
-H3.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  className: PropTypes.string,
-}
-
-export const H4 = props => (
-  <h4 className={cx(h4Style, props.className)}>{props.children}</h4>
-)
-
-H4.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  className: PropTypes.string,
-}
