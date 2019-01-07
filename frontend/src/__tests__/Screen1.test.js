49425a2604362351b0b4dd00381d5cbab468f11d
Mike Williamson <mike@korora.ca>
2019-01-07 12:47:55 -0500

    Split WhatHappenedForm out of Screen1

--- frontend/src/__tests__/Screen1.test.js
+++ frontend/src/__tests__/Screen1.test.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { mount } from 'enzyme'
-import { ApolloProvider } from 'react-apollo'
+import { render, cleanup } from 'react-testing-library'
+import { MockedProvider } from 'react-apollo/test-utils'
 import { Screen1 } from '../Screen1'
+import { I18nProvider } from '@lingui/react'
+import en from '../../locale/en/messages.js'
+const catalogs = { en }
 
-describe('Screen1', () => {
-  it('renders', () => {
-    mount(
-      <ApolloProvider client={{}}>
-        <Screen1 />
-      </ApolloProvider>,
+describe('<Screen1/>', () => {
+  afterEach(cleanup)
+
+  it('renders the expected text', () => {
+    let { container } = render(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <I18nProvider language={'en'} catalogs={catalogs}>
+          <Screen1 />
+        </I18nProvider>
+      </MockedProvider>,
     )
+
+    expect(container).toHaveTextContent('Describe what happened')
   })
 })
