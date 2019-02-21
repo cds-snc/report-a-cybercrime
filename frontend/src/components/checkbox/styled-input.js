6cdc09dd459ecf9463b8cdc348d9c5f89ca2f2a2
Steve Astels <sastels@gmail.com>
2019-02-21 17:07:43 -0500

    why don't the boxes have a focus border? :disappointed:

--- frontend/src/components/checkbox/styled-input.js
+++ frontend/src/components/checkbox/styled-input.js
@@ -5,22 +5,21 @@ import tag from 'clean-tag'
 import theme from '../../theme'
 
 const BaseStyledInput = styled(tag.input)`
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 24px;
-    height: 24px;
-    z-index: 1;
-    margin: 0;
-    zoom: 1;
-    opacity: 0;
-    :checked + span:after {
-      opacity: 1;
-    };
-    :focus + span:before {
-      boxShadow: 0 0 0 3px ${theme.colors.yellow};
-    };
-  };
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 24px;
+  height: 24px;
+  z-index: 1;
+  margin: 0;
+  zoom: 1;
+  opacity: 0;
+  :checked + span:after {
+    opacity: 1;
+  }
+  :focus + span:before {
+    boxshadow: 0 0 0 3px ${({ theme }) => theme.colors.yellow};
+  }
 `
 
 export const StyledInput = props => (
