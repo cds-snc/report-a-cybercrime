40e62372dca623d09e4126c3e3248edcbeb5112c
Steve Astels <sastels@gmail.com>
2019-03-21 16:13:29 -0400

    fix tests

--- frontend/src/__tests__/LanguageSwitcher.test.js
+++ frontend/src/__tests__/LanguageSwitcher.test.js
@@ -5,33 +5,28 @@ import React from 'react'
 import { testClient } from '../utils/createTestClient'
 
 describe('<LanguageSwitcher />', () => {
-  describe('with {language:"en"} in the client cache', () => {
-    it('displays a link to the French version', () => {
-      const englishClient = testClient({ language: 'en' })
-      const wrapper = mount(
-        <ApolloProvider client={englishClient}>
-          <LanguageSwitcher />
-        </ApolloProvider>,
-      )
-      expect(wrapper.text()).toMatch(/Français/)
-    })
+  it('displays the correct language name for lang=en', () => {
+    const wrapper = mount(
+      <ApolloProvider client={testClient({ language: 'en' })}>
+        <LanguageSwitcher language="en" />
+      </ApolloProvider>,
+    )
+    expect(wrapper.text()).toMatch(/English/)
   })
 
-  describe('with {language:"fr"} in the client cache', () => {
-    it('displays a link to the English version', () => {
-      const wrapper = mount(
-        <ApolloProvider client={testClient({ language: 'fr' })}>
-          <LanguageSwitcher />
-        </ApolloProvider>,
-      )
-      expect(wrapper.text()).toMatch(/English/)
-    })
+  it('displays the correct language name for lang=fr', () => {
+    const wrapper = mount(
+      <ApolloProvider client={testClient({ language: 'en' })}>
+        <LanguageSwitcher language="fr" />
+      </ApolloProvider>,
+    )
+    expect(wrapper.text()).toMatch(/Français/)
   })
 
   it(`renders a button and not a link because it doesn't navigate anywhere`, () => {
     const wrapper = mount(
-      <ApolloProvider client={testClient({ language: 'fr' })}>
-        <LanguageSwitcher />
+      <ApolloProvider client={testClient({ language: 'en' })}>
+        <LanguageSwitcher language="fr" />
       </ApolloProvider>,
     )
     expect(wrapper.find('button')).toHaveLength(1)
