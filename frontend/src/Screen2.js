102e10dcf9f3d3872bf02d54ff6dc6aabac5b8cf
evadb <evademersbrett@gmail.com>
2019-04-04 17:29:50 -0400

    Change to Page2, reaadd old Screen2 for now

--- frontend/src/Screen2.js
+++ frontend/src/Screen2.js
@@ -3,33 +3,41 @@ import { navigate } from '@reach/router'
 import { Trans } from '@lingui/macro'
 import { Form, Field } from 'react-final-form'
 import { H1 } from './components/header'
-
+import { Checkbox } from './components/checkbox'
+import { Link } from './components/link'
+import { Text } from './components/text'
+import { Breadcrumb } from './components/breadcrumb'
 import { ApolloConsumer } from 'react-apollo'
 import { TrackPageViews } from './TrackPageViews'
 import { i18nMark } from '@lingui/react'
+import { finalFormAdapter } from './utils/finalFormAdapter'
 import { Container } from './components/container'
-import { jsx, css } from '@emotion/core'
+import { jsx } from '@emotion/core'
 import { TextArea } from './components/text-area'
 import { Button } from './components/button'
 import { Label } from './components/label'
 import { P } from './components/paragraph'
 
+const CheckboxAdapter = finalFormAdapter(Checkbox)
+
 const submitAndNavigate = (client, data) => {
-  // client.writeData({ data })
-  console.log('hit submit')
+  client.writeData({ data })
   navigate('/old/form3')
 }
 
 const validate = values => {
   let errors = {}
-  if (!values.whatAreYouWorriedAbout || !values.identifier) {
-    errors.identifier = i18nMark(
+  if (
+    (!values.whatWasInvolved || !values.whatWasInvolved.length) &&
+    !values.whatWasInvolvedOther
+  ) {
+    errors.whatWasInvolvedOther = i18nMark(
       'Please complete the form to tell us what was affected.',
     )
   }
   return errors
 }
-const MyForm = () => (
+const MyForm = options => (
   <ApolloConsumer>
     {client => (
       <Form
@@ -37,73 +45,54 @@ const MyForm = () => (
         validate={validate}
         render={({ handleSubmit }) => (
           <form onSubmit={handleSubmit}>
-            <P fontSize={[3, null, 4]}>
-              <Label htmlFor="whatAreYouWorriedAbout">
-                <Trans>What are you worried about?</Trans>
-              </Label>
-            </P>
             <Container>
-              <Field id="whatAreYouWorriedAbout" name="whatAreYouWorriedAbout">
-                {({ input }) => (
-                  <Container>
-                    <TextArea
-                      width={1}
-                      border="1px solid black"
-                      height={['100px', null, '200px']}
-                      {...input}
-                      placeholder=""
-                    />
-                  </Container>
-                )}
-              </Field>
+              <P fontSize={4}>
+                <Label htmlFor="whatWasInvolved">
+                  <Trans>What was affected? Choose all that apply.</Trans>
+                </Label>
+              </P>
+              <Container mb={5}>
+                {Object.keys(options).map(key => {
+                  return (
+                    <Label key={key}>
+                      <Field
+                        id="whatWasInvolved"
+                        name="whatWasInvolved"
+                        component={CheckboxAdapter}
+                        type="checkbox"
+                        value={key}
+                        label={options[key]}
+                      />
+                    </Label>
+                  )
+                })}
+              </Container>
             </Container>
-            <P fontSize={[3, null, 4]} mt={5}>
-              <Label htmlFor="identifier">
-                <Trans>
-                  {' '}
-                  Enter a website link, email address, or phone number
-                </Trans>
+            <P fontSize={4}>
+              <Label htmlFor="whatWasInvolvedOther">
+                <Trans>Other</Trans>
               </Label>
             </P>
             <Container>
-              <Field id="identifier" name="identifier">
+              <Field id="whatWasInvolvedOther" name="whatWasInvolvedOther">
                 {({ input, meta }) => (
                   <Container>
-                    <TextArea
-                      width={1}
-                      border="1px solid black"
-                      height={['35px', null, '45px']}
-                      {...input}
-                      placeholder=""
-                    />
-                    <Container mt={3} display="inline-block">
-                      <P fontSize={[2, null, 3]} color="red">
-                        {meta.error && meta.touched && (
-                          <Trans id={meta.error} />
-                        )}
-                      </P>
+                    <TextArea {...input} placeholder="" />
+                    <Container
+                      mt={3}
+                      display="inline-block"
+                      fontSize={4}
+                      color="red"
+                    >
+                      {meta.error && meta.touched && <Trans id={meta.error} />}
                     </Container>
                   </Container>
                 )}
               </Field>
             </Container>
-            <Container display="flex" flexDirection="row" width={1}>
-              <Container width={1 / 2}>
-                <Button>
-                  <Trans>&lt; Back</Trans>
-                </Button>
-              </Container>
-              <Container width={1 / 2}>
-                <Button
-                  type="submit"
-                  css={css`
-                    float: right;
-                  `}
-                >
-                  <Trans>Next &gt;</Trans>
-                </Button>
-              </Container>
-            </Container>
+            <Button type="submit">
+              <Trans>Next</Trans>
+            </Button>
           </form>
         )}
       />
@@ -111,10 +100,33 @@ const MyForm = () => (
   </ApolloConsumer>
 )
 
+const options = {
+  website: <Trans>Website</Trans>,
+  email: <Trans>Email address</Trans>,
+  phone: <Trans>Phone</Trans>,
+  'text message': <Trans>Text messages</Trans>,
+  'social media': <Trans>Social media accounts</Trans>,
+  'bank account': <Trans>Bank accounts</Trans>,
+  "I'm not sure": <Trans>I’m not sure</Trans>,
+}
+
 export const Screen2 = () => (
-  <Container width={['100%', null, '750px']} m="auto">
-    <H1>Signal a suspected scam</H1>
+  <Container m="auto" width={[1, 1 / 2, 1 / 4]}>
+    <Breadcrumb>
+      <Link to="/old">
+        <Trans>Landing Page</Trans>
+      </Link>
+      <Link to="/old/form1">
+        <Trans>What Happened</Trans>
+      </Link>
+      <Text display="inline-block">
+        <Trans>How did that happen?</Trans>
+      </Text>
+    </Breadcrumb>
+    <H1>
+      <Trans>How did that happen?</Trans>
+    </H1>
     <TrackPageViews />
-    <MyForm />
+    {MyForm(options)}
   </Container>
 )
