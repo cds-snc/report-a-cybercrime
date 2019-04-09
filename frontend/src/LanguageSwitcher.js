ca53bcba64ee4fa549e63673818bc9fe8190d6d2
Steve Astels <sastels@gmail.com>
2019-04-09 14:58:01 -0400

    refactored ButtonLink as a new component

--- frontend/src/LanguageSwitcher.js
+++ frontend/src/LanguageSwitcher.js
@@ -2,21 +2,8 @@ import React from 'react'
 import { ApolloConsumer } from 'react-apollo'
 import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
+import { ButtonLink } from './components/button-link'
 
-const ButtonLink = styled('button')`
-  font-family: ${({ theme }) => theme.fontSans};
-  font-size: ${({ theme }) => theme.fontSizes[2]};
-  background: none;
-  border: none;
-  color: white;
-  text-decoration: underline;
-  &:hover {
-    cursor: pointer;
-  }
-  &:focus {
-    outline: 3px solid #ffbf47;
-  }
-`
 const LanguageSwitcherStyle = styled('section')`
   align-content: right;
   display: block-inline;
