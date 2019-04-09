c85366716d506aa89768ae347daa9a1e684494b6
evadb <evademersbrett@gmail.com>
2019-04-09 15:11:38 -0400

    Completed Email type with tests

--- api/src/Email.js
+++ api/src/Email.js
@@ -3,33 +3,41 @@ const { GraphQLError } = require('graphql/error')
 const { Kind } = require('graphql/language')
 
 const emailRegex = new RegExp(
-  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
 )
 
 function isEmail(value) {
   return emailRegex.test(value)
 }
 
-const GraphQLEmail = new GraphQLScalarType({
+const Email = new GraphQLScalarType({
   name: 'Email',
   description:
     'The `Email` scalar type represents an email address as specified by [RFC822](https://www.w3.org/Protocols/rfc822/).',
   serialize: value => {
-    if (!isEmail(value)) {
-      throw new GraphQLError(`Vale is not a valid email address: ${value}`)
+    if (typeof value !== 'string') {
+      throw new TypeError(`Value is not a string`)
     }
+
+    if (!isEmail(value)) {
+      throw new TypeError(`Value is not a valid email address: ${value}`)
+    }
+    return value
   },
 
   parseValue: value => {
-    if (!isEmail(value)) {
-      throw new GraphQLError(`Value is not a valid email address: ${value}`)
+    if (typeof value !== 'string') {
+      throw new TypeError(`Value is not a string`)
     }
 
+    if (!isEmail(value)) {
+      throw new TypeError(`Value is not a valid email address: ${value}`)
+    }
     return value
   },
 
   parseLiteral: ast => {
-    if (ast.kind != Kind.STRING) {
+    if (ast.kind !== Kind.STRING) {
       throw new GraphQLError(
         `Can only validate strings as email addresses but recieved: ${
           ast.kind
@@ -38,11 +46,11 @@ const GraphQLEmail = new GraphQLScalarType({
     }
 
     if (!isEmail(ast.value)) {
-      throw new GraphQLError(`Value is not a valid email address: ${ast.value}`)
+      throw new TypeError(`Value is not a valid email address: ${ast.value}`)
     }
 
     return ast.value
   },
 })
 
-module.exports.GraphQLEmail = GraphQLEmail
+module.exports.Email = Email
---
+++ api/src/__tests__/Email.test.js
@@ -0,0 +1,112 @@
+const { Email } = require('../Email')
+const { Kind } = require('graphql/language')
+const { GraphQLError } = require('graphql/error')
+
+describe('Email', () => {
+  describe('valid inputs', () => {
+    it('Serializes valid inputs', () => {
+      expect(Email.serialize('anemail@email.com')).toBe('anemail@email.com')
+    })
+
+    it('Parses valid inputs by value', () => {
+      expect(Email.parseValue('anemail@email.co.uk')).toBe(
+        'anemail@email.co.uk',
+      )
+    })
+
+    it('Parses valid inputs by literal', () => {
+      expect(
+        Email.parseLiteral({
+          value: 'anemail1234@gmail.com',
+          kind: Kind.STRING,
+        }),
+      ).toBe('anemail1234@gmail.com')
+    })
+  })
+
+  describe('invalid inputs', () => {
+    describe('input is not an email address', () => {
+      const invalidEmail = 'this is not an email'
+
+      it('rejects invalid emails in serialize()', () => {
+        expect(() => Email.serialize(invalidEmail)).toThrow(TypeError)
+      })
+
+      it('rejects invalid emails in parseValue()', () => {
+        expect(() => Email.parseValue(invalidEmail)).toThrow(TypeError)
+      })
+
+      it('rejects invalid emails in parseLiteral()', () => {
+        expect(() =>
+          Email.parseLiteral({
+            value: invalidEmail,
+            kind: Kind.STRING,
+          }),
+        ).toThrow(TypeError)
+      })
+    })
+
+    describe('input is not a string', () => {
+      const notAString = 1234
+
+      it('rejects non string inputs in serialize()', () => {
+        expect(() => Email.serialize(notAString)).toThrow(TypeError)
+      })
+
+      it('rejects non string inputs in parseValue()', () => {
+        expect(() => Email.parseValue(notAString)).toThrow(TypeError)
+      })
+
+      it('rejects non string inputs in parseLiteral()', () => {
+        expect(() =>
+          Email.parseLiteral({
+            value: notAString,
+            kind: Kind.INT,
+          }),
+        ).toThrow(GraphQLError)
+      })
+    })
+
+    describe('input is missing @ symbol', () => {
+      const missingAtSymbol = 'iamanemailgmail.com'
+
+      it('rejects emails that are missing an @ symbol in serialize()', () => {
+        expect(() => Email.serialize(missingAtSymbol)).toThrow(TypeError)
+      })
+
+      it('rejects emails that are missing an @ symbol in parseValue()', () => {
+        expect(() => Email.parseValue(missingAtSymbol)).toThrow(TypeError)
+      })
+
+      it('rejects emails that are missing an @ symbol in parseLiteral()', () => {
+        expect(() =>
+          Email.parseLiteral({
+            value: missingAtSymbol,
+            kind: Kind.STRING,
+          }),
+        ).toThrow(TypeError)
+      })
+    })
+
+    describe('input is missing domain extension', () => {
+      const missingDomainExt = 'iamanemail@gmailcom'
+
+      it('rejects emails that are missing a domain extension in serialize()', () => {
+        expect(() => Email.serialize(missingDomainExt)).toThrow(TypeError)
+      })
+
+      it('rejects emails that are missing a domain extension in parseValue()', () => {
+        expect(() => Email.parseValue(missingDomainExt)).toThrow(TypeError)
+      })
+
+      it('rejects emails that are missing a domain extension in parseLiteral()', () => {
+        expect(() =>
+          Email.parseLiteral({
+            value: missingDomainExt,
+            kind: Kind.STRING,
+          }),
+        ).toThrow(TypeError)
+      })
+    })
+  })
+})
