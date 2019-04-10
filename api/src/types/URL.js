748ef4586bcc3f11165908f60624641b5c37c3f3
Keith Colbourne <keithiancolbourne@gmail.com>
2019-04-10 15:44:53 -0400

    updated url validation

--- api/src/types/URL.js
+++ api/src/types/URL.js
@@ -1,13 +1,11 @@
 const { GraphQLScalarType } = require('graphql')
 const { GraphQLError } = require('graphql/error')
 const { Kind } = require('graphql/language')
+const { isUrl } = require('../utils')
 
 // this file taken and modified from https://github.com/okgrow/graphql-scalars
 
 // see https://gist.github.com/dperini/729294
-const URL_REGEX = new RegExp(
-  /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$|^https?:\/\/localhost.*|^http:\/\/(\d{1,3})(\.\d{1,3}){3}(:\d{4})?\/$/i,
-)
 
 const URL = new GraphQLScalarType({
   name: 'URL',
@@ -20,7 +18,7 @@ const URL = new GraphQLScalarType({
       throw new TypeError(`Value is not a string`)
     }
 
-    if (!URL_REGEX.test(value)) {
+    if (!isUrl(value)) {
       throw new TypeError(`Value is not a valid URL`)
     }
 
@@ -32,7 +30,7 @@ const URL = new GraphQLScalarType({
       throw new TypeError(`Value is not a string`)
     }
 
-    if (!URL_REGEX.test(value)) {
+    if (!isUrl(value)) {
       throw new TypeError(`Value is not a valid URL`)
     }
 
@@ -44,7 +42,7 @@ const URL = new GraphQLScalarType({
       throw new GraphQLError(`Can only validate strings as URLs`)
     }
 
-    if (!URL_REGEX.test(ast.value)) {
+    if (!isUrl(ast.value)) {
       throw new TypeError(`Value is not a valid URL`)
     }
 
