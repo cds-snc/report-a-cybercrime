58e81b3214d514b12afecb5e3138c45a7878218a
Steve Astels <sastels@gmail.com>
2019-04-11 22:03:11 -0400

    add French

--- frontend/src/Stats.js
+++ frontend/src/Stats.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react'
 import { Query } from 'react-apollo'
 import PropTypes from 'prop-types'
-import { i18nMark, Trans } from '@lingui/react'
+import { I18n } from '@lingui/react'
+import { t, Trans } from '@lingui/macro'
 import { P } from './components/paragraph'
 import { H3 } from './components/header'
 import { IDENTIFIER_FLAGGINGS_WITHIN } from './utils/queriesAndMutations'
@@ -14,43 +15,51 @@ import {
   ResponsiveContainer,
 } from 'recharts'
 import theme from './theme'
-import { ThemeContext } from '@emotion/core'
 
 class Chart extends PureComponent {
   render() {
     return (
-      <React.Fragment>
-        <H3 mt={6} textAlign="center">
-          <Trans>Reports similar to yours</Trans>
-        </H3>
-        <div style={{ width: '100%', height: 300 }}>
-          <ResponsiveContainer>
-            <AreaChart
-              data={this.props.data}
-              margin={{
-                top: 0,
-                right: 0,
-                left: -20,
-                bottom: 0,
-              }}
-            >
-              <CartesianGrid strokeDasharray="2 2" />
-              <XAxis fontFamily={theme.fontSans} dataKey="date" />
-              <YAxis
-                dataKey="total"
-                fontFamily={theme.fontSans}
-                label={{
-                  value: i18nMark('Reports per day'),
-                  fontSize: '17px',
-                  angle: -90,
-                  fontFamily: theme.fontSans,
-                }}
-              />
-              <Area type="monotone" dataKey="total" stroke="#000" fill="#999" />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-      </React.Fragment>
+      <I18n>
+        {({ i18n }) => (
+          <React.Fragment>
+            <H3 mt={6} textAlign="center">
+              <Trans>Reports similar to yours</Trans>
+            </H3>
+            <div style={{ width: '100%', height: 300 }}>
+              <ResponsiveContainer>
+                <AreaChart
+                  data={this.props.data}
+                  margin={{
+                    top: 0,
+                    right: 0,
+                    left: -20,
+                    bottom: 0,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="2 2" />
+                  <XAxis fontFamily={theme.fontSans} dataKey="date" />
+                  <YAxis
+                    dataKey="total"
+                    fontFamily={theme.fontSans}
+                    label={{
+                      value: i18n._(t`Reports per day`),
+                      fontSize: '17px',
+                      angle: -90,
+                      fontFamily: theme.fontSans,
+                    }}
+                  />
+                  <Area
+                    type="monotone"
+                    dataKey="total"
+                    stroke="#000"
+                    fill="#999"
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+            </div>
+          </React.Fragment>
+        )}
+      </I18n>
     )
   }
 }
