3dab703bfba5ff947c64d8be2f5450d367dadae8
Mike Williamson <mike.williamson@cds-snc.ca>
2019-04-13 12:58:45 -0400

    Add bilingual page titles
    The title of the page was showing some leftover values from previous
    incarnations of the app. This commit builds on the work in 4c67cef to create
    dynamic page titles that update as we switch language.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 /**@jsx jsx **/
 import React from 'react'
 import { Global, css, jsx } from '@emotion/core'
+import { I18n } from '@lingui/react'
 import { ThemeProvider } from 'emotion-theming'
 import theme from './theme'
 import { Query } from 'react-apollo'
@@ -13,101 +14,103 @@ import { Footer } from './components/footer'
 import { FooterLink } from './components/link'
 import { Content } from './components/container'
 
-const pageTitle = language => {
-  if (language === 'fr') {
-    return 'Racontez-nous votre histoire de cybercriminalité'
-  } else {
-    return 'Tell us your cybercrime story'
-  }
-}
-
 const App = () => (
-  <Query query={GET_LANGUAGE_QUERY}>
-    {({ data: { language } }) => (
-      <React.Fragment>
-        <Helmet>
-          <html lang={language} />
-          <meta charset="utf-8" />
-          <title lang={language}>{pageTitle(language)}</title>
-          <link
-            href="https://fonts.googleapis.com/css?family=Roboto"
-            rel="stylesheet"
-          />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link
-            rel="apple-touch-icon"
-            sizes="192x192"
-            href="/apple-touch-icon.png"
-          />
-          <script>
-            {`
+  <I18n>
+    {({ i18n }) => {
+      return (
+        <Query query={GET_LANGUAGE_QUERY}>
+          {({ data: { language } }) => (
+            <React.Fragment>
+              <Helmet>
+                <html lang={language} />
+                <meta charset="utf-8" />
+                console.log(i18n) return (
+                <title lang={language}>{i18n._('Report a scam')}</title>
+                )
+                <link
+                  href="https://fonts.googleapis.com/css?family=Roboto"
+                  rel="stylesheet"
+                />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1"
+                />
+                <link
+                  rel="apple-touch-icon"
+                  sizes="192x192"
+                  href="/apple-touch-icon.png"
+                />
+                <script>
+                  {`
               window.__APOLLO_STATE__=${JSON.stringify({
                 language,
               })}`}
-          </script>
-
-          {process.env.NODE_ENV === 'production' ? (
-            <script>
-              {`
+                </script>
+                {process.env.NODE_ENV === 'production' ? (
+                  <script>
+                    {`
               if ('serviceWorker' in navigator) {
                 window.addEventListener('load', () => {
                   navigator.serviceWorker.register('service-worker.js')
                 });
               }
             `}
-            </script>
-          ) : null}
-        </Helmet>
-        <Global
-          styles={css`
-            html,
-            body {
-              height: 100%;
-            }
+                  </script>
+                ) : null}
+              </Helmet>
+              <Global
+                styles={css`
+                  html,
+                  body {
+                    height: 100%;
+                  }
 
-            body {
-              margin: 0;
-              padding: 0;
-            }
+                  body {
+                    margin: 0;
+                    padding: 0;
+                  }
 
-            #root {
-              display: flex;
-              flex-direction: column;
-              height: 100%;
-            }
-          `}
-        />
-        <ThemeProvider theme={theme}>
-          <TopBanner lang={language} bg="black" />
-          <Content>
-            <Home />
-          </Content>
+                  #root {
+                    display: flex;
+                    flex-direction: column;
+                    height: 100%;
+                  }
+                `}
+              />
+              <ThemeProvider theme={theme}>
+                <TopBanner lang={language} bg="black" />
+                <Content>
+                  <Home />
+                </Content>
 
-          <Footer bg="black">
-            <FooterLink
-              href={
-                language === 'en'
-                  ? 'https://digital.canada.ca/legal/privacy/'
-                  : 'https://numerique.canada.ca/transparence/confidentialite/'
-              }
-            >
-              <Trans>Privacy</Trans>
-            </FooterLink>
-            <FooterLink
-              href={
-                language === 'en'
-                  ? 'https://digital.canada.ca/legal/terms/'
-                  : 'https://numerique.canada.ca/transparence/avis/'
-              }
-            >
-              <Trans>Terms and Conditions</Trans>
-            </FooterLink>
-          </Footer>
-        </ThemeProvider>
-      </React.Fragment>
-    )}
-  </Query>
+                <Footer bg="black">
+                  <FooterLink
+                    href={
+                      language === 'en'
+                        ? 'https://digital.canada.ca/legal/privacy/'
+                        : 'https://numerique.canada.ca/transparence/confidentialite/'
+                    }
+                  >
+                    <Trans>Privacy</Trans>
+                  </FooterLink>
+                  <FooterLink
+                    href={
+                      language === 'en'
+                        ? 'https://digital.canada.ca/legal/terms/'
+                        : 'https://numerique.canada.ca/transparence/avis/'
+                    }
+                  >
+                    <Trans>Terms and Conditions</Trans>
+                  </FooterLink>
+                </Footer>
+              </ThemeProvider>
+            </React.Fragment>
+          )}
+        </Query>
+      )
+    }}
+  </I18n>
 )
 
 export default App
