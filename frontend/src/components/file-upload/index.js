2bf423d709bdc3609404b80959db024f16b4fd7c
Steve Astels <sastels@gmail.com>
2019-04-16 10:20:52 -0400

    add focus border to button

--- frontend/src/components/file-upload/index.js
+++ frontend/src/components/file-upload/index.js
@@ -1,27 +1,26 @@
 /** @jsx jsx */
 import React from 'react'
 import PropTypes from 'prop-types'
-import { jsx, css } from '@emotion/core'
+import { jsx } from '@emotion/core'
 import { fontSize, fontWeight, lineHeight, space, color } from 'styled-system'
 import { Label } from '../label'
 import { Input } from '../input'
+import('./index.css')
 
 export const FileUpload = ({ onChange, accept, ...props }) => {
   return (
     <React.Fragment>
-      <Label colors="button" padding={1} htmlFor="uploader" {...props}>
-        {props.children}
-      </Label>
       <Input
         type="file"
         id="uploader"
         name="uploader"
         accept={accept}
         onChange={onChange}
-        css={css`
-          opacity: 0;
-        `}
+        width="0"
       />
+      <Label colors="button" padding={1} htmlFor="uploader" {...props}>
+        {props.children}
+      </Label>
     </React.Fragment>
   )
 }
