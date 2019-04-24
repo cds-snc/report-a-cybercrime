0bf955ce7b790fb990b720421e3d94330c28e92b
Mike Williamson <mike.williamson@cds-snc.ca>
2019-04-24 11:10:17 -0400

    Use better summary data
    This commit modifies the frontend to make use of the new flaggingsWithin
    function exposed by the api. This will address some of the shortcomings with
    the data visualization (ie, not displaying days with 0 reports).
    The values for the demo are still hardcoded (ie the phone number identifier),
    but the refactoring here will allow us to remove them and get this working for
    real easily. This will be the focus of a followup PR.

--- frontend/src/__tests__/Stats.test.js
+++ frontend/src/__tests__/Stats.test.js
@@ -1,31 +1,49 @@
 import React from 'react'
 import wait from 'waait'
-import { mount } from 'enzyme'
+import { render, cleanup } from 'react-testing-library'
 import { MockedProvider } from 'react-apollo/test-utils'
 import { I18nProvider } from '@lingui/react'
 import { Stats } from '../Stats'
+import { Chart } from '../Chart'
 import en from '../../locale/en/messages.js'
-import { IDENTIFIER_FLAGGINGS_WITHIN } from '../utils/queriesAndMutations'
+import { FLAGGINGS_WITHIN } from '../utils/queriesAndMutations'
 const catalogs = { en }
+
+let data = [
+  {
+    date: '2019-04-01',
+    total: 1,
+  },
+  {
+    date: '2019-04-02',
+    total: 1,
+  },
+  {
+    date: '2019-04-03',
+    total: 3,
+  },
+  {
+    date: '2019-04-04',
+    total: 0,
+  },
+]
 
 let mocks = [
   {
     request: {
-      query: IDENTIFIER_FLAGGINGS_WITHIN,
-      variables: { identifier: '555-555-5555' },
+      query: FLAGGINGS_WITHIN,
+      variables: {
+        identifier: '555-555-5555',
+        startDate: '2019-04-01',
+        endDate: '2019-04-04',
+      },
     },
     result: {
       data: {
         stats: {
-          identifierFlaggingsWithin: {
+          flaggingsWithin: {
             identifier: '555-555-5555',
-            summary: [
-              { date: 'Monday', total: 6 },
-              { date: 'Tuesday', total: 9 },
-              { date: 'Wednesday', total: 23 },
-              { date: 'Thursday', total: 30 },
-              { date: 'Friday', total: 54 },
-            ],
+            summary: data,
           },
         },
       },
@@ -34,21 +52,37 @@ let mocks = [
 ]
 
 describe('<Stats/>', () => {
+  afterEach(cleanup)
   describe('when the API returns data', () => {
-    it('displays stats', async () => {
+    it(`calls it's child as a function`, async () => {
       /* eslint-disable no-console */
       let warn = console.warn
       console.warn = jest.fn()
-      let wrapper = mount(
+      let { container } = render(
         <MockedProvider mocks={mocks} addTypename={false}>
           <I18nProvider language={'en'} catalogs={catalogs}>
-            <Stats />
+            <Stats
+              identifier="555-555-5555"
+              startDate="2019-04-01"
+              endDate="2019-04-04"
+            >
+              {({ summary }) => (
+                <Chart height={100} width={100} data={summary} />
+              )}
+            </Stats>
           </I18nProvider>
         </MockedProvider>,
       )
 
+      // Wait for promise with data to resolve
       await wait(0)
-      expect(wrapper.text()).toMatch('')
+
+      let chartLabels = Array.from(container.querySelectorAll('tspan')).map(
+        e => e.innerHTML,
+      )
+
+      let dates = data.map(d => d.date)
+      expect(chartLabels).toContain(...dates)
       console.warn = warn
       /* eslint-enable no-console */
     })
@@ -56,43 +90,70 @@ describe('<Stats/>', () => {
 
   describe('when loading', () => {
     it('displays an empty string', async () => {
-      let wrapper = mount(
+      /* eslint-disable no-console */
+      let warn = console.warn
+      console.warn = jest.fn()
+      let { container } = render(
         <MockedProvider mocks={mocks} addTypename={false}>
           <I18nProvider language={'en'} catalogs={catalogs}>
-            <Stats />
+            <Stats
+              identifier="555-555-5555"
+              startDate="2019-04-01"
+              endDate="2019-04-04"
+            >
+              {({ summary }) => (
+                <Chart height={100} width={100} data={summary} />
+              )}
+            </Stats>
           </I18nProvider>
         </MockedProvider>,
       )
 
-      expect(wrapper.text()).toMatch('')
+      let child = container.firstChild
+      expect(child.textContent).toEqual('')
+      console.warn = warn
+      /* eslint-enable no-console */
     })
   })
 
   describe('when an error is raised', () => {
-    it('displays an error message', async () => {
+    it('displays an error message instead of the child', async () => {
       /* eslint-disable no-console */
       let warn = console.warn
       console.warn = jest.fn()
-      let mocks = [
+      let failureMock = [
         {
           request: {
-            query: IDENTIFIER_FLAGGINGS_WITHIN,
-            variables: { identifier: '555-555-5555' },
+            query: FLAGGINGS_WITHIN,
+            variables: {
+              identifier: '555-555-5555',
+              startDate: '2019-04-01',
+              endDate: '2019-04-04',
+            },
           },
           error: new Error('sadness'),
         },
       ]
 
-      let wrapper = mount(
-        <MockedProvider mocks={mocks} addTypename={false}>
+      let { container } = render(
+        <MockedProvider mocks={failureMock} addTypename={false}>
           <I18nProvider language={'en'} catalogs={catalogs}>
-            <Stats />
+            <Stats
+              identifier="555-555-5555"
+              startDate="2019-04-01"
+              endDate="2019-04-04"
+            >
+              {({ summary }) => (
+                <Chart height={100} width={100} data={summary} />
+              )}
+            </Stats>
           </I18nProvider>
         </MockedProvider>,
       )
 
       await wait(0)
-      expect(wrapper.text()).toMatch(/sadness/)
+      let child = container.firstChild
+      expect(child.textContent).toMatch(/sadness/)
       console.warn = warn
       /* eslint-enable no-console */
     })
