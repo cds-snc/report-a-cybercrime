d5e6b1a63d276c419ede240f4d58013aaea1200c
Steve Astels <sastels@gmail.com>
2019-05-06 13:20:24 -0400

    rough option 3 page code

---
+++ frontend/src/Option3Page.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { Trans } from '@lingui/macro'
+import { H1, H2 } from './components/header'
+import { P } from './components/paragraph'
+import { Ul } from './components/unordered-list'
+import { Li } from './components/list-item'
+import { ButtonLink, ButtonAnchor } from './components/link'
+import { TrackPageViews } from './TrackPageViews'
+import { Container } from './components/container'
+
+const CenterContent = styled('div')`
+  max-width: 750px;
+  margin: auto;
+`
+
+export const Option3Page = () => (
+  <CenterContent>
+    <TrackPageViews />
+    <H1 textAlign="center" fontSize={[5, null, 6]}>
+      <Trans>Reporting Options</Trans>
+    </H1>
+    <Container textAlign="center">
+      <Container>
+        <ButtonLink mb={[3, null, 5]} to="">
+          <Trans>Report online</Trans>
+        </ButtonLink>
+      </Container>
+      <Container>
+        <ButtonLink mb={[3, null, 5]} to="">
+          <Trans>Long Form</Trans>
+        </ButtonLink>
+      </Container>
+      <Container>
+        <ButtonAnchor
+          mb={[3, null, 5]}
+          href="http://www.antifraudcentre-centreantifraude.ca/reportincident-signalerincident/index-eng.htm"
+        >
+          <Trans>Call the Canadian Anti-Fraud Centre</Trans>
+        </ButtonAnchor>
+      </Container>
+    </Container>
+  </CenterContent>
+)
