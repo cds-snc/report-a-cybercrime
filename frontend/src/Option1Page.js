9929d9257975d9f12d759a088c576cc996a3d11a
Steve Astels <sastels@gmail.com>
2019-05-08 11:07:27 -0400

    spiff up a bit

--- frontend/src/Option1Page.js
+++ frontend/src/Option1Page.js
@@ -3,7 +3,8 @@ import styled from '@emotion/styled'
 import { Trans } from '@lingui/macro'
 import { H1, H2 } from './components/header'
 import { P } from './components/paragraph'
-import { ButtonLink } from './components/link'
+import { Ul } from './components/unordered-list'
+import { Li } from './components/list-item'
 import { TrackPageViews } from './TrackPageViews'
 import { Container } from './components/container'
 
@@ -16,20 +17,19 @@ export const Option1Page = () => (
   <CenterContent>
     <TrackPageViews />
     <H1 textAlign="center">
-      <Trans>You should:</Trans>
+      <Trans>Call 911</Trans>
     </H1>
-    <Container textAlign="center" fontSize={[5, null, 6]}>
-      <Container>
-        <ButtonLink mb={[3, null, 5]} to="">
-          <Trans>Call 911</Trans>
-        </ButtonLink>
-      </Container>
+    <Container>
       <Container marginTop={[3, null, 5]}>
         <H2 fontSize={[3, null, 4]}>
           <Trans>You will get:</Trans>
         </H2>
         <P>
-          <Trans>Local, immediate response</Trans>
+          <Ul>
+            <Li>
+              <Trans>Local, immediate response</Trans>
+            </Li>
+          </Ul>
         </P>
       </Container>
     </Container>
