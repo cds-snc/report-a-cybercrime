43b8002e16ef143250d6d253521843e837da2f7e
Keith Colbourne <keithiancolbourne@gmail.com>
2019-05-08 10:53:54 -0400

    cssing

--- frontend/src/HowToTellPolice.js
+++ frontend/src/HowToTellPolice.js
@@ -1,62 +1,104 @@
 /** @jsx jsx */
 import { Link } from '@reach/router'
 import { Trans } from '@lingui/macro'
-import { ButtonLink } from './components/link'
-import { H1 } from './components/header'
-import { Button } from './components/button'
-import { TrackPageViews } from './TrackPageViews'
+import { H1, H2 } from './components/header'
+import styled from '@emotion/styled'
 import { Container } from './components/container'
-import { P } from './components/paragraph'
 import { jsx, css } from '@emotion/core'
 import cafc from './images/cafc.svg'
 import online from './images/online.svg'
 
+const CenterContent = styled('div')`
+  max-width: 750px;
+  margin: auto;
+`
+
+const Card = styled('div')`
+  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  padding-bottom: 1rem;
+  border-radius: 5px;
+  border: 1px solid grey;
+  max-width: 300px;
+  :hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+`
+
 export const HowToTellPolice = () => (
-  <Container width={['100%', null, '600px']} m="auto">
-    <H1 fontSize={[5, null, 6]}>
-      <Trans>How do you want to tell the police?</Trans>
+  <CenterContent>
+    <H1 fontSize={[5, null, 6]} mb={5}>
+      <Trans>{'How do you want to tell the police?'}</Trans>
     </H1>
-    <Container textAlign="center" mb={3} mt={3}>
-      <P fontSize={[3, null, 4]}>
-        <Link to="/tbd">
-          <Trans>I want to talk to someone directly</Trans>
+    <Container
+      display="flex"
+      flexDirection={['column', null, 'row']}
+      width={1}
+      alignItems="center"
+      mb={6}
+    >
+      <Container
+        flex="1 1 auto"
+        css={css`
+          text-align: center;
+          float: left;
+        `}
+        mb={[4, null, 0]}
+        mr={[0, null, 5]}
+      >
+        <Link to="/">
+          <Card>
+            <img
+              src={cafc}
+              alt="I want to talk to someone directly"
+              css={css`
+                width: 100%;
+              `}
+            />
+            <H2
+              fontSize={[3, null, 4]}
+              css={css`
+                text-decoration: underline;
+              `}
+            >
+              <Trans>{'I want to talk to someone directly'}</Trans>
+              <br />
+              <br />
+            </H2>
+          </Card>
         </Link>
-      </P>
-      <img
-        src={cafc}
-        style={{ width: '250px', height: '250px' }}
-        alt="Talk to somebody"
-      />
-    </Container>
-    <Container textAlign="center" mb={3} mt={3}>
-      <P fontSize={[3, null, 4]}>
-        <Link to="/tbd">
-          <Trans>I want to report on my own</Trans>
+      </Container>
+      <Container
+        flex="1 1 auto"
+        css={css`
+          text-align: center;
+          float: right;
+        `}
+      >
+        <Link to="/">
+          <Card
+            css={css`
+              float: right;
+            `}
+          >
+            <img
+              src={online}
+              alt="I want to report on my own"
+              css={css`
+                width: 100%;
+              `}
+            />
+            <H2
+              fontSize={[3, null, 4]}
+              css={css`
+                text-decoration: underline;
+              `}
+            >
+              <Trans>{'I want to report on my own'}</Trans>
+            </H2>
+          </Card>
         </Link>
-      </P>
-      <img
-        src={online}
-        alt="Report on my own"
-        style={{ width: '250px', height: '250px' }}
-      />
-    </Container>
-    <TrackPageViews />
-    <Container display="flex" flexDirection="row">
-      <Container width={1 / 2}>
-        <ButtonLink to="/">
-          &lt; <Trans>Back</Trans>
-        </ButtonLink>
-      </Container>
-      <Container width={'400px'}>
-        <Button
-          type="Submit"
-          css={css`
-            float: right;
-          `}
-        >
-          <Trans>Cancel</Trans>
-        </Button>
       </Container>
     </Container>
-  </Container>
+  </CenterContent>
 )
