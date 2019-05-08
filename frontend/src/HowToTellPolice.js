e2fe4d5c3cc2e5495ee355f9e0e699524f330b03
Keith Colbourne <keithiancolbourne@gmail.com>
2019-05-08 13:43:14 -0400

    Content updates

--- frontend/src/HowToTellPolice.js
+++ frontend/src/HowToTellPolice.js
@@ -14,7 +14,7 @@ const CenterContent = styled('div')`
 `
 
 const Card = styled('div')`
-  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   padding-bottom: 1rem;
   border-radius: 5px;
@@ -28,7 +28,7 @@ const Card = styled('div')`
 export const HowToTellPolice = () => (
   <CenterContent>
     <H1 fontSize={[5, null, 6]} mb={5}>
-      <Trans>{'How do you want to tell the police?'}</Trans>
+      <Trans>{'How do you want to make your report?'}</Trans>
     </H1>
     <Container
       display="flex"
@@ -50,7 +50,7 @@ export const HowToTellPolice = () => (
           <Card>
             <img
               src={cafc}
-              alt="I want to talk to someone directly"
+              alt="I would like to talk to someone"
               css={css`
                 width: 100%;
               `}
@@ -61,9 +61,7 @@ export const HowToTellPolice = () => (
                 text-decoration: underline;
               `}
             >
-              <Trans>{'I want to talk to someone directly'}</Trans>
-              <br />
-              <br />
+              <Trans>{'I would like to talk to someone'}</Trans>
             </H2>
           </Card>
         </Link>
@@ -83,7 +81,7 @@ export const HowToTellPolice = () => (
           >
             <img
               src={online}
-              alt="I want to report on my own"
+              alt="I would like to report online on my own"
               css={css`
                 width: 100%;
               `}
@@ -94,7 +92,7 @@ export const HowToTellPolice = () => (
                 text-decoration: underline;
               `}
             >
-              <Trans>{'I want to report on my own'}</Trans>
+              <Trans>{'I would like to report online on my own'}</Trans>
             </H2>
           </Card>
         </Link>
