dfc5dfa070cb905af1745f1969409d82c7f2a5f0
Steve Astels <sastels@users.noreply.github.com>
2019-05-08 15:56:55 -0400

    tweak option 1 and 2 pages (#298)

--- frontend/src/Option1Page.js
+++ frontend/src/Option1Page.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { Trans } from '@lingui/macro'
-import { H1, H2 } from './components/header'
-import { Ul } from './components/unordered-list'
-import { Li } from './components/list-item'
+import { H1 } from './components/header'
+import { P } from './components/paragraph'
 import { TrackPageViews } from './TrackPageViews'
 import { Container } from './components/container'
 
@@ -20,14 +19,9 @@ export const Option1Page = () => (
     </H1>
     <Container>
       <Container marginTop={[3, null, 5]}>
-        <H2 fontSize={[3, null, 4]}>
-          <Trans>You will get:</Trans>
-        </H2>
-        <Ul>
-          <Li>
-            <Trans>Local, immediate response</Trans>
-          </Li>
-        </Ul>
+        <P fontSize={[3, null, 4]}>
+          <Trans>You will get a local immediate response.</Trans>
+        </P>
       </Container>
     </Container>
   </CenterContent>
--- frontend/src/Option2Page.js
+++ frontend/src/Option2Page.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { Trans } from '@lingui/macro'
-import { H1, H2 } from './components/header'
-import { Ul } from './components/unordered-list'
-import { Li } from './components/list-item'
+import { H1 } from './components/header'
+import { P } from './components/paragraph'
 import { TrackPageViews } from './TrackPageViews'
 import { Container } from './components/container'
 
@@ -23,14 +22,9 @@ export const Option2Page = () => (
     </H1>
     <Container>
       <Container marginTop={[3, null, 5]}>
-        <H2 fontSize={[3, null, 4]}>
-          <Trans>You will get:</Trans>
-        </H2>
-        <Ul>
-          <Li>
-            <Trans>specific advice based on your situation</Trans>
-          </Li>
-        </Ul>
+        <P fontSize={[3, null, 4]}>
+          <Trans>You’ll get specific advice based on your situation.</Trans>
+        </P>
       </Container>
     </Container>
   </CenterContent>
