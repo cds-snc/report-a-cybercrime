84290de6be9d23fa1744b1b678df8158968ba6ea
Steve Astels <sastels@gmail.com>
2019-06-11 21:46:50 -0400

    more translations

--- frontend/src/FileUpload.js
+++ frontend/src/FileUpload.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { Component } from 'react'
 import { Trans } from '@lingui/macro'
+import { I18n } from '@lingui/react'
 import { jsx } from '@emotion/core'
 import { Form, Field } from 'react-final-form'
 import styled from '@emotion/styled'
@@ -14,6 +15,7 @@ import { Button } from './components/button'
 import { FileUpload } from './components/file-upload'
 import { finalFormAdapter } from './utils/finalFormAdapter'
 import { UPLOAD_FILE_MUTATION } from './utils/queriesAndMutations'
+import { valueToObjectRepresentation } from 'apollo-utilities'
 
 const FileUploadAdapter = finalFormAdapter(FileUpload)
 const CenterContent = styled('div')`
@@ -25,6 +27,8 @@ export class FileUploadPage extends Component {
     super(props)
     this.state = {
       file: undefined,
+      uploadSuccess: undefined,
+      fileMD5: undefined,
       resultText: '', // Text that shows the result of the upload operation, does not appear if blank
       postSubmission: false, // Whether or not we are asking the user to submit further files
       dataRestrictionsSpecific: '',
@@ -35,17 +39,17 @@ export class FileUploadPage extends Component {
     this.setState({ file: e.target.files[0] })
   }
 
-  makeFeedbackString = result => {
-    var returnString = ''
-    returnString +=
-      result.success === true
-        ? 'File uploaded successfully'
-        : result.errorMessage
-    if (result.success === true) {
-      returnString += '\n' + 'MD5: ' + result.md5
-    }
-    return returnString
-  }
+  // makeFeedbackString = result => {
+  //   var returnString = ''
+  //   returnString +=
+  //     result.success === true
+  //       ? 'File uploaded successfully'
+  //       : result.errorMessage
+  //   // if (result.success === true) {
+  //   //   returnString += '\n' + 'MD5: ' + result.md5
+  //   // }
+  //   return returnString
+  // }
 
   submit = uploadFile => {
     const file = this.state.file
@@ -58,7 +62,8 @@ export class FileUploadPage extends Component {
       this.setState({
         file: undefined,
         postSubmission: true,
-        resultText: this.makeFeedbackString(fileResult.data.uploadFile),
+        uploadSuccess: fileResult.data.uploadFile.success,
+        fileMD5: fileResult.data.uploadFile.md5,
       })
     })
   }
@@ -145,8 +150,17 @@ export class FileUploadPage extends Component {
                           <P
                             mt={4}
                             style={{ whiteSpace: 'pre-wrap' }}
-                            hidden={this.state.resultText === ''}
+                            hidden={this.state.uploadSuccess === undefined}
                           >
+                            {this.state.uploadSuccess ? (
+                              <Trans>
+                                File uploaded successfully, MD5:{' '}
+                                {this.state.fileMD5}
+                              </Trans>
+                            ) : (
+                              <Trans>Error uploading file</Trans>
+                            )}
+
                             {this.state.resultText}
                           </P>
                           {this.state.postSubmission && (
