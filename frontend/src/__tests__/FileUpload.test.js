dab76289bce3302198052346e207d3108b294521
Steve Astels <sastels@gmail.com>
2019-06-17 13:32:29 -0400

    use new react testing library

--- frontend/src/__tests__/FileUpload.test.js
+++ frontend/src/__tests__/FileUpload.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, cleanup } from 'react-testing-library'
+import { render, cleanup } from '@testing-library/react'
 import { ApolloProvider } from 'react-apollo'
 import { testClient } from '../utils/createTestClient'
 import { FileUploadPage } from '../FileUpload'
