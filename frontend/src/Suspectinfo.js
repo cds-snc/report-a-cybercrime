7c46843224dc9e64a8cfa129cf3f9686b75a3cdf
Steve Astels <sastels@users.noreply.github.com>
2019-06-27 14:53:49 -0400

    put all yes/no buttons in rows rather than cols (#386)

--- frontend/src/Suspectinfo.js
+++ frontend/src/Suspectinfo.js
@@ -1,13 +1,23 @@
-import React from 'react'
+/**@jsx jsx */
+import { jsx, css } from '@emotion/core'
 import styled from '@emotion/styled'
 import { Trans } from '@lingui/macro'
 import { H1 } from './components/header'
 import { ButtonLink } from './components/link'
 import { TrackPageViews } from './TrackPageViews'
+import { Container } from './components/container'
 
 const CenterContent = styled('div')`
   max-width: 750px;
   margin: AudioTrack;
+`
+
+const bottomBarContainer = css`
+  display: flex;
+  width: 50%;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 20px;
 `
 
 export const Suspectinfo = () => (
@@ -16,24 +26,14 @@ export const Suspectinfo = () => (
     <H1 fontSize={[5, null, 6]}>
       <Trans>Do you have information about the suspect</Trans>
     </H1>
-    <p>
+
+    <Container css={bottomBarContainer}>
       <ButtonLink mb={[3, null, 5]} to="/suspectinfo">
         <Trans>Yes</Trans>
       </ButtonLink>
-    </p>
-
-    <br />
-    <p>
       <ButtonLink mb={[3, null, 5]} to="/moneylostquestion">
         <Trans>No</Trans>
       </ButtonLink>
-    </p>
-
-    <br />
-    <p>
-      <ButtonLink mb={[3, null, 5]} to="/">
-        <Trans>Cancel Report</Trans>
-      </ButtonLink>
-    </p>
+    </Container>
   </CenterContent>
 )
