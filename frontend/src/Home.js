d23259a51530abe43d6ce05ecffbb7293ce2d75a
Keith Colbourne <keithiancolbourne@gmail.com>
2019-06-27 14:49:02 -0400

    trying to fix merge and trans issues

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,57 +4,66 @@ import { Trans } from '@lingui/macro'
 import styled from '@emotion/styled'
 import { PhaseBanner } from './components/phase-banner'
 import { PageNotFound } from './PageNotFound'
-import { Page1 } from './Page1'
+
 import { Urgency } from './Urgency'
-import { FileUploadPage } from './FileUpload'
+import { FileUploadPage } from './FileUploadPage'
 import { AmountOfInfoPage } from './amountOfInfoPage'
 import { HowToTellPolice } from './HowToTellPolice'
 import { IdentityPage } from './identityPage'
 import { Option1Page } from './Option1Page'
 import { Option2Page } from './Option2Page'
 import { Option3Page } from './Option3Page'
+import { Ifvictim } from './Ifvictim'
+import { Suspectinfo } from './Suspectinfo'
 import { DemoPage } from './DemoPage'
-import { OtherFlowPage } from './OtherFlowPage'
+import { ScamInfoPage } from './ScamInfoPage'
 import { MoneyLostPage } from './MoneyLostPage'
+import { ConfirmationPage } from './ConfirmationPage'
 import { SuspectInfoPage } from './SuspectInfoPage'
 import { ContactInfoPage } from './ContactInfoPage'
 import { LoseMoney } from './LoseMoney'
 import { ThankYou } from './ThankYou'
+import { Willbecontacted } from './Willbecontacted'
+import { LandingPage } from './LandingPage'
+import { FilesToUpload } from './FilesToUpload'
 
 const Root = styled('div')`
   margin: 20pt;
 `
-
 const Screen = styled('div')`
   margin-top: 30pt;
 `
-
 export const Home = () => (
   <Root>
     <PhaseBanner phase={<Trans>ALPHA</Trans>} phaseColor="purple">
       <Trans>
-        This site will be changing as we test ideas and learn from people like
-        you.
+        Hello, This site will be changing as we test ideas and learn from people
+        like you.
       </Trans>
     </PhaseBanner>
     <Screen>
       <Router>
         <PageNotFound default />
         <DemoPage path="/demo" />
-        <Page1 path="/" />
         <Option1Page path="/option1" />
         <Option2Page path="/option2" />
         <Option3Page path="/option3" />
         <HowToTellPolice path="/howtotell" />
-        <LoseMoney path="/losemoney" />
-        <FileUploadPage path="/file" />
         <Urgency path="/urgency" />
         <AmountOfInfoPage path="/amountofinfo" />
         <IdentityPage path="/identity" />
-        <OtherFlowPage path="/otherflow" />
-        <MoneyLostPage path="/moneylost" />
+        <LandingPage path="/" />
+        <Ifvictim path="/ifvictimquestion" />
+        <ScamInfoPage path="/scaminfo" />
+        <Suspectinfo path="/suspectinfoquestion" />
         <SuspectInfoPage path="/suspectinfo" />
+        <LoseMoney path="/moneylostquestion" />
+        <MoneyLostPage path="/moneylost" />
+        <FilesToUpload path="/uploadfilesquestion" />
+        <FileUploadPage path="/uploadfiles" />
+        <Willbecontacted path="/contactinfoquestion" />
         <ContactInfoPage path="/contactinfo" />
+        <ConfirmationPage path="/confirmation" />
         <ThankYou path="/thankyou" />
       </Router>
     </Screen>
