a7a6aca2771796df239b8a9ec262161e8b4e51f4
Keith Colbourne <keithiancolbourne@gmail.com>
2019-06-28 11:07:35 -0400

    connecting stepper

--- frontend/src/components/stepper/index.js
+++ frontend/src/components/stepper/index.js
@@ -2,28 +2,19 @@
 import { jsx } from '@emotion/core'
 import Stepper from 'react-stepper-horizontal'
 import PropTypes from 'prop-types'
-import { I18n, i18nMark } from '@lingui/react'
-
-const languages = [
-  i18nMark('Describe'),
-  i18nMark('Money'),
-  i18nMark('Scammer'),
-  i18nMark('Upload'),
-  i18nMark('Contact'),
-]
 
 export const Steps = ({ activeStep, ...props }) => (
-  <I18n>
-    {({ i18n }) => (
-      <Stepper
-        steps={languages.map(key => {
-          return { title: i18n._(key) }
-        })}
-        activeStep={activeStep}
-        {...props}
-      />
-    )}
-  </I18n>
+  <Stepper
+    steps={[
+      { title: '' },
+      { title: '' },
+      { title: '' },
+      { title: '' },
+      { title: '' },
+    ]}
+    activeStep={activeStep}
+    {...props}
+  />
 )
 
 Steps.propTypes = {
