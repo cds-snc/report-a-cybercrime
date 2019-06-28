6fa0afea99d78d06d8d123cef0ed216acec0604c
Keith Colbourne <keithiancolbourne@gmail.com>
2019-06-28 11:04:46 -0400

    connecting stepper

--- frontend/src/MoneyLostPage.js
+++ frontend/src/MoneyLostPage.js
@@ -1,10 +1,11 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx, css } from '@emotion/core'
 import { navigate } from '@reach/router'
 import styled from '@emotion/styled'
 import { Trans } from '@lingui/macro'
 import { H1 } from './components/header'
-
+import { Container } from './components/container'
+import { Steps } from './components/stepper'
 import { TrackPageViews } from './TrackPageViews'
 import { MoneyLostForm } from './forms/MoneyLostForm'
 
@@ -12,14 +13,23 @@ const CenterContent = styled('div')`
   max-width: 750px;
   margin: auto;
 `
+const topBarContainer = css`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
+  margin-bottom: 20px;
+`
 
 const submitAndNavigate = (client, data) => {
   client.writeData({ data })
-  navigate('/uploadfilesquestion')
+  navigate('/suspectinfoquestion')
 }
 
 export const MoneyLostPage = () => (
   <CenterContent>
+    <Container css={topBarContainer}>
+      <Steps activeStep={1} />
+    </Container>
     <H1>
       <Trans>Specify money lost.</Trans>
     </H1>
