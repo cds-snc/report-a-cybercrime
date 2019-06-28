23fea3a108cd80c5d36c4b1df47c7818ebfa9f7b
Keith Colbourne <keithiancolbourne@gmail.com>
2019-06-28 11:05:03 -0400

    connecting stepper

--- frontend/src/ScamInfoPage.js
+++ frontend/src/ScamInfoPage.js
@@ -1,25 +1,36 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx, css } from '@emotion/core'
 import { navigate } from '@reach/router'
 import styled from '@emotion/styled'
 import { Trans } from '@lingui/macro'
 import { H1 } from './components/header'
-
 import { TrackPageViews } from './TrackPageViews'
 import { ScamInfoForm } from './forms/ScamInfoForm'
+import { Container } from './components/container'
+import { Steps } from './components/stepper'
 
 const CenterContent = styled('div')`
   max-width: 750px;
   margin: auto;
 `
 
+const topBarContainer = css`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
+  margin-bottom: 20px;
+`
+
 const submitAndNavigate = (client, data) => {
   client.writeData({ data })
-  navigate('/suspectinfoquestion')
+  navigate('/moneylostquestion')
 }
 
 export const ScamInfoPage = () => (
   <CenterContent>
+    <Container css={topBarContainer}>
+      <Steps activeStep={0} />
+    </Container>
     <H1>
       <Trans>Describe the scam.</Trans>
     </H1>
