4fab5478f138b9f9a48b311e5243818f4a3ef443
Keith Colbourne <keithiancolbourne@gmail.com>
2019-06-28 11:05:56 -0400

    connecting stepper

--- frontend/src/Suspectinfo.js
+++ frontend/src/Suspectinfo.js
@@ -6,6 +6,7 @@ import { H1 } from './components/header'
 import { ButtonLink } from './components/link'
 import { TrackPageViews } from './TrackPageViews'
 import { Container } from './components/container'
+import { Steps } from './components/stepper'
 
 const CenterContent = styled('div')`
   max-width: 750px;
@@ -14,15 +15,25 @@ const CenterContent = styled('div')`
 
 const bottomBarContainer = css`
   display: flex;
-  width: 50%;
+  width: 90%;
   flex-direction: row;
   justify-content: space-between;
+  margin-bottom: 20px;
+`
+
+const topBarContainer = css`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
   margin-bottom: 20px;
 `
 
 export const Suspectinfo = () => (
   <CenterContent>
     <TrackPageViews />
+    <Container css={topBarContainer}>
+      <Steps activeStep={2} />
+    </Container>
     <H1 fontSize={[5, null, 6]}>
       <Trans>Do you have information about the suspect</Trans>
     </H1>
@@ -31,7 +42,7 @@ export const Suspectinfo = () => (
       <ButtonLink mb={[3, null, 5]} to="/suspectinfo">
         <Trans>Yes</Trans>
       </ButtonLink>
-      <ButtonLink mb={[3, null, 5]} to="/moneylostquestion">
+      <ButtonLink mb={[3, null, 5]} to="/uploadfilesquestion">
         <Trans>No</Trans>
       </ButtonLink>
     </Container>
