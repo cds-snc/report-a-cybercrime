ae827d087c964b1a99162c8352593f3bb16cfe01
Keith Colbourne <keithiancolbourne@gmail.com>
2019-06-28 11:04:25 -0400

    connecting stepper

--- frontend/src/LoseMoney.js
+++ frontend/src/LoseMoney.js
@@ -6,6 +6,7 @@ import { H1 } from './components/header'
 import { ButtonLink } from './components/link'
 import { TrackPageViews } from './TrackPageViews'
 import { Container } from './components/container'
+import { Steps } from './components/stepper'
 
 const CenterContent = styled('div')`
   max-width: 750px;
@@ -14,15 +15,25 @@ const CenterContent = styled('div')`
 
 const bottomBarContainer = css`
   display: flex;
-  width: 50%;
+  width: 90%;
   flex-direction: row;
   justify-content: space-between;
+  margin-bottom: 20px;
+`
+
+const topBarContainer = css`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
   margin-bottom: 20px;
 `
 
 export const LoseMoney = () => (
   <CenterContent>
     <TrackPageViews />
+    <Container css={topBarContainer}>
+      <Steps activeStep={1} />
+    </Container>
     <H1 fontSize={[5, null, 6]}>
       <Trans>Did you lose money?</Trans>
     </H1>
@@ -30,7 +41,7 @@ export const LoseMoney = () => (
       <ButtonLink mb={[3, null, 5]} to="/moneylost">
         <Trans>Yes</Trans>
       </ButtonLink>
-      <ButtonLink mb={[3, null, 5]} to="/uploadfilesquestion">
+      <ButtonLink mb={[3, null, 5]} to="/suspectinfoquestion">
         <Trans>No</Trans>
       </ButtonLink>
     </Container>
