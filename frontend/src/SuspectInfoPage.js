24dd65174f97fb1b5f4e610190ed7d2d96d9601f
Keith Colbourne <keithiancolbourne@gmail.com>
2019-06-28 11:05:18 -0400

    connecting stepper

--- frontend/src/SuspectInfoPage.js
+++ frontend/src/SuspectInfoPage.js
@@ -1,10 +1,11 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx, css } from '@emotion/core'
 import { navigate } from '@reach/router'
 import styled from '@emotion/styled'
 import { Trans } from '@lingui/macro'
 import { H1 } from './components/header'
-
+import { Container } from './components/container'
+import { Steps } from './components/stepper'
 import { TrackPageViews } from './TrackPageViews'
 import { SuspectInfoForm } from './forms/SuspectInfoForm'
 
@@ -13,13 +14,23 @@ const CenterContent = styled('div')`
   margin: auto;
 `
 
+const topBarContainer = css`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
+  margin-bottom: 20px;
+`
+
 const submitAndNavigate = (client, data) => {
   client.writeData({ data })
-  navigate('/moneylostquestion')
+  navigate('/uploadfilesquestion')
 }
 
 export const SuspectInfoPage = () => (
   <CenterContent>
+    <Container css={topBarContainer}>
+      <Steps activeStep={2} />
+    </Container>
     <H1>
       <Trans>Add scammer details.</Trans>
     </H1>
