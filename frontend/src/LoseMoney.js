91119f98071eea5e464c3563389d72712a8734df
Steve Astels <sastels@users.noreply.github.com>
2019-07-02 09:59:56 -0400

    Tweak yes no buttons (#404)

--- frontend/src/LoseMoney.js
+++ frontend/src/LoseMoney.js
@@ -14,14 +14,6 @@ const CenterContent = styled('div')`
   margin: auto;
 `
 
-const bottomBarContainer = css`
-  display: flex;
-  width: 90%;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 20px;
-`
-
 const topBarContainer = css`
   display: flex;
   width: 90%;
@@ -41,11 +33,26 @@ export const LoseMoney = () => (
     <P>
       <Trans>Did you lose money as a result of the scam?</Trans>
     </P>
-    <Container css={bottomBarContainer}>
-      <ButtonLink mb={[3, null, 5]} to="/moneylost">
+
+    <Container
+      display="flex"
+      maxWidth={['200px', null, '300px']}
+      flexDirection="row"
+      justifyContent="space-between"
+      marginBottom=""
+    >
+      <ButtonLink
+        width={['40px', null, '50px']}
+        textAlign="center"
+        to="/moneylost"
+      >
         <Trans>Yes</Trans>
       </ButtonLink>
-      <ButtonLink mb={[3, null, 5]} to="/suspectinfoquestion">
+      <ButtonLink
+        width={['40px', null, '50px']}
+        textAlign="center"
+        to="/suspectinfoquestion"
+      >
         <Trans>No</Trans>
       </ButtonLink>
     </Container>
