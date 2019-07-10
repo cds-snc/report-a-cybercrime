f8f4a90be460736cdea5dcbcc3b61b2d29154f4a
Steve Astels <sastels@gmail.com>
2019-07-10 10:23:09 -0400

    fix a few console errors

--- frontend/src/ThankYou.js
+++ frontend/src/ThankYou.js
@@ -8,7 +8,9 @@ import { TrackPageViews } from './TrackPageViews'
 import { Steps } from './components/stepper'
 import { Container } from './components/container'
 import { Layout } from './components/layout'
-// import { ButtonLink } from './components/link'
+import { Ul } from './components/unordered-list'
+import { Li } from './components/list-item'
+import { Text } from './components/text'
 
 const topBarContainer = css`
   display: flex;
@@ -46,30 +48,28 @@ export const ThankYou = () => (
     <H2 fontSize={[4, null, 5]}>
       <Trans>You may also want to:</Trans>
     </H2>
-    <P>
-      <ul>
-        <li>
+    <Text>
+      <Ul>
+        <Li>
           <Trans>
             Report directly to the website where the scam took place.
           </Trans>
-        </li>
-        <br />
-        <li>
+        </Li>
+        <Li>
           <Trans>
             Report to the financial institution where you sent money.
           </Trans>
-        </li>
-        <br />
-        <li>
+        </Li>
+        <Li>
           <Trans>
             Report to the credit bureaus,{' '}
             <A href="https://www.consumer.equifax.ca/personal/">Equifax</A> and{' '}
             <A href="https://www.transunion.ca/">TransUnion</A>, if your
             identity was stolen.
           </Trans>
-        </li>
-      </ul>
-    </P>
+        </Li>
+      </Ul>
+    </Text>
     <Container
       maxWidth="300px"
       marginTop={[2, null, 3]}
@@ -92,8 +92,6 @@ export const ThankYou = () => (
           <Trans> Report another scam</Trans>
         </ButtonLink>
       </Container>
-
-     
     </Container>
   </Layout>
 )
