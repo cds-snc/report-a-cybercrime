ccebf9f18ea131f0cfcecbae5cc0e9113ab845fe
Mike Williamson <mike.williamson@cds-snc.ca>
2019-07-11 09:41:13 -0400

    Remove size plugin (#451)
    Removing because the bundle size plugin frequently fails and blocks our builds.
    Integration has been removed in Github as well.

--- frontend/razzle.config.js
+++ frontend/razzle.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   modify: (config, { _target, _dev }, _webpack) => {
-    const SizePlugin = require('cds-size-plugin')
-    config.plugins.push(new SizePlugin())
     return config
   },
 }
