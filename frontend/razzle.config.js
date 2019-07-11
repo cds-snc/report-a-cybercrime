4429f0aebe0f89a0cdae4728538ee165152d19b7
Mike Williamson <mike.williamson@cds-snc.ca>
2019-07-11 18:05:17 -0400

    Add webpack-bundle-analyzer (#449)
    This commit adds webpack-bundle-analyzer in dev mode so that we have some
    better visibility into our bundles.
    
    After running `npm run dev` you can find the output in
    `build/bundle-analysis.html`.

--- frontend/razzle.config.js
+++ frontend/razzle.config.js
@@ -1,5 +1,15 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
 module.exports = {
-  modify: (config, { _target, _dev }, _webpack) => {
+  modify: (config, { _target, dev }, _webpack) => {
+    if (dev)
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          openAnalyzer: false,
+          reportFilename: 'bundle-analysis.html',
+        }),
+      )
     return config
   },
 }
