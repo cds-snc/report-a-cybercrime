d9bc53a9afc985b3f3f9be9039808e521ade089a
Steve Astels <sastels@users.noreply.github.com>
2019-07-24 13:34:11 -0400

    k6 script for load testing graphql api (#490)

---
+++ api/utils/loadTesting.js
@@ -0,0 +1,23 @@
+import http from 'k6/http'
+import { check } from 'k6'
+
+const checkForReportCount = r => {
+  try {
+    return JSON.parse(r.body).data.stats.reportCount >= 0
+  } catch (error) {
+    return false
+  }
+}
+
+export default function() {
+  var url = 'https://www.report-a-cybercrime.alpha.rcmp-grc.gc.ca/graphql'
+  var payload = JSON.stringify({ query: '{ stats { reportCount } }' })
+  var params = { headers: { 'Content-Type': 'application/json' } }
+  var res = http.post(url, payload, params)
+
+  check(res, {
+    'status was 200': r => r.status == 200,
+    'transaction time OK': r => r.timings.duration < 1000,
+    'api call successful': r => checkForReportCount(r),
+  })
+}
