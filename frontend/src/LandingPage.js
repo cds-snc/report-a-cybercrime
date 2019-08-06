7d7dd2b0c70ad29d8274ab2e91921de8ea301595
Steve Astels <sastels@gmail.com>
2019-08-06 14:44:01 -0400

    fix header back

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -15,7 +15,7 @@ export const LandingPage = () => (
   <Layout>
     <TrackPageViews />
     <H1>
-      <Trans>Report a scam - new!</Trans>
+      <Trans>Report a scam</Trans>
     </H1>
     <ApolloConsumer>
       {client =>
