a940c229a8eb4c52137d055da39a7f0af5b85efc
Steve Astels <sastels@users.noreply.github.com>
2019-08-09 13:08:16 -0400

    Root landing page (#541)

---
+++ frontend/src/RootLandingPage.js
@@ -0,0 +1,23 @@
+/**@jsx jsx */
+import { jsx } from '@emotion/core'
+import { Trans } from '@lingui/macro'
+import { ButtonLink } from './components/link'
+import { Container } from './components/container'
+import { Layout } from './components/layout'
+
+export const RootLandingPage = () => (
+  <Layout maxWidth={['300px', null, '400px']}>
+    <Container
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-between"
+    >
+      <ButtonLink width={['40px', null, '50px']} textAlign="center" to="/p1">
+        <Trans>Prototype 1</Trans>
+      </ButtonLink>
+      <ButtonLink width={['40px', null, '50px']} textAlign="center" to="/p2">
+        <Trans>Prototype 2</Trans>
+      </ButtonLink>
+    </Container>
+  </Layout>
+)
