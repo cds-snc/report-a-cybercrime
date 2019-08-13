f36497ee388e74f99e337aabcb2ca0d855a11a3d
Steve Astels <sastels@gmail.com>
2019-08-13 15:11:24 -0400

    translations

---
+++ frontend/utils/linguiHelper.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const readline = require('readline')
+
+console.log(
+  'This outputs the lines of locale/fr/messages.json that require a translation. You can paster the lines into your favourite tranlation site, then paste in the results, and get lines to append to the messages.json file',
+)
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('locale/fr/messages.json'),
+  crlfDelay: Infinity,
+})
+
+lines = []
+rl.on('line', line => {
+  if (line.includes('""')) {
+    let match = line.match(/".+":/)[0]
+    match = match.slice(1, -2)
+    console.log(match)
+    lines.push(match)
+  }
+})
+
+var stdin = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+})
+
+index = 0
+stdin.on('line', function(translation) {
+  if (index == 0) {
+    console.log('')
+  }
+  console.log(`"${lines[index]}": "${translation}",`)
+  index++
+})
