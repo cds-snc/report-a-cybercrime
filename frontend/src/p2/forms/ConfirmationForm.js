c186cfcc57504b3b6657e1e53c1809200f9c4651
Steve Astels <sastels@gmail.com>
2019-08-14 15:47:54 -0400

    small tweak to function

--- frontend/src/p2/forms/ConfirmationForm.js
+++ frontend/src/p2/forms/ConfirmationForm.js
@@ -19,78 +19,83 @@ import {
 
 const TextAreaAdapter = finalFormAdapter(TextArea)
 
-export const ConfirmationForm = props => (
-  <React.Fragment>
-    <H2 marginTop={[4, null, 5]}>
-      <Trans>Tell us more</Trans>
-    </H2>
-    <ApolloConsumer>
-      {client => (
-        <Mutation mutation={SUBMIT_REPORT_MUTATION}>
-          {submitReport => (
-            <Form
-              initialValues={getTellUsMore(client)}
-              onSubmit={data => props.onSubmit(client, submitReport, data)}
-              render={({ handleSubmit }) => (
-                <form onSubmit={handleSubmit}>
-                  <label htmlFor="tellUsMore">
-                    <Text marginTop={[1, null, 2]} marginBottom={[1, null, 2]}>
-                      <Trans>
-                        Is there information you’d like to add that didn’t fit
-                        anywhere else?
-                      </Trans>
-                    </Text>
-                  </label>
-                  <div>
-                    <Field
-                      name="tellUsMore"
-                      id="tellUsMore"
-                      component={TextAreaAdapter}
-                      height="200px"
-                    />
-                  </div>
+export const ConfirmationForm = props => {
+  return (
+    <React.Fragment>
+      <H2 marginTop={[4, null, 5]}>
+        <Trans>Tell us more</Trans>
+      </H2>
+      <ApolloConsumer>
+        {client => (
+          <Mutation mutation={SUBMIT_REPORT_MUTATION}>
+            {submitReport => (
+              <Form
+                initialValues={getTellUsMore(client)}
+                onSubmit={data => props.onSubmit(client, submitReport, data)}
+                render={({ handleSubmit }) => (
+                  <form onSubmit={handleSubmit}>
+                    <label htmlFor="tellUsMore">
+                      <Text
+                        marginTop={[1, null, 2]}
+                        marginBottom={[1, null, 2]}
+                      >
+                        <Trans>
+                          Is there information you’d like to add that didn’t fit
+                          anywhere else?
+                        </Trans>
+                      </Text>
+                    </label>
+                    <div>
+                      <Field
+                        name="tellUsMore"
+                        id="tellUsMore"
+                        component={TextAreaAdapter}
+                        height="200px"
+                      />
+                    </div>
 
-                  <Container
-                    width="305px"
-                    marginTop={[1, null, 1]}
-                    css={css`
-                      display: flex;
-                      flex-direction: column;
-                      justify-content: space-between;
-                    `}
-                  >
-                    <Button type="submit">
-                      <Trans>Submit Report</Trans>
-                    </Button>
-                  </Container>
-
-                  <Container
-                    width="300px"
-                    marginTop={[1, null, 1]}
-                    css={css`
-                      display: flex;
-                      flex-direction: column;
-                      justify-content: space-between;
-                    `}
-                  >
-                    <Link
-                      type="button"
-                      color="black"
-                      to="/p2"
-                      textAlign="center"
+                    <Container
+                      width="305px"
+                      marginTop={[1, null, 1]}
+                      css={css`
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: space-between;
+                      `}
                     >
-                      <Trans>Cancel report</Trans>
-                    </Link>
-                  </Container>
-                </form>
-              )}
-            />
-          )}
-        </Mutation>
-      )}
-    </ApolloConsumer>
-  </React.Fragment>
-)
+                      <Button type="submit">
+                        <Trans>Submit Report</Trans>
+                      </Button>
+                    </Container>
+
+                    <Container
+                      width="300px"
+                      marginTop={[1, null, 1]}
+                      css={css`
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: space-between;
+                      `}
+                    >
+                      <Link
+                        type="button"
+                        color="black"
+                        to="/p2"
+                        textAlign="center"
+                      >
+                        <Trans>Cancel report</Trans>
+                      </Link>
+                    </Container>
+                  </form>
+                )}
+              />
+            )}
+          </Mutation>
+        )}
+      </ApolloConsumer>
+    </React.Fragment>
+  )
+}
 
 ConfirmationForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
