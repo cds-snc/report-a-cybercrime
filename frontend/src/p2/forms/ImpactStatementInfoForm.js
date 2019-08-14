7db38e9dec930f8d1505d841dde545c84188b5d7
Deyuan Liu <deyuanliu@Deyuans-MacBook-Pro.local>
2019-08-14 12:08:12 -0400

    fixed 'build fail lint" issue

--- frontend/src/p2/forms/ImpactStatementInfoForm.js
+++ frontend/src/p2/forms/ImpactStatementInfoForm.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { css, jsx } from '@emotion/core'
 import styled from '@emotion/styled'
@@ -111,9 +111,9 @@ export const ImpactStatementInfoForm = props => (
                     <Trans>Describe the extent of the damage.</Trans>
                 </H3>
 <ul>
-    <li>>What did you lose? </li>
-    <li>>What personal information was compromised?</li>
-    <li>>How much money was involved? </li>
+    <li>What did you lose? </li>
+    <li>What personal information was compromised?</li>
+    <li>How much money was involved? </li>
 </ul>
 
          <div>
