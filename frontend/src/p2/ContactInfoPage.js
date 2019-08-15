bde3a2c5bb888c2e779e65bb56bf27fcf49ea341
Daniel Tse <danprime@gmail.com>
2019-08-15 15:29:57 -0400

    Contact Info Page for P2 (#549)
    * Front end according to card/wireframe.
    * Kept the P1 components and paths.

---
+++ frontend/src/p2/ContactInfoPage.js
@@ -0,0 +1,44 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { navigate } from '@reach/router'
+import { Trans } from '@lingui/macro'
+import { H1, H2 } from '../components/header'
+import { P } from '../components/paragraph'
+import { Steps } from '../components/stepper'
+import { Container } from '../components/container'
+import { TrackPageViews } from '../TrackPageViews'
+import { ContactInfoForm } from './forms/ContactInfoForm'
+import { Layout } from '../components/layout'
+
+const submitAndNavigate = (client, data) => {
+  client.writeData({ data: { contactInfo: JSON.stringify(data) } })
+  navigate('confirmation')
+}
+
+export const ContactInfoPage = () => (
+  <Layout>
+    <Container
+      display="flex"
+      width="90%"
+      flexDirection="row"
+      marginBottom="20px"
+    >
+      <Steps activeStep={4} steps={[{}, {}, {}, {}, {}]} />
+    </Container>
+    <H1>
+      <Trans>Leave your contact information</Trans>
+    </H1>
+    <H2>
+      <Trans>Things will get better</Trans>
+    </H2>
+    <P>
+      <Trans>
+        Please provide your contact details so we can get in touch with you in
+        case we need to check any details with you in the future. We don’t get
+        in contact with everyone who submits a report.
+      </Trans>
+    </P>
+    <TrackPageViews />
+    <ContactInfoForm onSubmit={submitAndNavigate} />
+  </Layout>
+)
