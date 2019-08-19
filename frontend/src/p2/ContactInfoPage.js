b05033d39a5625eb1eeb0400b2c28030f6a7dab3
Anik Brazeau <anik.brazeau@cds-snc.ca>
2019-08-19 16:09:38 -0400

    Make description on contact info page more concise

--- frontend/src/p2/ContactInfoPage.js
+++ frontend/src/p2/ContactInfoPage.js
@@ -29,14 +29,11 @@ export const ContactInfoPage = () => (
     <H1>
       <Trans>Leave your contact information</Trans>
     </H1>
-    <H2>
-      <Trans>Things will get better</Trans>
-    </H2>
     <P>
       <Trans>
         Please provide your contact details so we can get in touch with you in
-        case we need to check any details with you in the future. We don’t get
-        in contact with everyone who submits a report.
+        case we need to check any information. We don’t follow up with everyone
+        who submits a report.
       </Trans>
     </P>
     <TrackPageViews />
