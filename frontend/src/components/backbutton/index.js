605e6dd535f33ad7ca3e359c1c052328c628f69b
nmakuch <makuch.nick@gmail.com>
2019-08-22 14:37:14 -0400

    removed default styling for back link, added more padding before underline

--- frontend/src/components/backbutton/index.js
+++ frontend/src/components/backbutton/index.js
@@ -1,24 +1,31 @@
 /** @jsx jsx **/
-import { Container } from '../container'
 import { jsx, css } from '@emotion/core'
 import { Link } from '../link'
 import { Trans } from '@lingui/macro'
 import PropTypes from 'prop-types'
 
 export const BackButton = ({ route = '' }) => (
-  <Container
-    width="105px"
-    marginTop={[1, null, 1]}
+  <div
     css={css`
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
+      margin-bottom: 1.5rem;
+
+      a {
+        text-decoration: none;
+        border-bottom: 1px solid;
+        padding-bottom: 3px;
+
+        span {
+          font-size: 0.9rem;
+        }
+      }
     `}
   >
     <Link type="button" to={route} textAlign="left">
-      <Trans>&#9664; Back </Trans>
+      <Trans>
+        <span>&#9664;</span> Back{' '}
+      </Trans>
     </Link>
-  </Container>
+  </div>
 )
 
 BackButton.propTypes = {
