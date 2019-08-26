caec0938de445cd778c11f9d2444e8f860ccce97
Keith Colbourne <keithiancolbourne@gmail.com>
2019-08-26 11:01:32 -0400

    Azure pipelining

--- frontend/src/RootLandingPage.js
+++ frontend/src/RootLandingPage.js
@@ -13,7 +13,7 @@ export const RootLandingPage = () => (
       justifyContent="space-between"
     >
       <ButtonLink width={['40px', null, '50px']} textAlign="center" to="/p1">
-        <Trans>AZURE TEST Prototype 1</Trans>
+        AZURE TEST <Trans>Prototype 1</Trans>
       </ButtonLink>
       <ButtonLink width={['40px', null, '50px']} textAlign="center" to="/p2">
         <Trans>Prototype 2</Trans>
