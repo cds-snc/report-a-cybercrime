833dbf8ca2d52dec35555a57b46d87e6aef92994
Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>
2019-08-29 19:15:38 -0400

    Content refinements (#667)
    * Softer language in instruction
    
    * Emphasis that these are crimes
    
    * Stronger instructive label
    
    * Stronger label
    
    * Change text box size on impact form to make less prominent
    
    * Change text box size to make less prominent on confirmation page
    
    * add space / line break
    
    * français
    
    * add bullet to cover how people were contacted
    
    * Change language around next steps, guidance and support
    
    * More specific about how affected
    
    * Test label to match text on the page
    
    * French string was in English
    
    * French strings
    
    * Linting
    
    * Remove guidance/expectation from P1
    
    * français
    
    * Microcopy edit
    
    * Remove expectation-setting messaging from P1
    
    * Remove unused components and guidance on final page
    
    * microcopy edit
    
    * French strings
    
    * linting
    
    * missing translations
    
    * French button correction

--- frontend/src/p1/ThankYou.js
+++ frontend/src/p1/ThankYou.js
@@ -1,15 +1,11 @@
 /**@jsx jsx */
 import { jsx } from '@emotion/core'
 import { Trans } from '@lingui/macro'
-import { H1, H2 } from '../components/header'
+import { H1 } from '../components/header'
 import { P } from '../components/paragraph'
-import { A } from '../components/link'
 import { TrackPageViews } from '../TrackPageViews'
 import { ButtonsContainer } from '../components/buttons-container'
 import { Layout } from '../components/layout'
-import { Ul } from '../components/unordered-list'
-import { Li } from '../components/list-item'
-import { Text } from '../components/text'
 
 export const ThankYou = () => (
   <Layout>
@@ -25,35 +21,10 @@ export const ThankYou = () => (
     </P>
     <P>
       <Trans>
-        An analyst will now assess the information and combine it with other
-        reports to help police catch scammers.
+        An analyst will assess the information and combine it with other reports
+        to help police catch scammers.
       </Trans>
     </P>
-    <H2 fontSize={[4, null, 5]}>
-      <Trans>You may also want to:</Trans>
-    </H2>
-    <Text>
-      <Ul>
-        <Li>
-          <Trans>
-            Report directly to the website where the scam took place.
-          </Trans>
-        </Li>
-        <Li>
-          <Trans>
-            Report to the financial institution where you sent money.
-          </Trans>
-        </Li>
-        <Li>
-          <Trans>
-            Report to the credit bureaus,{' '}
-            <A href="https://www.consumer.equifax.ca/personal/">Equifax</A> and{' '}
-            <A href="https://www.transunion.ca/">TransUnion</A>, if your
-            identity was stolen.
-          </Trans>
-        </Li>
-      </Ul>
-    </Text>
     <ButtonsContainer buttonLink={true} cancel={false} route="/p1/scaminfo" />
   </Layout>
 )
