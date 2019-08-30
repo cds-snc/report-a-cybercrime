3bd4e9942156ecc193cb36cd1e91c2b92bb135e1
Anik Brazeau <anik.brazeau@cds-snc.ca>
2019-08-30 10:35:45 -0400

    Add reference number to P1 and NC3 link

--- frontend/src/p1/ThankYou.js
+++ frontend/src/p1/ThankYou.js
@@ -3,6 +3,7 @@ import { jsx } from '@emotion/core'
 import { Trans } from '@lingui/macro'
 import { H1 } from '../components/header'
 import { P } from '../components/paragraph'
+import { A } from '../components/link'
 import { TrackPageViews } from '../TrackPageViews'
 import { ButtonsContainer } from '../components/buttons-container'
 import { Layout } from '../components/layout'
@@ -15,14 +16,18 @@ export const ThankYou = () => (
     </H1>
     <P>
       <Trans>
-        The RCMP&apos;s National Cybercrime Coordination Unit has received your
-        report.
+        The{' '}
+        <A href="http://www.rcmp-grc.gc.ca/en/the-national-cybercrime-coordination-unit-nc3">
+          RCMP&apos;s National Cybercrime Coordination Unit
+        </A>{' '}
+        will assess the information and combine it with other reports to help
+        police catch scammers.
       </Trans>
     </P>
     <P>
       <Trans>
-        An analyst will assess the information and combine it with other reports
-        to help police catch scammers.
+        Your reference number is <strong>#NC300234234</strong>. Keep this number
+        for your records or to add more information later.
       </Trans>
     </P>
     <ButtonsContainer buttonLink={true} cancel={false} route="/p1/scaminfo" />
