46c5984728c17e682e89a89008a95fc26160e112
Mike Williamson <mike.williamson@cds-snc.ca>
2019-08-30 18:48:39 -0400

    Replace jest-dom (#671)
    This package has been replaced with @testing-library/jest-dom as per
    https://github.com/testing-library/dom-testing-library/issues/260

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,5 +1,5 @@
 import '@babel/polyfill'
-import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { matchers } from 'jest-emotion'
