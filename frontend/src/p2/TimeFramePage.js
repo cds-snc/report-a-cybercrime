391b1592c813cf12a4c2cd9fa8f919040aa12bc6
Deyuan Liu <deyuan.liu@cds-snc.ca>
2019-09-04 10:00:29 -0400

    fix lint issue

--- frontend/src/p2/TimeFramePage.js
+++ frontend/src/p2/TimeFramePage.js
@@ -7,7 +7,6 @@ import { Layout } from '../components/layout'
 import { TimeFrameInfoForm } from './forms/TimeFrameInfoForm'
 import { TrackPageViews } from '../TrackPageViews'
 import { getDoneForms } from '../utils/queriesAndMutations'
-import { Container } from '../components/container'
 import { BackButton } from '../components/backbutton'
 import { Steps } from '../components/stepper'
 
@@ -22,11 +21,6 @@ export const TimeFramePage = () => (
       <Trans>the start page</Trans>
     </BackButton>
     <Steps activeStep={1} totalSteps={6} />
-
-
-    {/* <Link type="button" color="black" to="/p2/" textAlign="center">
-                <Trans>Cancel report</Trans>
-              </Link> */}
 
     <H1>
       <Trans>When did the scam happen?</Trans>
