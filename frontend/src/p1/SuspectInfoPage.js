bf670dd5f7600f4083d9022c9ed434b45fd7751f
Anik Brazeau <anik.brazeau@cds-snc.ca>
2019-09-04 14:32:47 -0400

    Remove redundant question on yes/no page

--- frontend/src/p1/SuspectInfoPage.js
+++ frontend/src/p1/SuspectInfoPage.js
@@ -23,14 +23,8 @@ export const SuspectInfoPage = () => (
     </BackButton>
     <Steps activeStep={3} totalSteps={6} />
     <H1>
-      <Trans>Add suspect clues</Trans>
+      <Trans>Do you have any information on where the scam came from?</Trans>
     </H1>
-    <P>
-      <Trans>
-        Any information that was used by the scammer can help police identify
-        suspects.
-      </Trans>
-    </P>
     <TrackPageViews />
     <SuspectInfoForm onSubmit={submitAndNavigate} />
   </Layout>
