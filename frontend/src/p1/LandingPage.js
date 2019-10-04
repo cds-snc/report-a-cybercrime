165eb2b31f7521a69e845b80e73c454740799467
Keith Colbourne <keithiancolbourne@gmail.com>
2019-10-04 11:35:17 -0400

    testing

--- frontend/src/p1/LandingPage.js
+++ frontend/src/p1/LandingPage.js
@@ -42,7 +42,7 @@ export const LandingPage = props => {
 
       <P>
         <Trans>
-          Tell the RCMP's National Cybercrime Coordination Unit about a scam to
+          Tell the RCMP's Nationall Cybercrime Coordination Unit about a scam to
           help police across Canada investigate these crimes.
         </Trans>
       </P>
