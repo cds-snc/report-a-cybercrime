911abdf7265608a3e4a171fa4ab20a55d2af36c3
Steve Astels <sastels@users.noreply.github.com>
2019-10-23 16:04:15 -0400

    use a darker blue (#840)

--- frontend/src/theme/index.js
+++ frontend/src/theme/index.js
@@ -28,7 +28,7 @@ export const BREAKPOINTS = ['320px', '641px', '769px']
 const colors = {
   white: '#FFF',
   black: '#000',
-  blue: '#005ea5',
+  blue: '#003a66',
   lightBlue: '#2b8cc4',
   purple: '#4c2c92',
   yellow: '#ffbf47',
