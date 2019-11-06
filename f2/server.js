c85b37a7897b1d378729a191a365ffedd056e244
Steve Astels <sastels@gmail.com>
2019-11-06 15:52:09 -0500

    send ping data to cosmosdb

--- f2/server.js
+++ f2/server.js
@@ -1,10 +1,35 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+require('dotenv').config()
 const app = express()
+
+const MongoClient = require('mongodb').MongoClient
+
+const dbName = process.env.COSMOSDB_NAME
+const dbKey = process.env.COSMOSDB_KEY
+
+const url = `mongodb://${dbName}:${dbKey}@${dbName}.documents.azure.com:10255/mean-dev?ssl=true&sslverifycertificate=false`
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err
+  console.log('Connected to db!')
+  db.close()
+})
+
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/ping', function(req, res) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err
+    var dbo = db.db('mydb')
+    var myobj = { got: 'ping', sent: 'pong' }
+    dbo.collection('customers').insertOne(myobj, function(err, res) {
+      if (err) throw err
+      console.log('1 document inserted')
+      db.close()
+    })
+  })
   return res.send('pong')
 })
 
