9b2784e40c9c891f476fd0e9ffa075b18c30604c
Philippe Caron <amazingphilippe@gmail.com>
2019-11-19 11:18:29 -0500

    next and cancel buttons + container as chakra box

--- f2/src/components/container/index.js
+++ f2/src/components/container/index.js
@@ -1,66 +1,9 @@
 /** @jsx jsx **/
-import styled from '@emotion/styled'
 import { jsx } from '@emotion/core'
 
-import {
-  alignItems,
-  alignContent,
-  justifyContent,
-  flexWrap,
-  flexBasis,
-  flexDirection,
-  width,
-  maxWidth,
-  height,
-  maxHeight,
-  color,
-  borders,
-  borderColor,
-  flex,
-  justifySelf,
-  display,
-  alignSelf,
-  space,
-  textAlign,
-  position,
-  top,
-  right,
-  bottom,
-  left,
-  zIndex,
-} from 'styled-system'
-import { cleanProps } from '../../utils/cleanProps'
 import { Box } from '@chakra-ui/core'
 
-export const Container = styled('div', {
-  shouldForwardProp: prop => cleanProps(prop),
-})`
-  ${alignItems}
-  ${alignContent}
-  ${justifyContent}
-  ${flex}
-  ${flexWrap}
-  ${flexBasis}
-  ${flexDirection}
-  ${width}
-  ${maxWidth}
-  ${height}
-  ${maxHeight}
-  ${color}
-  ${display}
-  ${space}
-  ${justifySelf}
-  ${alignSelf}
-  ${borders}
-  ${borderColor}
-  ${textAlign}
-  ${position}
-  ${top}
-  ${bottom}
-  ${left}
-  ${right}
-  ${zIndex}
-`
+export const Container = props => <Box {...props} />
 
 export const InfoCard = props => (
   <Box
---
+++ f2/src/components/next-and-cancel-buttons/index.js
@@ -0,0 +1,25 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { Button } from '../button'
+import { Trans } from '@lingui/macro'
+import { Link } from '../link'
+import PropTypes from 'prop-types'
+import { Link, Flex } from '@chakra-ui/core'
+
+export const NextAndCancelButtons = ({ cancelRoute = '/', ...props }) => (
+  <Flex direction="row" align="center">
+    <Button type="submit" rightIcon="chevron-right" variantColor="green">
+      {props.children}
+    </Button>
+    <Box ml={4}>
+      <Link type="button" color="black" to={cancelRoute} textAlign="center">
+        <Trans id="button.cancelReport" />
+      </Link>
+    </Box>
+  </Flex>
+)
+
+NextAndCancelButtons.propTypes = {
+  cancelRoute: PropTypes.string,
+  children: PropTypes.any,
+}
