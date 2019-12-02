834bbbc88e1b84e82500f46067991d391dbabe29
Steve Astels <sastels@gmail.com>
2019-12-02 14:33:16 -0500

    fix linting

--- f2/src/components/button/__tests__/Button.test.js
+++ f2/src/components/button/__tests__/Button.test.js
@@ -4,20 +4,14 @@ import { ThemeProvider } from 'emotion-theming'
 import canada from '../../../theme/canada'
 import { Button } from '../'
 
-import chroma from 'chroma-js'
-
 describe('<Button />', () => {
   afterEach(cleanup)
 
-  it('passes WCAG color contrast', () => {
-    const { getByText } = render(
+  it('renders', () => {
+    render(
       <ThemeProvider theme={canada}>
         <Button>foo</Button>
       </ThemeProvider>,
     )
-
-    //console.log(getByText(/foo/).getAttribute('color'));
-
-    expect(chroma.contrast('black', 'white')).toBeGreaterThan(4.5)
   })
 })
