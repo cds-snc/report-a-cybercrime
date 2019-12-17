190320e7708a7d74f8694ada559ad2cd6dc59666
Philippe Caron <amazingphilippe@gmail.com>
2019-12-17 14:17:18 -0500

    linted the thing

--- f2/src/components/footer/__tests__/Footer.test.js
+++ f2/src/components/footer/__tests__/Footer.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Footer } from '../'
-import canada from '../../../theme/canada'
 import { render, cleanup } from '@testing-library/react'
 
 describe('<Footer />', () => {
