7beeef518ef978ca9fb32cae0453891f5aafdf20
Kevin Yang <kevinyang@Kevins-RCMP-MacBook-Pro.local>
2019-12-20 13:11:52 -0500

    added title

---
+++ f2/src/LocationPage.js
@@ -0,0 +1,48 @@
+import { Route } from 'react-router-dom'
+import React from 'react'
+import { Trans } from '@lingui/macro'
+import { H1 } from './components/header'
+import { P } from './components/paragraph'
+import { Layout } from './components/layout'
+import { TimeFrameInfoForm } from './forms/TimeFrameInfoForm'
+import { TrackPageViews } from './TrackPageViews'
+import { getDoneForms } from './utils/queriesAndMutations'
+import { BackButton } from './components/backbutton'
+import { Steps } from './components/stepper'
+import { Stack } from '@chakra-ui/core'
+
+export const LocationPage = () => (
+    <Route
+        render={({ history }) => (
+            <Layout>
+                <TrackPageViews />
+
+                <Stack spacing={10} shouldWrapChildren>
+                    <BackButton route="/">
+                        <Trans id="timeFramePage.backButton" />
+                    </BackButton>
+
+                    <Stack spacing={4} role="heading" aria-level="1">
+                        <Steps activeStep={1} totalSteps={6} />
+                        <H1 as="span">
+                            <Trans id="locationPage.title" />
+                        </H1>
+                    </Stack>
+
+                    <P>
+                        <Trans id="timeFramePage.intro" />
+                    </P>
+
+                    <TimeFrameInfoForm
+                        onSubmit={(client, data) => {
+                            client.writeData({ data: { timeFrame: JSON.stringify(data) } })
+                            history.push(
+                                getDoneForms(client) ? '/confirmation' : '/whathappened',
+                            )
+                        }}
+                    />
+                </Stack>
+            </Layout>
+        )}
+    />
+)
