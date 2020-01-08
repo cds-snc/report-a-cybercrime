0abb514daf0e8247b921d75b2adec155a1cd93a1
Steve Astels <sastels@gmail.com>
2020-01-08 15:13:30 -0500

    add WhatWasAffectedPage

---
+++ f2/src/WhatWasAffected.js
@@ -0,0 +1,55 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import { Route } from 'react-router-dom'
+import React from 'react'
+import { Trans } from '@lingui/macro'
+import { H1 } from './components/header'
+import { P } from './components/paragraph'
+import { Layout } from './components/layout'
+import { Steps } from './components/stepper'
+import { WhatWasAffectedForm } from './forms/WhatWasAffectedForm'
+import { TrackPageViews } from './TrackPageViews'
+import { BackButton } from './components/backbutton'
+import { Stack } from '@chakra-ui/core'
+import { useStateValue } from './utils/state'
+
+export const WhatWasAffectedPage = () => {
+  const [data, dispatch] = useStateValue()
+  const { doneForms } = data
+
+  return (
+    <Route
+      render={({ history }) => (
+        <Layout>
+          <TrackPageViews />
+          <Stack spacing={10} shouldWrapChildren>
+            <BackButton route="/howdiditstart">
+              <Trans id="whatWasAffectedPage.backButton" />
+            </BackButton>
+
+            <Stack spacing={4} role="heading" aria-level="1">
+              <Steps activeStep={4} totalSteps={6} />
+              <H1 as="span">
+                <Trans id="whatWasAffectedPage.title" />
+              </H1>
+            </Stack>
+
+            <P>
+              <Trans id="whatWasAffectedPage.intro" />
+            </P>
+
+            <WhatWasAffectedForm
+              onSubmit={data => {
+                dispatch({
+                  type: 'saveFormData',
+                  data: { whatWasAffected: data },
+                })
+                history.push(doneForms ? '/confirmation' : '/contactinfo')
+              }}
+            />
+          </Stack>
+        </Layout>
+      )}
+    />
+  )
+}
