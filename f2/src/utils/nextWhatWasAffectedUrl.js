00964d7a64e952a6bdd9320f0bc1f037a849040b
Steve Astels <sastels@gmail.com>
2020-01-09 16:50:47 -0500

    fix linting

--- f2/src/utils/nextWhatWasAffectedUrl.js
+++ f2/src/utils/nextWhatWasAffectedUrl.js
@@ -2,13 +2,13 @@ import { whatWasAffectedPages } from '../forms/WhatWasAffectedForm'
 
 export const nextWhatWasAffectedUrl = (optionsSelected, currentPage) => {
   let pages = whatWasAffectedPages.filter(
-    page => page.url != '' && optionsSelected.indexOf(page.key) > -1,
+    page => page.url !== '' && optionsSelected.indexOf(page.key) > -1,
   )
   pages.unshift({ key: '', url: 'whatwasaffected' }) // start of list
   pages.push({ key: '', url: 'whathappened' }) // end of list
 
   for (const [index, page] of pages.entries()) {
-    if (page.url == currentPage) {
+    if (page.url === currentPage) {
       return pages[Math.min(index + 1, pages.length)].url
     }
   }
