26c865970fbcdb174b243bcaed37be4af7ffe2fc
Khalid El Aggan <khalid.elaggan@cds-snc.ca>
2020-01-13 13:31:48 -0500

    Added the missing forms

--- f2/src/forms/HowDidItStartForm.js
+++ f2/src/forms/HowDidItStartForm.js
@@ -5,11 +5,13 @@ import { Trans } from '@lingui/macro'
 import { Form, Field, useField } from 'react-final-form'
 import { NextAndCancelButtons } from '../components/next-and-cancel-buttons'
 import { Checkbox } from '../components/checkbox'
+import { Radio } from '../components/radio'
 import { FormControl, Stack, Box } from '@chakra-ui/core'
 import { FormHelperText } from '../components/FormHelperText'
 import { TextArea } from '../components/text-area'
 import { useStateValue } from '../utils/state'
 import { FormLabel } from '../components/FormLabel'
+import { P } from '../components/paragraph'
 import { ConditionalForm } from '../components/container'
 
 const Control = ({ name, ...rest }) => {
@@ -36,6 +38,23 @@ const CheckboxArrayControl = ({ name, value, defaultIsChecked, children }) => {
   )
 }
 
+const RadioButtonArrayControl = ({ name, value, defaultIsChecked, children }) => {
+  const {
+    input: { checked, ...input },
+    meta: { error, touched },
+  } = useField(name, {
+    type: 'radio',
+    value,
+    defaultIsChecked,
+  })
+
+  return (
+    <Radio {...input} isChecked={checked} isInvalid={error && touched}>
+      {children}
+    </Radio>
+  )
+}
+
 const validate = () => {
   return {}
 }
@@ -57,21 +76,41 @@ export const HowDidItStartForm = props => {
    { name:"others", channel:"howDidTheyReachYou.others", label: "howDidTheyReachYouLabel.question5", hint: "howDidTheyReachYouLabel.hint5"}
   ];
 
+  const timeline = {
+    whenDidItStart: [],
+    ...data.formData.timeline,
+  }
+
+  const whenDidItStart = [
+    'whenDidItStart.today',
+    'whenDidItStart.pastWeek',
+    'whenDidItStart.pastMonth',
+    'whenDidItStart.pastYear',
+    'whenDidItStart.moreThanOneYear',
+  ]
+
+  const recurrenceCheck = {
+    howManyTimes: [],
+    ...data.formData.timeline,
+  }
+
+  const howManyTimes = [
+    'howManyTimes.once',
+    'howManyTimes.severalTimes',
+  ]
+
   return (
     <React.Fragment>
+      {false ? ( // mark ids for lingui
+        <div>
+          <Trans id="whenDidItStart.today" />
+          <Trans id="whenDidItStart.pastWeek" />
+          <Trans id="whenDidItStart.pastMonth" />
+          <Trans id="whenDidItStart.pastYear" />
+          <Trans id="whenDidItStart.moreThanOneYear" />
+        </div>
+      ) : null}
 
-    {/*
-          {false ? (
-            <div>
-              //TODO: mark the proper ids for lingui
-              <Trans id="howDidTheyReachYou.email" />
-              <Trans id="howDidTheyReachYou.phone" />
-              <Trans id="howDidTheyReachYou.online" />
-              <Trans id="howDidTheyReachYou.app" />
-              <Trans id="howDidTheyReachYou.others" />
-            </div>
-          ) : null}
-    */}
       <Form
         initialValues={channel}
         onSubmit={data => props.onSubmit(data)}
@@ -127,10 +166,53 @@ export const HowDidItStartForm = props => {
               </Stack>
             </Control>
 
+            <Control as="fieldset" name="whenDidItStart">
+              <FormLabel as="legend" htmlFor="whenDidItStart" mb={2}>
+                <Trans id="whenDidItStart.label" />
+              </FormLabel>
+              <Stack spacing={4} shouldWrapChildren>
+                {whenDidItStart.map(key => {
+                  return (
+                    <Box key={key}>
+                      <RadioButtonArrayControl
+                        name="whenDidItStart"
+                        value={key}
+                        isChecked={timeline.whenDidItStart.includes(key)}
+                      >
+                        {i18n._(key)}
+                      </RadioButtonArrayControl>
+                    </Box>
+                  )
+                })}
+              </Stack>
+            </Control>
+
+            <Control as="fieldset" name="howManyTimes">
+              <FormLabel as="legend" htmlFor="howManyTimes" mb={2}>
+                <Trans id="howManyTimes.label" />
+              </FormLabel>
+              <Stack spacing={4} shouldWrapChildren>
+                {howManyTimes.map(key => {
+                  return (
+                    <Box key={key}>
+                      <RadioButtonArrayControl
+                        name="howManyTimes"
+                        value={key}
+                        isChecked={recurrenceCheck.howManyTimes.includes(key)}
+                      >
+                        {i18n._(key)}
+                      </RadioButtonArrayControl>
+                    </Box>
+                  )
+                })}
+              </Stack>
+            </Control>
+            <P>
+              <Trans id="businessInfoPage.nextPage" />
+            </P>
+
             <NextAndCancelButtons>
-              <Trans id="howDidItStartPage.nextButton">
-                Next: ??
-              </Trans>
+              <Trans id="howDidItStartPage.nextButton" />
             </NextAndCancelButtons>
           </Stack>
         )}
