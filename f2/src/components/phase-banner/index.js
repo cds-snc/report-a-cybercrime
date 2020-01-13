5be284e1d176ba1f4294bf2cd4f2962d1100efd5
Philippe Caron <amazingphilippe@gmail.com>
2020-01-13 15:27:55 -0500

    fixes tag width on small screens

--- f2/src/components/phase-banner/index.js
+++ f2/src/components/phase-banner/index.js
@@ -5,11 +5,23 @@ import { Flex, Tag } from '@chakra-ui/core'
 import { Text } from '../text'
 
 export const PhaseBanner = props => (
-  <Flex align="center" p={4} bg="green.50">
-    <Tag variantColor="green">{props.phase}</Tag>
-    <Text color="green.900" ml={4}>
-      {props.children}
-    </Text>
+  <Flex
+    align="center"
+    p={4}
+    wrap={{ base: 'wrap', sm: 'nowrap' }}
+    bg="green.50"
+  >
+    <Tag
+      rounded="none"
+      variantColor="green"
+      variant="solid"
+      minW="inherit"
+      mr={4}
+      mb={{ base: 4, sm: 0 }}
+    >
+      {props.phase}
+    </Tag>
+    <Text color="green.900">{props.children}</Text>
   </Flex>
 )
 
