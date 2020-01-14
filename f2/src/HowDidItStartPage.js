a0f055c50564fa260deca926895922586f9a067e
Khalid El Aggan <khalid.elaggan@cds-snc.ca>
2020-01-14 11:31:50 -0500

    typo

--- f2/src/HowDidItStartPage.js
+++ f2/src/HowDidItStartPage.js
@@ -39,7 +39,7 @@ export const HowDidItStartPage = () => {
             <HowDidItStartForm
               onSubmit={data => {
                 dispatch({ type: 'saveFormData', data: { howdiditstart: data } })
-                history.push(doneForms ? '/privacyconsent' : '/whatwasaffcted')
+                history.push(doneForms ? '/privacyconsent' : '/whatwasaffected')
               }}
             />
           </Stack>
