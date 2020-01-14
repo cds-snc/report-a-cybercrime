493144f23635d6f2c935d81fb4e1606e30eae736
amazingphilippe <amazingphilippe@gmail.com>
2020-01-14 11:36:39 -0500

    Phase banner tag (#1079)
    * Fix main content flex
    
    * testing out a grid layout. Test in IE
    
    * Working on options for grid layout
    
    * simple grid logic applied to main container
    
    * linted
    
    * working starter for summary page. Needs to be updated along with new forms
    
    * split confirmationSummary page into summary directory
    
    * fixes tag width on small screens
    
    * nowrap layout and purple color

--- f2/src/components/phase-banner/index.js
+++ f2/src/components/phase-banner/index.js
@@ -5,11 +5,17 @@ import { Flex, Tag } from '@chakra-ui/core'
 import { Text } from '../text'
 
 export const PhaseBanner = props => (
-  <Flex align="center" p={4} bg="green.50">
-    <Tag variantColor="green">{props.phase}</Tag>
-    <Text color="green.900" ml={4}>
-      {props.children}
-    </Text>
+  <Flex align="center" p={4} bg="purple.50">
+    <Tag
+      rounded="none"
+      variantColor="purple"
+      variant="solid"
+      minW="inherit"
+      mr={4}
+    >
+      {props.phase}
+    </Tag>
+    <Text color="green.900">{props.children}</Text>
   </Flex>
 )
 
