593b988ffff806605d0b6e2db14d342ba2f08217
Steve Astels <sastels@gmail.com>
2020-01-15 21:02:05 -0500

    use children in warning banner

--- f2/src/components/warning-banner/index.js
+++ f2/src/components/warning-banner/index.js
@@ -5,23 +5,22 @@ import PropTypes from 'prop-types'
 import { Alert, AlertIcon, AlertDescription } from '@chakra-ui/core'
 
 export const WarningBanner = props => {
-  const { status, message, bg } = props
+  const { status, bg } = props
 
   return (
     <Alert p={4} status={status} bg={`${bg}.400`} fontFamily="body">
       <AlertIcon mt={0} color="black" />
-      <AlertDescription color="black">{message}</AlertDescription>
+      <AlertDescription color="black">{props.children}</AlertDescription>
     </Alert>
   )
 }
 
 WarningBanner.propTypes = {
   bg: PropTypes.string,
+  status: PropTypes.string,
 }
 
 WarningBanner.defaultProps = {
   bg: 'yellow',
   status: 'warning',
-  message:
-    "Warning! This is a prototype. It won't actually submit your report to the RCMP.",
 }
