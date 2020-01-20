40db3f874b321d4c2a7d191405f3b5d1d6ec0733
Philippe Caron <amazingphilippe@gmail.com>
2020-01-20 14:03:40 -0500

    added a passing test

---
+++ f2/src/components/Field/__tests__/Field.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Field } from '../'
+import { render, cleanup } from '@testing-library/react'
+import { Form } from 'react-final-form'
+
+describe('<Field/>', () => {
+  afterEach(cleanup)
+
+  it('renders and has matching htmlFor/id/name', () => {
+    const submitMock = jest.fn()
+    const { getByLabelText, getByText } = render(
+      <Form
+        initialValues={location}
+        onSubmit={submitMock}
+        render={({ handleSubmit }) => (
+          <Field id="test" label="foo" helpText="help" />
+        )}
+      />,
+    )
+
+    const input = getByLabelText('foo')
+    const label = getByText('foo')
+
+    //Make sure all 3 of these are matching
+    expect(input.id).toEqual('test')
+    expect(input.name).toEqual('test')
+    expect(label.htmlFor).toEqual('test')
+  })
+})
