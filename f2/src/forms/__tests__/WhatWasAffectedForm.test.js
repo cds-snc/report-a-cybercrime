496a1fbe70cba70cb89bab6db62d28875481bcec
amazingphilippe <amazingphilippe@gmail.com>
2020-02-04 11:04:49 -0500

    stacked mobile buttons (#1259)
    * making the cancel button a button, stacking buttons on mobile
    
    * lint and tests
    
    * updated form tests to account for multiple buttons on the same page
    
    * switched the tests a little bit. Now lints

--- f2/src/forms/__tests__/WhatWasAffectedForm.test.js
+++ f2/src/forms/__tests__/WhatWasAffectedForm.test.js
@@ -21,7 +21,7 @@ describe('<WhatWasAffectedForm />', () => {
   it('does not call the onSubmit function when the form is submitted', async () => {
     const submitMock = jest.fn()
 
-    const { getByRole } = render(
+    const { getByText } = render(
       <MemoryRouter initialEntries={['/']}>
         <ThemeProvider theme={canada}>
           <I18nProvider i18n={i18n}>
@@ -33,7 +33,9 @@ describe('<WhatWasAffectedForm />', () => {
       </MemoryRouter>,
     )
 
-    const nextButton = getByRole('button')
+    // we want to grab whatever is in the submit button as text, pass it to getByText
+    const context = document.querySelector('[type="submit"]').textContent
+    const nextButton = getByText(context)
 
     clickOn(nextButton)
     await wait(0) // Wait for promises to resolve
@@ -44,7 +46,7 @@ describe('<WhatWasAffectedForm />', () => {
   it('calls the onSubmit function when at least one option is checked and the form is submitted', async () => {
     const submitMock = jest.fn()
 
-    const { getByRole, getByLabelText } = render(
+    const { getByText, getByLabelText } = render(
       <MemoryRouter initialEntries={['/']}>
         <ThemeProvider theme={canada}>
           <I18nProvider i18n={i18n}>
@@ -57,7 +59,9 @@ describe('<WhatWasAffectedForm />', () => {
     )
 
     const checkbox = getByLabelText('whatWasAffectedForm.other')
-    const nextButton = getByRole('button')
+    // we want to grab whatever is in the submit button as text, pass it to getByText
+    const context = document.querySelector('[type="submit"]').textContent
+    const nextButton = getByText(context)
 
     clickOn(checkbox) // Wait for promises to resolve
     await wait(0)
