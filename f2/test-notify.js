05a18dbfc05a460aa95a6031faab30d7500c2c7c
Steve Astels <sastels@gmail.com>
2020-02-04 16:04:51 -0500

    added in norm's code

--- f2/test-notify.js
+++ f2/test-notify.js
@@ -1,12 +1,82 @@
 'use strict'
 
 const nodemailer = require('nodemailer')
+const ldap = require('ldapjs')
+const fs = require('fs')
+const exec = require('child_process').exec
+
 require('dotenv').config()
 
 const mailHost = process.env.MAIL_HOST
 const mailUser = process.env.MAIL_USER
 const mailPass = process.env.MAIL_PASS
+const mailLdap = process.env.MAIL_LDAP
+const mailUid = process.env.MAIL_UID
 const mailTo = process.env.MAIL_TO
+
+function encryptMessage(uid, message) {
+  var opts = {
+    filter: '(uid=' + uid + ')',
+    scope: 'sub',
+    attributes: ['cn', 'userCertificate;binary'],
+  }
+
+  const ldapClient = ldap.createClient({
+    url: mailLdap,
+  })
+
+  ldapClient.search('ou=People,ou=rcmp-grc,o=gc,c=ca', opts, function(
+    err,
+    res,
+  ) {
+    res.on('searchEntry', function(entry) {
+      // console.log('entry: ' + JSON.stringify(entry.object))
+      /* We can probably do this without any files by using /dev/stdin or something???? */
+      fs.writeFile(
+        uid + '.cer',
+        '-----BEGIN CERTIFICATE-----\r\n' +
+          entry.object['userCertificate;binary'].replace(/(.{64})/g, '$1\r\n') +
+          '-----END CERTIFICATE-----',
+        function(err) {
+          if (err) throw err
+          // console.log('Certificate Saved!')
+          fs.writeFile(uid + '.txt', message, function(err) {
+            if (err) throw err
+            // console.log('Message Saved!')
+            exec(
+              'openssl smime -encrypt -out /dev/stdout ' +
+                '-in ' +
+                uid +
+                '.txt ' +
+                uid +
+                '.cer',
+              { cwd: process.cwd() },
+              function(error, stdout, stderr) {
+                const attachment = stdout
+
+                console.log(attachment)
+
+                // console.log('UID:' + uid)
+                // console.log('Message:' + message)
+                // console.log('STDOUT:' + stdout)
+                // console.log('STDERR:' + stderr)
+              },
+            )
+          })
+        },
+      )
+    })
+    res.on('searchReference', function(referral) {
+      console.log('referral: ' + referral.uris.join())
+    })
+    res.on('error', function(err) {
+      console.error('error: ' + err.message)
+    })
+    res.on('end', function(result) {
+      console.log('status: ' + result.status)
+    })
+  })
+}
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main() {
@@ -118,24 +188,11 @@ ZeWp52b+0fpRkMTmMa3GGMQ4Pg==`,
     ],
   }
 
-  //   const message = {
-  //     from: '"Fred Foo 👻" <foo@guqvkp.m.notification.alpha.cdssandbox.xyz>', // sender address
-  //     to: mailTo, // list of receivers
-  //     subject: 'node notify test', // Subject line
-  //     attachments: [
-  //       {
-  //         raw: `Content-Type: text/plain;
-  //   Content-Disposition: attachment
-
-  //   Attached text file 333
-  // `,
-  //       },
-  //     ],
-  //   }
-
   let info = await transporter.sendMail(message)
 
   console.log(`Message sent to ${mailTo}: ${info.messageId}`)
 }
 
-main().catch(console.error)
+// main().catch(console.error)
+
+encryptMessage(mailUid, 'hi Norm!')
