41aad1602575944e2e2fcdb1f7517df3d44642fc
amazingphilippe <amazingphilippe@gmail.com>
2020-02-05 16:47:10 -0500

    Cancellation page (#1289)
    * cancel pages + form data deletion
    
    * renamed a test file

---
+++ f2/src/__tests__/ConfirmCancelPage.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { i18n } from '@lingui/core'
+import { MemoryRouter } from 'react-router-dom'
+import { render, cleanup } from '@testing-library/react'
+import { ThemeProvider } from 'emotion-theming'
+import { I18nProvider } from '@lingui/react'
+import en from '../locales/en.json'
+import canada from '../theme/canada'
+import { StateProvider, initialState, reducer } from '../utils/state'
+import { ConfirmCancelPage } from '../ConfirmCancelPage'
+
+i18n.load('en', { en })
+i18n.activate('en')
+
+describe('<ConfirmCancelPage />', () => {
+  afterEach(cleanup)
+
+  it('renders', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <ThemeProvider theme={canada}>
+          <I18nProvider i18n={i18n}>
+            <StateProvider initialState={initialState} reducer={reducer}>
+              <ConfirmCancelPage />
+            </StateProvider>
+          </I18nProvider>
+        </ThemeProvider>
+      </MemoryRouter>,
+    )
+  })
+})
