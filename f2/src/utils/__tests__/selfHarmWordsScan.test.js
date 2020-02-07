916158b2153cc54e37900322a35677055352c19b
Steve Astels <sastels@gmail.com>
2020-02-07 12:22:45 -0500

    catch words with accents

--- f2/src/utils/__tests__/selfHarmWordsScan.test.js
+++ f2/src/utils/__tests__/selfHarmWordsScan.test.js
@@ -6,11 +6,19 @@ describe('selfHarmWordsScan', () => {
     expect(selfHarmWordsScan(data)).toEqual([])
   })
 
-  it('returns 2 words when they are in data when no words match', () => {
+  it('returns 2 words when they are nested in data', () => {
     const data = {
       a: { b: 'hi' },
       aa: ['this', 'agile', 'thing'],
       cc: { ccc: { dd: 'hi', ee: 'scrum' } },
+    }
+    expect(selfHarmWordsScan(data)).toEqual(['agile', 'scrum'])
+  })
+
+  it('catches words with accents', () => {
+    const data = {
+      a: 'agilé',
+      aa: 'this sçrum',
     }
     expect(selfHarmWordsScan(data)).toEqual(['agile', 'scrum'])
   })
--- f2/src/utils/selfHarmWordsScan.js
+++ f2/src/utils/selfHarmWordsScan.js
@@ -2,8 +2,11 @@
 
 const selfHarmWords = 'agile, lean, mvp, scrum'.split(',').map(w => w.trim())
 
+// from https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+const removeAccents = s => s.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
 const selfHarmWordsScan = data => {
-  const json = JSON.stringify(data).toLowerCase()
+  const json = removeAccents(JSON.stringify(data).toLowerCase())
   const wordsUsed = selfHarmWords.filter(w => json.includes(w))
   return wordsUsed
 }
