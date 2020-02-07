4207a47cad37f23b8bed741495aebc81bc0ecc14
Steve Astels <sastels@gmail.com>
2020-02-07 15:07:21 -0500

    with hooks

--- f2/src/components/layout/index.js
+++ f2/src/components/layout/index.js
@@ -1,16 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Container } from '../container'
-
-class ScrollToTop extends React.Component {
-  componentDidMount() {
-    window.scrollTo(0, 0)
-  }
-
-  render() {
-    return <div />
-  }
-}
 
 export const Layout = ({ fluid, columns, ...props }) => {
   const col = {
@@ -20,23 +10,25 @@ export const Layout = ({ fluid, columns, ...props }) => {
     xl: columns.xl * 100 + '%',
   }
 
+  // scroll to the top of the page when this Layout renders
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  })
+
   return (
-    <React.Fragment>
-      <ScrollToTop />
-      <Container
-        {...(fluid
-          ? { w: '100%' }
-          : {
-              maxW: { sm: 540, md: 768, lg: 960, xl: 1200 },
-              mx: 'auto',
-              px: 4,
-              w: '100%',
-            })}
-        {...props}
-      >
-        <Container w={col}>{props.children}</Container>
-      </Container>
-    </React.Fragment>
+    <Container
+      {...(fluid
+        ? { w: '100%' }
+        : {
+            maxW: { sm: 540, md: 768, lg: 960, xl: 1200 },
+            mx: 'auto',
+            px: 4,
+            w: '100%',
+          })}
+      {...props}
+    >
+      <Container w={col}>{props.children}</Container>
+    </Container>
   )
 }
 
