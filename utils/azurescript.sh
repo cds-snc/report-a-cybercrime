879d9ee5be8375a50e53f527a8a6a2b0fd5d93b7
dhcdn <51758091+davidheerema@users.noreply.github.com>
2020-02-13 11:37:15 -0500

    scanner image from ACR

--- utils/azurescript.sh
+++ utils/azurescript.sh
@@ -5,6 +5,7 @@ export RG_NAME=MpPCCDSCybercrimeRG
 
 export ACR_NAME=MpPCCDSCybercrimeacr
 export IMAGE_NAME=f2
+export VIRUS_SCANNER_IMAGE_NAME=clamav
 
 export DB_NAME=mppccdscybercrimecosdb
 
@@ -58,7 +59,7 @@ SP_APP_ID=$(az ad sp show --id http://$SERVICE_PRINCIPAL_NAME --query appId --ou
 # - Currently not using the alpine version because mk0x needs to rebuild from clamd v102.2 or 103 to pickup known azure bugfix.
 # - See https://bugzilla.clamav.net/show_bug.cgi?id=12469
 # To query the log analytics workspace for container logs query ContainerEvent_CL and ContainerInstanceLog_CL tables
-az container create --resource-group $RG_NAME --name $VIRUS_SCANNER_NAME --image mk0x/docker-clamav --dns-name-label $VIRUS_SCANNER_NAME --ports 3310 --log-analytics-workspace $(az monitor log-analytics workspace show --resource-group $LOG_RG --workspace-name $LOG_ANALYTICS --query customerId --output tsv) --log-analytics-workspace-key $(az monitor log-analytics workspace get-shared-keys --resource-group $LOG_RG --workspace-name $LOG_ANALYTICS --query primarySharedKey --output tsv)
+az container create --resource-group $RG_NAME --name $VIRUS_SCANNER_NAME --image ${ACR_NAME}.azurecr.io/${VIRUS_SCANNER_IMAGE_NAME}:latest --dns-name-label $VIRUS_SCANNER_NAME --ports 3310 --registry-login-server ${ACR_NAME}.azurecr.io --registry-password $SP_PASSWD --registry-username $SP_APP_ID --log-analytics-workspace $(az monitor log-analytics workspace show --resource-group $LOG_RG --workspace-name $LOG_ANALYTICS --query customerId --output tsv) --log-analytics-workspace-key $(az monitor log-analytics workspace get-shared-keys --resource-group $LOG_RG --workspace-name $LOG_ANALYTICS --query primarySharedKey --output tsv)
 
 ## Create Content Moderator - Azure Cognitive Services
 az cognitiveservices account create --name $COGNITIVE_NAME --resource-group $RG_NAME --kind ContentModerator --sku F0 --location canadacentral --yes
