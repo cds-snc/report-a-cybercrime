6efc00375ed3313263c0d2cc891b9058a7b53eb9
Anik Brazeau <anik.brazeau@cds-snc.ca>
2020-02-13 21:29:57 -0500

    remove unused P tag

--- f2/src/forms/LocationInfoForm.js
+++ f2/src/forms/LocationInfoForm.js
@@ -9,7 +9,6 @@ import { Stack, FormControl, VisuallyHidden } from '@chakra-ui/core'
 import { FormHelperText } from '../components/FormHelperText'
 import { FormLabel } from '../components/FormLabel'
 import { useStateValue } from '../utils/state'
-import { P } from '../components/paragraph'
 
 export const LocationInfoForm = props => {
   const [data] = useStateValue()
