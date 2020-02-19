9e5c7eb2e32da2318a457bebc15989ae7d1e769e
Khalid El Aggan <khalid.elaggan@cds-snc.ca>
2020-02-18 21:37:05 -0500

    Generate e2e test report for RCMP QC team demo

--- f2/cypress/integration/Consent/consent.js
+++ f2/cypress/integration/Consent/consent.js
@@ -3,21 +3,21 @@ Given('I open the report home page', () => {
     cy.visit(Cypress.env('production'))
 });
 
-  When('I click on create a report button', () => {
+When('I click on create a report button', () => {
     cy.contains('Report now').first().click({force: true});
-  });
+});
 
-  When('I read before you start instructions', () => {
+When('I read before you start instructions', () => {
     cy.contains('Start report').first().click({force: true});
-  });
+});
 
-  When('I check the consent checkbox', () => {
+When('I check the consent checkbox', () => {
     //cy.get('[type="checkbox"]').check({ force: true }).should('be.checked')
     //.check('privacyConsentInfoForm.yes')
     cy.get('form').find('[name="consentOptions"]').check({ force: true })
     cy.contains('Continue').first().click({force: true});
-  });
+});
 
-  Then('{string} should be shown', (content) => {
+Then('{string} should be shown', (content) => {
     cy.contains(content, {timeout:10000}).should('be.visible')
-  });
+});
