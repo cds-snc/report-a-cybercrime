a8789df9ac4c69de15bf3f671446149ebb6790b8
Steve Astels <sastels@gmail.com>
2020-02-24 15:03:34 -0500

    fix lint

--- f2/src/utils/__tests__/saveBlob.test.js
+++ f2/src/utils/__tests__/saveBlob.test.js
@@ -3,7 +3,7 @@ import { saveBlob } from '../saveBlob'
 describe('saveBlob', () => {
   it('logs an error if no blob storage keys, but does not crash', () => {
     console.warn = jest.fn()
-    const id = saveBlob({})
+    saveBlob({})
     expect(console.warn).toHaveBeenCalled()
   })
 })
