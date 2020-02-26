1de211080e476aafafc5e455be3b647c05cd842c
Philippe Caron <amazingphilippe@gmail.com>
2020-02-26 11:41:51 -0500

    updated tests

--- f2/src/components/next-and-cancel-buttons/_tests_/next-and-cancel-buttons.test.js
+++ f2/src/components/next-and-cancel-buttons/_tests_/next-and-cancel-buttons.test.js
@@ -7,6 +7,7 @@ import canada from '../../../theme/canada'
 import { MemoryRouter } from 'react-router-dom'
 import { NextAndCancelButtons } from '../'
 import en from '../../../locales/en.json'
+import { screen } from '@testing-library/dom'
 
 i18n.load('en', { en })
 i18n.activate('en')
@@ -15,7 +16,7 @@ describe('<NextAndCancelButtons />', () => {
   afterEach(cleanup)
 
   it('properly renders next button with cancel button beside', () => {
-    const { getAllByText } = render(
+    const { getAllByText, getAllByRole } = render(
       <MemoryRouter initialEntries={['/']}>
         <ThemeProvider theme={canada}>
           <I18nProvider i18n={i18n}>
@@ -24,17 +25,19 @@ describe('<NextAndCancelButtons />', () => {
         </ThemeProvider>
       </MemoryRouter>,
     )
+
+    //Expect to find 2 buttons
+    expect(getAllByRole('button')).toHaveLength(2)
+
+    const submitButton = getAllByRole('button')[0]
+    const cancelButton = getAllByRole('button')[1]
+
     //There is only one next and one cancel button
     expect(getAllByText(/Next/)).toHaveLength(1)
     expect(getAllByText(/button.cancelReport/)).toHaveLength(1)
 
-    //The cancel button returns to="/"
-    expect(document.querySelector('button[to]').getAttribute('to')).toBe(
-      '/confirmCancel',
-    )
-    //The submit button submits
-    expect(
-      document.querySelector('button[type="submit"]').getAttribute('type'),
-    ).toBe('submit')
+    //expect these properties to exist
+    expect(submitButton).toHaveProperty('type', 'submit')
+    expect(cancelButton).toHaveProperty('type', 'button')
   })
 })
