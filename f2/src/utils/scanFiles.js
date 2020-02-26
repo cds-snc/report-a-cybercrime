723dd502da476d1931d5a1d71853c3b6b035ae8a
Steve Astels <sastels@users.noreply.github.com>
2020-02-26 09:16:24 -0500

    Don't crash if blob storage is not configured (#1535)

--- f2/src/utils/scanFiles.js
+++ f2/src/utils/scanFiles.js
@@ -11,22 +11,26 @@ let serviceKey = process.env.CONTENT_MODERATOR_SERVICE_KEY
 if (!serviceKey) console.warn('WARNING: Azure content moderator not configured')
 
 async function scanFiles(data) {
-  var scanner = clamd.createScanner(process.env.CLAM_URL, 3310)
-  for (const file of Object.entries(data.evidence.files)) {
-    //scan file for virus
-    var readStream = fs.createReadStream(file[1].path)
-    //set timeout for 10000
-    await scanner
-      .scanStream(readStream, 10000)
-      .then(function(reply) {
-        file[1].malwareScanDetail = reply
-        file[1].malwareIsClean = clamd.isCleanReply(reply)
-      })
-      .catch(function(reply) {
-        file[1].malwareScanDetail = 'ERROR: Unable to perform virus scan'
-        file[1].malwareIsClean = false
-        console.error('Virus scan failed on ' + data.reportId)
-      })
+  try {
+    var scanner = clamd.createScanner(process.env.CLAM_URL, 3310)
+    for (const file of Object.entries(data.evidence.files)) {
+      //scan file for virus
+      var readStream = fs.createReadStream(file[1].path)
+      //set timeout for 10000
+      await scanner
+        .scanStream(readStream, 10000)
+        .then(function(reply) {
+          file[1].malwareScanDetail = reply
+          file[1].malwareIsClean = clamd.isCleanReply(reply)
+        })
+        .catch(function(reply) {
+          file[1].malwareScanDetail = 'ERROR: Unable to perform virus scan'
+          file[1].malwareIsClean = false
+          console.warn('Virus scan failed on ' + data.reportId)
+        })
+    }
+  } catch (error) {
+    console.warn('WARNING: File scanning failed')
   }
 }
 
