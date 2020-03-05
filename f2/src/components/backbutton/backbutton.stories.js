89fc5e2791a71420226885e5641aecb0a14b137e
Philippe Caron <amazingphilippe@gmail.com>
2020-03-05 16:25:02 -0500

    linted

--- f2/src/components/backbutton/backbutton.stories.js
+++ f2/src/components/backbutton/backbutton.stories.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { BackButton } from '.'
 import ThemeDecorator from '../../../.storybook/themeDecorator'
-import { withKnobs, text } from '@storybook/addon-knobs'
 
 //Doc Page
 export default {
   title: 'Components/Back Button',
-  decorators: [ThemeDecorator, withKnobs],
+  decorators: [ThemeDecorator],
   component: BackButton,
 }
 
