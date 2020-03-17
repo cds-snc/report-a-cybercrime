1dbd2bd26a7c60e787729cf8318182277567c622
Diane Liu <deyuan.liu@cds-snc.ca>
2020-03-16 22:31:21 -0400

    add test class for Anonymouspage.js and AnonymousInfoFormPage.js

---
+++ f2/src/forms/__tests__/AnonymousInfoForm.test.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import wait from 'waait'
+import { i18n } from '@lingui/core'
+import { render, fireEvent, cleanup } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { ThemeProvider } from 'emotion-theming'
+import { I18nProvider } from '@lingui/react'
+import en from '../../locales/en.json'
+import canada from '../../theme/canada'
+import { StateProvider, initialState, reducer } from '../../utils/state'
+import { AnonymousInfoForm } from '../AnonymousInfoForm'
+
+i18n.load('en', { en })
+i18n.activate('en')
+
+const clickOn = element => fireEvent.click(element)
+
+describe('<AnonymousInfoForm />', () => {
+  afterEach(cleanup)
+
+  it('calls the onSubmit function when the form is submitted', async () => {
+    const submitMock = jest.fn()
+
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <ThemeProvider theme={canada}>
+          <I18nProvider i18n={i18n}>
+            <StateProvider initialState={initialState} reducer={reducer}>
+              <AnonymousInfoForm onSubmit={submitMock} />
+            </StateProvider>
+          </I18nProvider>
+        </ThemeProvider>
+      </MemoryRouter>,
+    )
+
+    // find the next button so we can trigger a form submission
+    const nextButton = getByText(/nextButton/)
+
+    // Click the next button to trigger the form submission
+    clickOn(nextButton.parentElement)
+    await wait(0) // Wait for promises to resolve
+
+    expect(submitMock).toHaveBeenCalledTimes(1)
+  })
+})
