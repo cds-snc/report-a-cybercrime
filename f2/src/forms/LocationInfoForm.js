98a419c06829cf6e2b8539723ae38ac8dc62a248
Kevin Yang <kevinyang@Kevins-RCMP-MacBook-Pro.local>
2020-03-23 18:17:40 -0400

    move trans tag into lingui id block

--- f2/src/forms/LocationInfoForm.js
+++ f2/src/forms/LocationInfoForm.js
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+import React from 'react'
 import PropTypes from 'prop-types'
 import { jsx } from '@emotion/core'
 import { Trans } from '@lingui/macro'
@@ -50,77 +51,86 @@ export const LocationInfoForm = ({ onSubmit }) => {
   }
 
   return (
-    <Form
-      initialValues={location}
-      onSubmit={onSubmit}
-      validate={validate}
-      render={({
-        handleSubmit,
-        values,
-        errors,
-        submitFailed,
-        hasValidationErrors,
-      }) => (
-        <Stack as="form" onSubmit={handleSubmit} shouldWrapChildren spacing={6}>
-          {submitFailed && hasValidationErrors ? (
-            <ErrorSummary onSubmit={handleSubmit} errors={errors} />
-          ) : null}
-          <FormArrayControl
-            name="consentOptions"
-            errorMessage={<Trans id="locationInfoForm.warning" />}
-          ></FormArrayControl>
-          <Flex direction="row" align="center" wrap="wrap" mb={10}>
-            <P w="100%">
-              <Trans id="locationinfoPage.skipInfo" />
-            </P>
-            <Button
-              as={ReactRouterLink}
-              fontSize={{ base: 'lg', md: 'xl' }}
-              color="black"
-              variant="solid"
-              variantColor="gray"
-              bg="gray.400"
-              borderColor="gray.500"
-              to="/contactinfo"
-              textAlign="center"
-            >
-              <Trans id="locationinfoPage.skipButton" />
-              <Icon
-                focusable="false"
-                ml={2}
-                mr={-2}
-                name="chevron-right"
-                size="28px"
-              />
-            </Button>
-          </Flex>
-          <Field name="postalCode">
-            {props => (
-              <FormControl>
-                <FormLabel htmlFor="postalCode">
-                  <Trans id="locationinfoPage.postalCode" />
-                </FormLabel>
-                <FormHelperText>
-                  <Trans id="locationinfoPage.postalCodeExample">
-                    <VisuallyHidden as="span" />
-                  </Trans>
-                </FormHelperText>
-                <TextInput
-                  id="postalCode"
-                  name={props.input.name}
-                  value={props.input.value}
-                  onChange={props.input.onChange}
+    <React.Fragment>
+      {false ? ( // mark ids for lingui
+        <div>
+          <Trans id="locationInfoForm.warning" />
+        </div>
+      ) : null}
+      <Form
+        initialValues={location}
+        onSubmit={onSubmit}
+        validate={validate}
+        render={({
+          handleSubmit,
+          values,
+          errors,
+          submitFailed,
+          hasValidationErrors,
+        }) => (
+          <Stack
+            as="form"
+            onSubmit={handleSubmit}
+            shouldWrapChildren
+            spacing={6}
+          >
+            {submitFailed && hasValidationErrors ? (
+              <ErrorSummary onSubmit={handleSubmit} errors={errors} />
+            ) : null}
+            <FormArrayControl name="consentOptions"></FormArrayControl>
+            <Flex direction="row" align="center" wrap="wrap" mb={10}>
+              <P w="100%">
+                <Trans id="locationinfoPage.skipInfo" />
+              </P>
+              <Button
+                as={ReactRouterLink}
+                fontSize={{ base: 'lg', md: 'xl' }}
+                color="black"
+                variant="solid"
+                variantColor="gray"
+                bg="gray.400"
+                borderColor="gray.500"
+                to="/contactinfo"
+                textAlign="center"
+              >
+                <Trans id="locationinfoPage.skipButton" />
+                <Icon
+                  focusable="false"
+                  ml={2}
+                  mr={-2}
+                  name="chevron-right"
+                  size="28px"
                 />
-              </FormControl>
-            )}
-          </Field>
-          <NextAndCancelButtons
-            next={<Trans id="locationinfoPage.nextPage" />}
-            button={<Trans id="locationinfoPage.nextButton" />}
-          />
-        </Stack>
-      )}
-    />
+              </Button>
+            </Flex>
+            <Field name="postalCode">
+              {props => (
+                <FormControl>
+                  <FormLabel htmlFor="postalCode">
+                    <Trans id="locationinfoPage.postalCode" />
+                  </FormLabel>
+                  <FormHelperText>
+                    <Trans id="locationinfoPage.postalCodeExample">
+                      <VisuallyHidden as="span" />
+                    </Trans>
+                  </FormHelperText>
+                  <TextInput
+                    id="postalCode"
+                    name={props.input.name}
+                    value={props.input.value}
+                    onChange={props.input.onChange}
+                  />
+                </FormControl>
+              )}
+            </Field>
+            <NextAndCancelButtons
+              next={<Trans id="locationinfoPage.nextPage" />}
+              button={<Trans id="locationinfoPage.nextButton" />}
+            />
+          </Stack>
+        )}
+      />
+    </React.Fragment>
   )
 }
 
