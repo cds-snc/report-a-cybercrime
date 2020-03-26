f4cc348f78d9688618a94cb032f9834adcc7ad21
Philippe Caron <amazingphilippe@gmail.com>
2020-03-26 10:22:06 -0400

    linted

--- f2/src/components/button/Button.stories.js
+++ f2/src/components/button/Button.stories.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ThemeDecorator from '../../../.storybook/themeDecorator'
 
 import { Button } from '.'
-import { NextAndCancelButtons } from '../next-and-cancel-buttons'
 import { Stack } from '@chakra-ui/core'
 
 export default {
