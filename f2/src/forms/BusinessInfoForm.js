864f0411ca18af43e9c3ca0b8ae6ee01c1001c45
Kevin Yang <kevinyang@Kevins-RCMP-MacBook-Pro.local>
2020-04-07 16:20:11 -0400

    added all fields for business form

--- f2/src/forms/BusinessInfoForm.js
+++ f2/src/forms/BusinessInfoForm.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { jsx } from '@emotion/core'
 import { Trans } from '@lingui/macro'
-import { Form, Field } from 'react-final-form'
+import { Form } from 'react-final-form'
 import { NextAndCancelButtons } from '../components/next-and-cancel-buttons'
 import { TextArea } from '../components/text-area'
 import { Stack, FormControl, VisuallyHidden } from '@chakra-ui/core'
@@ -11,6 +11,9 @@ import { FormHelperText } from '../components/FormHelperText'
 import { FormLabel } from '../components/FormLabel'
 import { useStateValue } from '../utils/state'
 import { Input } from '../components/input'
+import { Field } from '../components/Field'
+import { FormArrayControl } from '../components/FormArrayControl'
+import { RadioAdapter } from '../components/radio'
 
 export const BusinessInfoForm = ({ onSubmit }) => {
   const [data] = useStateValue()
@@ -36,6 +39,36 @@ export const BusinessInfoForm = ({ onSubmit }) => {
               label={<Trans id="businessPage.nameOfBusiness" />}
               component={Input}
             />
+
+            <Field
+              name="industry"
+              label={<Trans id="businessPage.industry" />}
+              helperText={<Trans id="businessPage.industryExample" />}
+              component={Input}
+            />
+
+            <Field
+              name="role"
+              label={<Trans id="businessPage.role" />}
+              helperText={<Trans id="businessPage.roleExample" />}
+              component={Input}
+            />
+
+            <FormArrayControl
+              name="numberOfEmployee"
+              label={<Trans id="businessPage.numberOfEmployee" />}
+              helperText={<Trans id="businessPage.numberOfEmployeeExample" />}
+            >
+              <RadioAdapter name="check" value="value1">
+                1 to 99 employees
+              </RadioAdapter>
+              <RadioAdapter name="check" value="value2">
+                100 to 499 employees
+              </RadioAdapter>
+              <RadioAdapter name="check" value="value3">
+                500 employees or more
+              </RadioAdapter>
+            </FormArrayControl>
 
             <NextAndCancelButtons
               next={<Trans id="businessInfoPage.nextPage" />}
