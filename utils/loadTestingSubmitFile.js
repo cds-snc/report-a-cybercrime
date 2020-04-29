d44e3d09b1de41f5b054e3c99fca47b8c243f7e4
Steve Astels <sastels@users.noreply.github.com>
2020-04-29 11:22:32 -0400

    Load testing scripts (#1856)

---
+++ utils/loadTestingSubmitFile.js
@@ -0,0 +1,77 @@
+import { check, fail, sleep } from "k6";
+import http from "k6/http";
+
+const loadTestingUrl = `${__ENV.LOAD_TESTING_BASE_URL}`;
+if (loadTestingUrl === "undefined") {
+  fail("ERROR: Environment variable LOAD_TESTING_BASE_URL not defined");
+}
+
+const flattenedData = {
+  language: "",
+  prodVersion: "",
+  appVersion: "",
+  "consent.consentOptions": [],
+  "anonymous.anonymousOptions": ["anonymousPage.no"],
+  "howdiditstart.howDidTheyReachYou": [],
+  "howdiditstart.email": "",
+  "howdiditstart.phone": "",
+  "howdiditstart.online": "",
+  "howdiditstart.application": "",
+  "howdiditstart.others": "",
+  "howdiditstart.howManyTimes": "",
+  "howdiditstart.startDay": "",
+  "howdiditstart.startMonth": "",
+  "howdiditstart.startYear": "",
+  "whatWasAffected.affectedOptions": [],
+  "moneyLost.demandedMoney": "",
+  "moneyLost.moneyTaken": "",
+  "moneyLost.methodPayment": [],
+  "moneyLost.transactionDay": "",
+  "moneyLost.transactionMonth": "",
+  "moneyLost.transactionYear": "",
+  "moneyLost.methodOther": "",
+  "personalInformation.typeOfInfoReq": [],
+  "personalInformation.typeOfInfoObtained": [],
+  "personalInformation.infoReqOther": "",
+  "personalInformation.infoObtainedOther": "",
+  "devicesInfo.device": "",
+  "devicesInfo.account": "",
+  "businessInfo.nameOfBusiness": "",
+  "businessInfo.industry": "",
+  "businessInfo.role": "",
+  "businessInfo.numberOfEmployee": "",
+  "whatHappened.whatHappened": "",
+  "suspectClues.suspectClues1": "",
+  "suspectClues.suspectClues2": "",
+  "suspectClues.suspectClues3": "",
+  "evidence.files": [],
+  "evidence.fileDescriptions": [],
+  "location.postalCode": "",
+  "location.city": "",
+  "location.province": "",
+  "contactInfo.fullName": "Load Test",
+  "contactInfo.email": "",
+  "contactInfo.phone": "",
+};
+
+let preparedData = {};
+Object.entries(flattenedData).forEach(([key, value]) => {
+  preparedData[key] = JSON.stringify(value);
+});
+
+let binFile = open("bread.jpg", "b");
+
+export default function () {
+  // var data = {
+  //   field: "this is a field",
+  //   bread: http.file(binFile, "test.bin"),
+  // };
+  preparedData["bread.jpg"] = http.file(binFile, "bread.jpg");
+
+  var res = http.post(`${loadTestingUrl}/submit`, preparedData);
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
