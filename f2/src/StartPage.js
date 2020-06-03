067814d6e1e2eb973d0098e80d4adaadad46a99a
ngosset <ngosset@gmail.com>
2020-06-03 08:54:09 -0400

    Revert "John recaptcha (#1899)"
    This reverts commit 6d01fe44c556e84bb94d102a9b79b5639eb1adfc.

--- f2/src/StartPage.js
+++ f2/src/StartPage.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import { Route } from 'react-router-dom'
-import { GoogleReCaptcha } from 'react-google-recaptcha-v3'
 import PropTypes from 'prop-types'
 import { Trans } from '@lingui/macro'
 import { P } from './components/paragraph'
@@ -16,69 +15,64 @@ import { BackButton } from './components/backbutton'
 import { Page } from './components/Page'
 import { Well } from './components/Messages'
 
-export const StartPage = (props) => {
+export const StartPage = props => {
   const [state, dispatch] = useStateValue()
   if (state.doneForms) {
     dispatch({ type: 'saveDoneForms', data: false })
   }
 
   return (
-    <React.Fragment>
-      <GoogleReCaptcha
-        onVerify={(token) => console.log(`The token is ${token}`)}
-      />
-      <Route
-        render={({ history }) => (
-          <Page>
-            <Layout columns={{ base: 4 / 4, md: 6 / 8, lg: 7 / 12 }}>
-              <Stack spacing={10} shouldWrapChildren>
-                <BackButton />
+    <Route
+      render={({ history }) => (
+        <Page>
+          <Layout columns={{ base: 4 / 4, md: 6 / 8, lg: 7 / 12 }}>
+            <Stack spacing={10} shouldWrapChildren>
+              <BackButton />
 
-                <H1>
-                  <Trans id="startPage.title" />
-                </H1>
-                <Stack spacing={4}>
-                  <P>
-                    <Trans id="startPage.intro" />
-                  </P>
-                  <Ul>
-                    <Li>
-                      <Trans id="startPage.requirement1" />
-                    </Li>
-                    <Li>
-                      <Trans id="startPage.requirement2" />
-                    </Li>
-                    <Li>
-                      <Trans id="startPage.requirement3" />
-                    </Li>
-                    <Li>
-                      <Trans id="startPage.requirement4" />
-                    </Li>
-                  </Ul>
-                </Stack>
-                <Button
-                  onClick={() => {
-                    history.push('/privacyconsent')
-                  }}
-                >
-                  <Trans id="startPage.nextButton" />
-                  <Icon
-                    focusable="false"
-                    ml={2}
-                    mr={-2}
-                    name="chevron-right"
-                    size="28px"
-                  />
-                </Button>
-                <Well variantColor="blue">
-                  <Trans id="startPage.warning" />
-                </Well>
+              <H1>
+                <Trans id="startPage.title" />
+              </H1>
+              <Stack spacing={4}>
+                <P>
+                  <Trans id="startPage.intro" />
+                </P>
+                <Ul>
+                  <Li>
+                    <Trans id="startPage.requirement1" />
+                  </Li>
+                  <Li>
+                    <Trans id="startPage.requirement2" />
+                  </Li>
+                  <Li>
+                    <Trans id="startPage.requirement3" />
+                  </Li>
+                  <Li>
+                    <Trans id="startPage.requirement4" />
+                  </Li>
+                </Ul>
               </Stack>
-            </Layout>
-          </Page>
-        )}
-      />
-    </React.Fragment>
+              <Button
+                onClick={() => {
+                  history.push('/privacyconsent')
+                }}
+              >
+                <Trans id="startPage.nextButton" />
+                <Icon
+                  focusable="false"
+                  ml={2}
+                  mr={-2}
+                  name="chevron-right"
+                  size="28px"
+                />
+              </Button>
+              <Well variantColor="blue">
+                <Trans id="startPage.warning" />
+              </Well>
+            </Stack>
+          </Layout>
+        </Page>
+      )}
+    />
   )
 }
 
