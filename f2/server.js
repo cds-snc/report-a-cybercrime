03e6e005bf6bbc3cc8bf1d7a4b9daec3f828c22a
Justin <richardsonj1986@gmail.com>
2020-06-08 09:19:20 -0400

    Moved direct access check to checkIfAvailable

--- f2/server.js
+++ f2/server.js
@@ -8,7 +8,7 @@ const { unflatten } = require('flat')
 const { sanitize } = require('./src/utils/sanitize')
 const { encryptAndSend } = require('./src/utils/encryptedEmail')
 const { getCertsAndEmail } = require('./src/utils/ldap')
-const { isAvailable } = require('./src/utils/checkIfAvailable')
+const { isAvailable, requestAccess } = require('./src/utils/checkIfAvailable')
 const { getData } = require('./src/utils/getData')
 const { saveRecord } = require('./src/utils/saveRecord')
 const { getReportCount } = require('./src/utils/saveRecord')
@@ -126,20 +126,6 @@ const allowedOrigins = [
   'https://centreantifraude.ca',
 ]
 
-let availableData
-
-availableData = {
-  numberOfSubmissions: 0,
-  numberOfRequests: 0,
-  lastRequested: undefined,
-}
-const allowedReferrers = [
-  'antifraudcentre-centreantifraude.ca',
-  'centreantifraude-antifraudcentre.ca',
-  'antifraudcentre.ca',
-  'centreantifraude.ca',
-]
-
 getReportCount(availableData)
 setTimeout(() => console.log({ availableData }), 1000)
 
@@ -173,7 +159,6 @@ app.get('/', async function (req, res, next) {
 
   // Default to false. This represents if a user entered a valid TOTP code
   var isTotpValid = false
-  var validReferer = false
 
   // If the user passed in a TOTP query parm (/?totp=) and the correct
   // env var is set, then verify the code.
@@ -186,32 +171,13 @@ app.get('/', async function (req, res, next) {
     })
   }
 
-  if (process.env.CHECK_REFERER) {
-    var referer = req.headers.referer
-    validReferer = referer ? allowedReferrers.includes(new URL(referer).host.toLowerCase()) : referer
+  if ( isTotpValid ) next()
+    
+  var referer = req.headers.referer
+
+  if ( requestAccess(referer) ) {
+    next()
   } else {
-    validReferer = true
-  }
-
-  var maxSubmissions = availableData.numberOfSubmissions >= process.env.SUBMISSIONS_PER_DAY
-
-  var availabilityCheck = {
-    "SUBMISSIONS_PER_DAY":process.env.SUBMISSIONS_PER_DAY,
-    "NUMBER_OF_SUBMISSIONS": availableData.numberOfSubmissions,
-    "MAX_SUBMISSIONS": maxSubmissions,
-    "CHECK_REFERER": process.env.CHECK_REFERER,
-    "VALID_REFERER": validReferer,
-    "TOTP_SECRET": process.env.TOTP_SECRET,
-    "TOTP_VALID": isTotpValid
-  }
-
-  logger.info({
-    message: 'Availability Check',
-    availabilityCheck: availabilityCheck
-  })
-
-  // If user had a TOTP code, bypass the submissions_per_day restriction
-  if ( (maxSubmissions || !validReferer) && !isTotpValid ) {
     logger.info({
       message: 'Redirecting to CAFC'
     })
@@ -220,14 +186,6 @@ app.get('/', async function (req, res, next) {
         ? 'https://www.antifraudcentre-centreantifraude.ca/report-signalez-fra.htm'
         : 'https://www.antifraudcentre-centreantifraude.ca/report-signalez-eng.htm',
     )
-  } else {
-    availableData.numberOfRequests += 1
-    availableData.lastRequested = new Date()
-    logger.info({
-      message: 'New Request',
-      availableData: availableData,
-    })
-    next()
   }
 })
 app
--- f2/src/utils/checkIfAvailable.js
+++ f2/src/utils/checkIfAvailable.js
@@ -1,33 +1,102 @@
 require('dotenv').config()
+const logger = require('./src/utils/winstonLogger')
 
 const submissionsPerDay = process.env.SUBMISSIONS_PER_DAY
 const secondsBetweenRequests = process.env.SECONDS_BETWEEN_REQUESTS
+const checkReferer = process.env.CHECK_REFERER
+
+let availableData
+
+availableData = {
+  numberOfSubmissions: 0,
+  numberOfRequests: 0,
+  lastRequested: undefined,
+}
+const allowedReferrers = [
+  'antifraudcentre-centreantifraude.ca',
+  'centreantifraude-antifraudcentre.ca',
+  'antifraudcentre.ca',
+  'centreantifraude.ca',
+]
 
 if (!submissionsPerDay || !secondsBetweenRequests) {
-  console.error('ERROR: SUBMISSIONS_PER_DAY or SECONDS_BETWEEN_REQUESTS not configured.  The Server will constantly report unavailable as a result.')
+  logger.error({message:
+    `SUBMISSIONS_PER_DAY or SECONDS_BETWEEN_REQUESTS not configured.
+     The Server will constantly report unavailable as a result.`,
+     SUBMISSIONS_PER_DAY: `${submissionsPerDay}`,
+     SECONDS_BETWEEN_REQUESTS: `${secondsBetweenRequests}`})
 }
-// availableData is { numberOfSubmissions, numberOfRequests, lastRequested }
+
 const isAvailable = (availableData) => {
   try {
     const currentTime = new Date()
     const lastRequested = availableData.lastRequested
 
-    if (!lastRequested) return true
-    else if (
-      currentTime.getDate() !== lastRequested.getDate() ||
-      currentTime.getMonth() !== lastRequested.getMonth()
-    ) {
-      availableData.numberOfSubmissions = 0
-      return true
-    }
     if (!submissionsPerDay || !secondsBetweenRequests) return false
     if (availableData.numberOfSubmissions >= submissionsPerDay) return false
-    if (currentTime - availableData.lastRequested > secondsBetweenRequests * 1000) return true
+
+    if (!lastRequested) return true
+    else {
+
+      const timeSinceLastRequest = currentTime - lastRequested
+
+      if (currentTime.setHours(0,0,0,0) !== lastRequested.setHours(0,0,0,0)) {
+        availableData.numberOfSubmissions = 0
+        return true
+      }
+      if ( timeSinceLastRequest > (secondsBetweenRequests * 1000) ) return true
+    }
     
   } catch (error) {
-    console.error(`ERROR in isAvailable: ${error}`)
+    logger.error({message:`ERROR in isAvailable: ${error}`})
   }
   return false
 }
 
-module.exports = { isAvailable }
+const requestAccess = (referer) => {
+  try {
+
+    var validReferer = false
+
+    if (checkReferer) {
+      validReferer = referer ? allowedReferrers.includes(new URL(referer).host.toLowerCase()) : referer
+    } else {
+      validReferer = true
+    }
+  
+    var maxSubmissions = availableData.numberOfSubmissions >= submissionsPerDay
+  
+    var availabilityCheck = {
+      "SUBMISSIONS_PER_DAY": submissionsPerDay,
+      "NUMBER_OF_SUBMISSIONS": availableData.numberOfSubmissions,
+      "MAX_SUBMISSIONS": maxSubmissions,
+      "CHECK_REFERER": checkReferer,
+      "REFERER": referer,
+      "VALID_REFERER": validReferer
+    }
+  
+    logger.info({
+      message: 'Availability Check',
+      availabilityCheck: availabilityCheck
+    })
+  
+    if (maxSubmissions || !validReferer) {
+      return false
+    } else {
+      availableData.numberOfRequests += 1
+      availableData.lastRequested = new Date()
+      logger.info({
+        message: 'New Request',
+        availableData: availableData,
+      })
+      return true
+    }
+  }
+  catch(error) {
+    logger.error({message:`ERROR in requestAccess: ${error}`})
+    return false
+  }
+
+}
+
+module.exports = { isAvailable, requestAccess }
