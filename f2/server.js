9f9b09c7c0ef70165dfa2fb15e96fe09b4f8929a
ngosset <norman.gosset@cds-snc.ca>
2020-06-09 15:09:16 -0400

    Remove CSP for now until tested more (#2014)

--- f2/server.js
+++ f2/server.js
@@ -63,21 +63,6 @@ setTimeout(() => {
 const app = express()
 app
   .use(helmet())
-  .use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: [
-          "'self'",
-          "'unsafe-inline'",
-          'www.google-analytics.com',
-          'www.googletagmanager.com',
-        ],
-        styleSrc: ["'self'", "'unsafe-inline'", 'fonts.googleapis.com'],
-        fontSrc: ["'self'", 'fonts.gstatic.com'],
-      },
-    }),
-  )
   .use(helmet.referrerPolicy({ policy: 'same-origin' }))
   .use(
     helmet.featurePolicy({
