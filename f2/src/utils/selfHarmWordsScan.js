c3a5dd4885c6350416a4995f02a08a5b3289b563
justinr86 <richardsonj1986@gmail.com>
2020-06-17 14:26:56 -0400

    Added natural language processing, refactored code

--- f2/src/utils/selfHarmWordsScan.js
+++ f2/src/utils/selfHarmWordsScan.js
@@ -10,67 +10,85 @@ const selfHarmWords = selfHarmString
 console.info(`Self harm word list: ${selfHarmWords}`)
 
 natural.PorterStemmer.attach()
+natural.PorterStemmerFr.attach()
 
 const selfHarmWordsScan = (data) => {
-  const jsonString = unidecode(JSON.stringify(data).toLowerCase())
-  const formTokens = jsonString.tokenizeAndStem()
+  try {
+    const jsonString = unidecode(JSON.stringify(data).toLowerCase())
+    const json = JSON.parse(jsonString)
+    let keyWordMatch = []
 
-  //Create object
-  const json = JSON.parse(jsonString)
-  console.log('')
-  console.log('')
-  for (var key in json) {
-    if (typeof json[key] === 'object') {
-      //Get Strings from object
-      scanObject(json[key])
-    }
-    if (typeof json[key] === 'string') {
-      //Analyze String
-      scanString(json[key])
-    }
-  }
-
-  console.log(`${JSON.stringify(data, null, 2)}`)
-  console.log('')
-
-  scanString(formTokens)
-
-  const wordsUsed = selfHarmWords.filter((w) => {
-    const regEx = new RegExp('\\b' + w + '\\b')
-    return regEx.test(jsonString)
-  })
-  return wordsUsed
-}
-
-const scanString = (str) => {
-  const modifiedStr = str
-    .toString()
-    .replace(/\r?\n|\r/g, ' ') //Remove newline characters
-    .replace(/[^\w\s]|_/g, ' ') //Remove special characters
-    .replace(/\s+/g, ' ') //Remove any extra sapaces
-    .split(' ') //Break String into array of words(Strings)
-
-  console.log('')
-  console.log('')
-  console.log(`${modifiedStr}`)
-  console.log('')
-  console.log('')
-}
-
-const scanObject = (obj) => {
-  for (var key in obj) {
-    if (typeof obj[key] === 'object') {
-      var subObj = obj[key]
-      for (var subKey in subObj) {
-        if (typeof subObj[subKey] === 'string') {
-          scanString(subObj[subKey])
-        }
+    for (let key in json) {
+      if (typeof json[key] === 'object') {
+        keyWordMatch = keyWordMatch.concat(scanObject(json[key]))
+      }
+      if (typeof json[key] === 'string') {
+        keyWordMatch = keyWordMatch.concat(scanString(json[key]))
       }
     }
-    if (typeof obj[key] === 'string') {
-      scanString(obj[key])
-    }
+
+    return keyWordMatch
+  } catch (err) {
+    console.error(`Error scanning form for self-harm key words
+    ${err}`)
+    return []
   }
+}
+
+//Scan String for key words. Tokenize and stem to identify root words.
+const scanString = (str) => {
+  try {
+    let modifiedStr = str
+      .replace(/\r?\n|\r/g, ' ') //Remove newline characters
+      .replace(/[^\w\s']|_/g, ' ') //Remove special characters
+      .replace(/\s+/g, ' ') //Remove any extra sapaces
+
+    const formTokens = modifiedStr.tokenizeAndStem()
+
+    const modifiedTokens = formTokens.toString().replace(/,/g, ' ')
+
+    modifiedStr = modifiedStr + modifiedTokens
+
+    const wordsUsed = selfHarmWords.filter((w) => {
+      const regEx = new RegExp('\\b' + w + '\\b')
+      return regEx.test(modifiedStr)
+    })
+
+    return wordsUsed
+  } catch (err) {
+    console.error(`Error parsing String for self-harm key words: ${str}
+    ${err}`)
+    return []
+  }
+}
+
+//Scan object for String values. This assumes one level of nested objects.
+const scanObject = (obj) => {
+  let objStrings = []
+
+  try {
+    for (let key in obj) {
+      if (typeof obj[key] === 'object') {
+        let subObj = obj[key]
+        for (let subKey in subObj) {
+          if (typeof subObj[subKey] === 'string') {
+            let test = scanString(subObj[subKey])
+            objStrings = objStrings.concat(test)
+          }
+        }
+      }
+      if (typeof obj[key] === 'string') {
+        let test = scanString(obj[key])
+        objStrings = objStrings.concat(test)
+      }
+    }
+  } catch (err) {
+    console.error(`Error parsing Object for self-harm key words: ${obj}
+    ${err}`)
+    return []
+  }
+
+  return objStrings
 }
 
 module.exports = { selfHarmWordsScan }
