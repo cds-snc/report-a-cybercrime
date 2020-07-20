91c35599a92fa2f777a146f874c948d9be3064a8
Diane Liu <deyuan.liu@cds-snc.ca>
2020-07-20 14:22:18 -0400

    deleted unwanted line

--- f2/src/components/FormLabel/__tests__/FormLabel.test.js
+++ f2/src/components/FormLabel/__tests__/FormLabel.test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { cleanup } from '@testing-library/react'
 import { FormLabel } from '../'
 import ReactDOM from 'react-dom'
-// import { mount } from 'enzyme'
 
 afterEach(cleanup)
 it('renders without crashing', () => {
