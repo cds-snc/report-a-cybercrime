624752a5f058eb9644421484b0fd39ccd8703a7c
Diane Liu <deyuan.liu@cds-snc.ca>
2020-07-21 10:27:45 -0400

    add test component name in the code

--- f2/src/components/FormLabel/__tests__/FormLabel.test.js
+++ f2/src/components/FormLabel/__tests__/FormLabel.test.js
@@ -3,8 +3,11 @@ import { cleanup } from '@testing-library/react'
 import { FormLabel } from '../'
 import ReactDOM from 'react-dom'
 
-afterEach(cleanup)
-it('renders without crashing', () => {
-  const FormArrayControl = document.createElement('FormArrayControl')
-  ReactDOM.render(<FormLabel />, FormArrayControl)
+describe('<FormLabel />', () => {
+  afterEach(cleanup)
+
+  it('renders without crashing', () => {
+    const FormArrayControl = document.createElement('FormArrayControl')
+    ReactDOM.render(<FormLabel />, FormArrayControl)
+  })
 })
