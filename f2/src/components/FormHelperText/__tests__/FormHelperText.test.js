24442b371a236058089b2bc432bb2df26c229417
Diane Liu <deyuan.liu@cds-snc.ca>
2020-07-21 10:24:37 -0400

    add test component name in the code

--- f2/src/components/FormHelperText/__tests__/FormHelperText.test.js
+++ f2/src/components/FormHelperText/__tests__/FormHelperText.test.js
@@ -3,9 +3,11 @@ import { cleanup } from '@testing-library/react'
 import { FormHelperText } from '../'
 import ReactDOM from 'react-dom'
 
-afterEach(cleanup)
+describe('<FormHelperText />', () => {
+  afterEach(cleanup)
 
-it('renders without crashing', () => {
-  const FormArrayControl = document.createElement('FormArrayControl')
-  ReactDOM.render(<FormHelperText />, FormArrayControl)
+  it('renders without crashing', () => {
+    const FormArrayControl = document.createElement('FormArrayControl')
+    ReactDOM.render(<FormHelperText />, FormArrayControl)
+  })
 })
