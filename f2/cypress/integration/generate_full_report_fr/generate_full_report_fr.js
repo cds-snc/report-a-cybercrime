1056a4407fd00f1c00c848b40657aed55f223995
justinr86 <richardsonj1986@gmail.com>
2020-07-22 11:48:47 -0400

    Test commit

--- f2/cypress/integration/generate_full_report_fr/generate_full_report_fr.js
+++ f2/cypress/integration/generate_full_report_fr/generate_full_report_fr.js
@@ -1,7 +1,7 @@
 import { After, When, And, Then } from 'cypress-cucumber-preprocessor/steps'
 // Hooks for repeated commands/rules
 After(() => {
-  cy.reportA11y()
+  cy.reportA11y();
 })
 
 Given('I open the report home page', () => {
