681a14c0ccf2461d3e0e017b8b9f66c475c5cfc5
Diane Liu <deyuan.liu@cds-snc.ca>
2020-07-22 09:41:59 -0400

    add one more test

--- f2/src/components/FormHelperText/__tests__/FormHelperText.test.js
+++ f2/src/components/FormHelperText/__tests__/FormHelperText.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { cleanup } from '@testing-library/react'
 import { FormHelperText } from '../'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'emotion-theming'
+import canada from '../../../theme/canada'
+import { render, cleanup } from '@testing-library/react'
 
 describe('<FormHelperText />', () => {
   afterEach(cleanup)
@@ -9,5 +11,16 @@ describe('<FormHelperText />', () => {
   it('renders without crashing', () => {
     const FormArrayControl = document.createElement('FormArrayControl')
     ReactDOM.render(<FormHelperText />, FormArrayControl)
+  })
+
+  it('properly renders child components', () => {
+    const { getAllByText } = render(
+      <ThemeProvider theme={canada}>
+        <FormHelperText>foo</FormHelperText>
+      </ThemeProvider>,
+    )
+
+    const test = getAllByText(/foo/)
+    expect(test).toHaveLength(1)
   })
 })
