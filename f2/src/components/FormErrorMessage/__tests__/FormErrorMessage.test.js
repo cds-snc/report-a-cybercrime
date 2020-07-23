a62f57307cc49c0320e8f16b5f7b5322b172eb18
Diane Liu <deyuan.liu@cds-snc.ca>
2020-07-23 15:07:10 -0400

    add test file for formErrorMessage

--- f2/src/components/FormErrorMessage/__tests__/FormErrorMessage.test.js
+++ f2/src/components/FormErrorMessage/__tests__/FormErrorMessage.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { cleanup } from '@testing-library/react'
 import { FormErrorMessage } from '../'
 import ReactDOM from 'react-dom'
+import { render, cleanup } from '@testing-library/react'
+import { ThemeProvider } from 'emotion-theming'
+import canada from '../../../theme/canada'
 
 describe('<FormErrorMessage />', () => {
   afterEach(cleanup)
@@ -9,5 +11,14 @@ describe('<FormErrorMessage />', () => {
   it('renders without crashing', () => {
     const FormArrayControl = document.createElement('FormArrayControl')
     ReactDOM.render(<FormErrorMessage />, FormArrayControl)
+  })
+  it('properly renders child components', () => {
+    const { getAllByText } = render(
+      <ThemeProvider theme={canada}>
+        <FormErrorMessage>foo</FormErrorMessage>
+      </ThemeProvider>,
+    )
+    const test = getAllByText(/foo/)
+    expect(test).toHaveLength(1)
   })
 })
