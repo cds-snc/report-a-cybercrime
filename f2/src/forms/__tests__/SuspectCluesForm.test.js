6a0d919a08a0ecaf6075cbe58abe98ea38359bf0
kevinyang06 <58781425+kevinyang06@users.noreply.github.com>
2020-07-28 10:44:57 -0400

    Missing .test.js for the following forms (#2132)
    * suspect forms test file
    
    * comfirmationForm test file
    
    * evidenceInfoForm.test.js
    
    Co-authored-by: Kevin Yang <kevinyang@Kevins-RCMP-MacBook-Pro.local>

---
+++ f2/src/forms/__tests__/SuspectCluesForm.test.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import wait from 'waait'
+import { i18n } from '@lingui/core'
+import { render, fireEvent, cleanup } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { ThemeProvider } from 'emotion-theming'
+import { I18nProvider } from '@lingui/react'
+import { SuspectCluesForm } from '../SuspectCluesForm'
+import en from '../../locales/en.json'
+import canada from '../../theme/canada'
+import { StateProvider, initialState, reducer } from '../../utils/state'
+
+i18n.load('en', { en })
+i18n.activate('en')
+
+const clickOn = (element) => fireEvent.click(element)
+
+describe('<SuspectCluesForm />', () => {
+  afterEach(cleanup)
+
+  it('calls the onSubmit function when the form is submitted', async () => {
+    const submitMock = jest.fn()
+
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <ThemeProvider theme={canada}>
+          <I18nProvider i18n={i18n}>
+            <StateProvider initialState={initialState} reducer={reducer}>
+              <SuspectCluesForm onSubmit={submitMock} />
+            </StateProvider>
+          </I18nProvider>
+        </ThemeProvider>
+      </MemoryRouter>,
+    )
+
+    // find the next button so we can trigger a form submission
+    const nextButton = getByText(/nextButton/)
+
+    // Click the next button to trigger the form submission
+    clickOn(nextButton.parentElement)
+    await wait(0) // Wait for promises to resolve
+
+    // We expect that sequence of events to have caused our onSubmit mock to get
+    // exectuted.
+    expect(submitMock).toHaveBeenCalledTimes(1)
+  })
+})
