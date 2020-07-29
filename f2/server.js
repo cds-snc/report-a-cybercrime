f1740a7e6e5757d7ec481b7ad9dc10ee15bd5ef1
ying-chen-cds <ying.chen.cds@gmail.com>
2020-07-29 10:36:38 -0400

    clean up

--- f2/server.js
+++ f2/server.js
@@ -230,7 +230,6 @@ app
         logger.error('ERROR', err)
         throw err
       }
-      //submitFeedback(sanitize(JSON.stringify(fields.json)))
       submitFeedback(fields.json)
     })
     res.send('thanks')
