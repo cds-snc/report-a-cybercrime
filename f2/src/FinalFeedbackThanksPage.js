732b283c48c8ae5864ab97220f87b29c5b89c5db
DianeLiu2019 <51336707+DianeLiu2019@users.noreply.github.com>
2020-07-31 14:08:27 -0400

    change final feedback thanks  page as figma (#2134)
    * change final feedback thanks  page as figma
    
    * redirect user to finalfeedbackthanks page instead of Thankyoupage after
    finish feedback form
    
    * direct user to finalfeedbackthanks page instead of thankyou page

--- f2/src/FinalFeedbackThanksPage.js
+++ f2/src/FinalFeedbackThanksPage.js
@@ -4,10 +4,16 @@ import { Trans } from '@lingui/macro'
 import { H1 } from './components/header'
 import { TrackPageViews } from './TrackPageViews'
 import { Layout } from './components/layout'
-import { Stack } from '@chakra-ui/core'
+import { Stack, Box, Icon } from '@chakra-ui/core'
 import { Alert } from './components/Messages'
+import { Route } from 'react-router-dom'
+import { Link } from './components/link'
+import { useStateValue } from './utils/state'
+import { MidFeedbackForm } from './forms/MidFeedbackForm'
+import { submitToServer } from './utils/submitToServer'
 
 export const FinalFeedbackThanksPage = () => {
+  const [dispatch] = useStateValue()
   return (
     <Layout>
       <TrackPageViews />
@@ -18,6 +24,36 @@ export const FinalFeedbackThanksPage = () => {
         <Alert status="success" withIcon>
           <Trans id="thankYouPage.safelyCloseWindow" />
         </Alert>
+
+        <Box mb="auto">
+          <Route
+            render={({ history }) => (
+              <Link
+                onClick={() => {
+                  dispatch({
+                    type: 'deleteFormData',
+                  })
+                }}
+                to="/"
+              >
+                <Trans id="thankYouPage.createNewReport" />
+              </Link>
+            )}
+          />
+          <Icon
+            color="blue"
+            focusable="false"
+            ml={2}
+            mr={-2}
+            name="chevron-right"
+            size="24px"
+          />
+        </Box>
+        <MidFeedbackForm
+          onSubmit={(data) => {
+            submitToServer(data)
+          }}
+        />
       </Stack>
     </Layout>
   )
