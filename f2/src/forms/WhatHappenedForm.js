cdb5a78a2fe3eb78ed2d795028288f57b2393637
ying-chen-cds <ying.chen.cds@gmail.com>
2020-08-20 15:03:41 -0400

    refactor whatHappenedFrom

--- f2/src/forms/WhatHappenedForm.js
+++ f2/src/forms/WhatHappenedForm.js
@@ -2,23 +2,14 @@
 import PropTypes from 'prop-types'
 import { jsx } from '@emotion/core'
 import { Trans } from '@lingui/macro'
-import { Form } from 'react-final-form'
-import { Field } from '../components/Field'
 import { TextArea } from '../components/text-area'
-import { NextAndCancelButtons } from '../components/next-and-cancel-buttons'
-import { Stack } from '@chakra-ui/core'
 import { useStateValue } from '../utils/state'
-import { clientFieldsAreValid } from '../utils/clientFieldsAreValid'
 import { formDefaults } from './defaultValues'
-import { FormHelperText } from '../components/FormHelperText'
-import { FormLabel } from '../components/FormLabel'
+import { Form, Container, Row } from 'react-bootstrap'
+import { Formik, Field } from 'formik'
+import { NextCancelButtons } from '../components/formik/button'
 
 export const WhatHappenedForm = (props) => {
-  /*const localOnSubmit = (data) => {
-    if (clientFieldsAreValid(data, formDefaults.whatHappened))
-      props.onSubmit(data)
-  }*/
-
   const [data] = useStateValue()
   const whatHappened = {
     ...formDefaults.whatHappened,
@@ -28,14 +19,14 @@ export const WhatHappenedForm = (props) => {
 
   let formLabel = <Trans id="whatHappenedPage.summary" />
   let formHelpText = <Trans id="whatHappenedPage.hint" />
-  let nextButton = <Trans id="whatHappenedPage.nextPage" />
+  let nextPage = <Trans id="whatHappenedPage.nextPage" />
   let formHelpText2
 
   if (fyiForm) {
     formLabel = <Trans id="whatHappenedPage.fyi.summary" />
     formHelpText = <Trans id="whatHappenedPage.fyi.hint" />
     formHelpText2 = <Trans id="whatHappenedPage.fyi.hint2" />
-    nextButton = <Trans id="fyiForm.nextPage2" />
+    nextPage = <Trans id="fyiForm.nextPage2" />
   }
 
   return (
@@ -53,65 +44,38 @@ export const WhatHappenedForm = (props) => {
                 {formLabel}
               </Row>
               <Row className="form-helper-text">
-                <Trans id="howDidTheyReachYou.reminder" />
+                {formHelpText}
+                {formHelpText2 ? (
+                <p>
+                  <br />
+                  {formHelpText2}
+                </p>
+                ) : null}
               </Row>
-              <ErrorMessage name="howDidTheyReachYou" component={Error} />
             </Row>
             <Row className="form-section">
               <Field
-                name="anonymousOptions"
-                label={<Trans id="anonymousPage.yes" />}
-                component={CheckBox}
-                value="anonymousPage.yes"
+                name="whatHappened"
+                component={TextArea}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                type="checkbox"
-                id="checkbox-anonymous-option"
+                h="300px"
+                marginTop="-0.5rem"
+                id="textarea-whatHappened"
               >
-                <Warning>
-                  <Trans id="anonymousPage.warning" />
-                </Warning>
               </Field>
             </Row>
             <Row>
               <NextCancelButtons
-                submit={<Trans id="anonymousPage.nextButton" />}
+                submit={<Trans id="whatHappenedPage.nextButton" />}
                 cancel={<Trans id="button.cancelReport" />}
-                label={<Trans id="anonymousPage.nextPage" />}
+                label={nextPage}
               />
             </Row>
           </Container>
         </Form>
       )}
     </Formik>
-    <Form
-      initialValues={whatHappened}
-      onSubmit={localOnSubmit}
-      render={({ handleSubmit }) => (
-        <Stack as="form" onSubmit={handleSubmit} shouldWrapChildren>
-          <FormLabel htmlFor="whatHappened">{formLabel}</FormLabel>
-          <FormHelperText htmlFor="whatHappened">
-            {formHelpText}
-            {formHelpText2 ? (
-              <p>
-                <br />
-                {formHelpText2}
-              </p>
-            ) : null}
-          </FormHelperText>
-          <Field
-            name="whatHappened"
-            component={TextArea}
-            h="300px"
-            marginTop="-1.5rem"
-          />
-          <NextAndCancelButtons
-            next={nextButton}
-            button={<Trans id="whatHappenedPage.nextButton" />}
-          />
-        </Stack>
-      )}
-    />
   )
 }
 
