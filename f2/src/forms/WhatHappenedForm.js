55aaa53b3fcb5bb40c73602eb458df234cafdf3c
ying-chen-cds <ying.chen.cds@gmail.com>
2020-08-26 13:36:57 -0400

    use new textArea compoment

--- f2/src/forms/WhatHappenedForm.js
+++ f2/src/forms/WhatHappenedForm.js
@@ -59,7 +59,7 @@ export const WhatHappenedForm = (props) => {
                 component={TextArea}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                rows="10"
+                rows="12"
                 id="textarea-whatHappened"              >
               </Field>
             </Row>
