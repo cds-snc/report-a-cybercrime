e35bbfe0a935104108fac62badee3c4811be729c
David Deng(RCMP) <deng2018@yahoo.com>
2020-08-27 09:58:10 -0400

    record 60 percent work

--- f2/src/forms/ContactInfoForm.js
+++ f2/src/forms/ContactInfoForm.js
@@ -1,24 +1,32 @@
 /** @jsx jsx */
 import React from 'react'
 import addrs from 'email-addresses'
-import PropTypes from 'prop-types'
+//import PropTypes from 'prop-types'
 import { jsx } from '@emotion/core'
 import { Trans } from '@lingui/macro'
-import { Form } from 'react-final-form'
-import { NextAndCancelButtons } from '../components/next-and-cancel-buttons'
-import { Stack } from '@chakra-ui/core'
+//import { Form } from 'react-final-form'
+//import { NextAndCancelButtons } from '../components/next-and-cancel-buttons'
+//import { Stack } from '@chakra-ui/core'
 import { useStateValue } from '../utils/state'
-import { ErrorSummary } from '../components/ErrorSummary'
+//import { ErrorSummary } from '../components/ErrorSummary'
 import { Input } from '../components/input'
-import { Field } from '../components/Field'
-import { clientFieldsAreValid } from '../utils/clientFieldsAreValid'
-import { formatPhoneNumber } from '../utils/formatPhoneNumber'
-import { formDefaults } from './defaultValues'
+//import { Field } from '../components/Field'
+//import { clientFieldsAreValid } from '../utils/clientFieldsAreValid'
+//import { formatPhoneNumber } from '../utils/formatPhoneNumber'
+//import { formDefaults } from './defaultValues'
 import { containsData } from '../utils/containsData'
 import { Flex, Icon } from '@chakra-ui/core'
 import { P } from '../components/paragraph'
 import { Button } from '../components/button'
 import { Link as ReactRouterLink } from 'react-router-dom'
+
+import { Form, Container, Row } from 'react-bootstrap'
+import { Formik, FieldArray, Field, ErrorMessage } from 'formik'
+import { CheckBox } from '../components/formik/checkbox'
+import { TextArea } from '../components/formik/textArea'
+import { NextCancelButtons } from '../components/formik/button'
+import { Error, Info } from '../components/formik/alert'
+import { ContactInfoFormSchema } from './ContactInfoFormSchema'
 
 export const validate = (values) => {
   const errors = {}
@@ -58,22 +66,105 @@ const fyiValidate = (values) => {
 }
 
 export const ContactInfoForm = (props) => {
-  const localOnSubmit = (data) => {
-    if (clientFieldsAreValid(data, formDefaults.contactInfo))
-      props.onSubmit({ ...data, phone: formatPhoneNumber(data.phone) })
-  }
+  // const localOnSubmit = (data) => {
+  //   if (clientFieldsAreValid(data, formDefaults.contactInfo))
+  //     props.onSubmit({ ...data, phone: formatPhoneNumber(data.phone) })
+  // }
 
   const [data] = useStateValue()
   const contactInfo = {
-    ...formDefaults.contactInfo,
+    //...formDefaults.contactInfo,
     ...data.formData.contactInfo,
   }
+
+  const contactFormOptions = [
+    {
+      name: 'fullName',
+      label: <Trans id="contactinfoPage.fullName" />,
+      helpText: <Trans id="contactinfoForm.fullName.warning" />,
+    },
+    {
+      name: 'email',
+      label: <Trans id="contactinfoPage.emailAddress" />,
+      helpText: <Trans id="contactinfoForm.email.warning" />,
+    },
+    {
+      name: 'phone',
+      label: <Trans id="contactinfoPage.phoneNumber" />,
+      helpText: <Trans id="contactinfoForm.phone.warning" />,
+    },
+  ]
+
+  //onst formOptions = [
 
   const { fyiForm } = data.formData
 
   return (
     <React.Fragment>
-      {false ? ( // mark ids for lingui
+      <Formik
+        initialValues={contactInfo}
+        validationSchema={ContactInfoFormSchema()}
+        onSubmit={(values) => {
+          // contactFormOptions.forEach((question) => {
+          //   if (!values.ContactInfoForm.includes(question.name)) {
+          //     values[question.name] = ''
+          //   }
+          // })
+          props.onSubmit(values)
+        }}
+      >
+        {({ handleSubmit, handleChange, handleBlur }) => (
+          <Form onSubmit={handleSubmit}>
+            <Container>
+              <Row className="form-section">
+                name="suspectClues" className="form-section" render=
+                {() => {
+                  //formOptions.map((question) => {
+                  return (
+                    <React.Fragment>
+                      <Field
+                        name={'fullName'} //question.name
+                        label={<Trans id="contactinfoPage.fullName" />} // question.questionLabel
+                        helpText={
+                          <Trans id="contactinfoForm.fullName.warning" />
+                        } // question.helpText
+                        component={CheckBox}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                      />
+                    </React.Fragment>
+                  )
+                  //})
+                }}
+              </Row>
+              <Info>
+                <Trans id="howDidItStartPage.tip" />
+              </Info>
+
+              {/* <Field
+                  name="fullName"
+                  label={<Trans id="contactinfoPage.fullName" />}
+                  errorMessage={<Trans id="contactinfoForm.fullName.warning" />}
+                  component={Input}
+                  required={!fyiForm}
+                />
+                </Field> */}
+
+              <Row>
+                <NextCancelButtons
+                  submit={<Trans id="contactinfoPage.nextButton" />} //howDidItStartPage.nextButton
+                  cancel={<Trans id="button.cancelReport" />}
+                  label={<Trans id="contactinfoPage.nextInfo" />}
+                />
+              </Row>
+
+              {/* //</Form>/next={<Trans id="contactinfoPage.nextInfo" />}
+              //</Formik>button={<Trans id="contactinfoPage.nextButton" />} */}
+            </Container>
+          </Form>
+        )}
+      </Formik>
+      {/* {false ? ( // mark ids for lingui
         <div>
           <Trans id="contactInfoPage.victimDetail" />
         </div>
@@ -161,12 +252,11 @@ export const ContactInfoForm = (props) => {
               button={<Trans id="contactinfoPage.nextButton" />}
             />
           </Stack>
-        )}
-      />
+        )*/}
     </React.Fragment>
   )
 }
 
-ContactInfoForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-}
+// ContactInfoForm.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// }
