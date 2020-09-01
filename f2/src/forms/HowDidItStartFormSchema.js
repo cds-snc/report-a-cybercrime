792968c29cca2d492d841285f1f22ee2a7cca5f1
Kevin Yang <kevinyang@Kevins-RCMP-MacBook-Pro.local>
2020-09-01 15:21:10 -0400

    remove mandatory validation

--- f2/src/forms/HowDidItStartFormSchema.js
+++ f2/src/forms/HowDidItStartFormSchema.js
@@ -4,9 +4,6 @@ import { Trans } from '@lingui/macro'
 import { yupSchema } from '../utils/yupSchema'
 
 const howDidItStartFormSchema = Yup.object().shape({
-  howDidTheyReachYou: Yup.array().required(
-    <Trans id="howDidTheyReachYou.error" />,
-  ),
   email: yupSchema().emailSchema,
   phone: yupSchema().phoneSchema,
   online: Yup.string(),
