103b0bbe208d25624431edd848cab184e1433386
ying-chen-cds <ying.chen.cds@gmail.com>
2020-09-09 09:59:47 -0400

    clean up

--- f2/src/components/formik/textArea/index.js
+++ f2/src/components/formik/textArea/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Form } from 'react-bootstrap'
 
 export const TextArea = ({ field, form, ...props }) => {
-  const largeTextArea = props.size === 'large'
   return (
     <Form.Group>
       <Form.Label>{props.label}</Form.Label>
@@ -10,9 +9,7 @@ export const TextArea = ({ field, form, ...props }) => {
       <Form.Control
         {...field}
         as="textarea"
-        className={
-          'open-text-entry' + (largeTextArea ? ' large-text-area' : '')
-        }
+        className={'open-text-entry'}
         {...props}
       />
     </Form.Group>
