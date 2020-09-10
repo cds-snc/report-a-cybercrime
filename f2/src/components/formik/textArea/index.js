d47312952e7a7d6afb6f0e2a8583e2d4437a6964
ying-chen-cds <ying.chen.cds@gmail.com>
2020-09-10 13:12:10 -0400

    update

--- f2/src/components/formik/textArea/index.js
+++ f2/src/components/formik/textArea/index.js
@@ -10,7 +10,7 @@ export const TextArea = ({ field, form, ...props }) => {
         {...field}
         as="textarea"
         className={'open-text-entry'}
-        {...props}
+        rows={props.rows}
       />
     </Form.Group>
   )
