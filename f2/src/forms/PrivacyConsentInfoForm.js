cf6f3598f637693240e0aea6e63fdbd31d7bbd63
David Deng(RCMP) <deng2018@yahoo.com>
2020-09-21 17:58:39 -0400

    Add i18n handling, ErrorSummary, Ensure FYI

--- f2/src/forms/PrivacyConsentInfoForm.js
+++ f2/src/forms/PrivacyConsentInfoForm.js
@@ -3,13 +3,25 @@ import { Trans } from '@lingui/macro'
 import { useStateValue } from '../utils/state'
 import { A } from '../components/formik/link'
 import { Form, Container, Row } from 'react-bootstrap'
-import { Formik, Field, ErrorMessage } from 'formik'
+import { Formik, Field } from 'formik'
 import { CheckBox } from '../components/formik/checkbox'
 import { NextCancelButtons } from '../components/formik/button'
-import { Error } from '../components/formik/alert'
+import { useLingui } from '@lingui/react'
+import { ErrorSummary } from '../components/formik/alert'
 import { PrivacyConsentInfoFormSchema } from './PrivacyConsentInfoFormSchema'
 
+const createErrorSummary = (errors) => {
+  const errorSummary = {}
+  if (errors.consentOptions) {
+    errorSummary['consentOptions'] = {
+      message: <Trans id="privacyConsentInfoForm.hasValidationErrors" />,
+    }
+  }
+  return errorSummary
+}
+
 export const PrivacyConsentInfoForm = (props) => {
+  const { i18n } = useLingui()
   const [data] = useStateValue()
   const whetherConsent = {
     ...data.formData.consent,
@@ -23,12 +35,16 @@ export const PrivacyConsentInfoForm = (props) => {
       }}
       validationSchema={PrivacyConsentInfoFormSchema()}
     >
-      {({ handleSubmit, handleChange, handleBlur }) => (
+      {({ handleSubmit, handleChange, handleBlur, errors, submitCount }) => (
         <Form onSubmit={handleSubmit}>
           <Container>
-            <Row className="form-question">
-              <ErrorMessage name="consentOptions" component={Error} />
-            </Row>
+            {Object.keys(errors).length > 0 && (
+              <ErrorSummary
+                errors={createErrorSummary(errors)}
+                submissions={submitCount}
+                title={<Trans id="default.hasValidationErrors" />}
+              />
+            )}
             <Row className="form-section">
               <Field
                 name="consentOptions"
@@ -37,7 +53,7 @@ export const PrivacyConsentInfoForm = (props) => {
                     <A
                       color="#0000ff"
                       target="_blank"
-                      href={'/privacystatement?lang=en'}
+                      href={'/privacystatement?lang=' + i18n.locale} //{'/privacystatement?lang=en'}
                     />
                   </Trans>
                 }
