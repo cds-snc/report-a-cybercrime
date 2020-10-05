588ffb021ae2cb957ab59abe820f73c88f975513
justinr86 <richardsonj1986@gmail.com>
2020-10-05 12:08:58 -0400

    Temp commit

--- f2/src/components/formik/checkbox/index.js
+++ f2/src/components/formik/checkbox/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Form, Row } from 'react-bootstrap'
+import { FormRow } from '../row'
 
 export const CheckBox = ({ field, form, ...props }) => {
   return (
-    <Row className="form-row">
+    <FormRow>
       <Form.Check id={props.id} type="checkbox" custom>
         <Form.Check.Input type="checkbox" {...field} value={props.value} />
         <Form.Check.Label>{props.label}</Form.Check.Label>
@@ -12,6 +13,6 @@ export const CheckBox = ({ field, form, ...props }) => {
       {field.checked && props.children && (
         <div className="conditional-field">{props.children}</div>
       )}
-    </Row>
+    </FormRow>
   )
 }
