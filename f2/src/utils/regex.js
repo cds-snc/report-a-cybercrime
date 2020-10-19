8f8570b7766c8bdc2f463a0a1de559b2575bc11b
David Deng(RCMP) <deng2018@yahoo.com>
2020-10-19 10:53:55 -0400

    Implement strict Max 15 valid digits for phone number and internaitonal number

--- f2/src/utils/regex.js
+++ f2/src/utils/regex.js
@@ -2,6 +2,6 @@ export const regexDef = () => {
   return {
     phoneRegExp: /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/,
     postalCodeRegex: /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?\d[ABCEGHJKLMNPRSTVWXYZ]\d$/i,
-    isValidInternationalPhonenumber: /\d{7,15}$/,
+    isValidInternationalPhonenumber: /^\d{7,15}$/,
   }
 }
