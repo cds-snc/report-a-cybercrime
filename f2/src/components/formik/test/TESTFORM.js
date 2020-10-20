dd55c764a9d17e578fa46ae6678d61fc4923f267
justinr86 <62246403+justinr86@users.noreply.github.com>
2020-10-20 09:17:46 -0400

    Update checkbox radio (#2346)
    * Update package-lock.json
    
    * Temp commit
    
    * Temp
    
    * Temp
    
    * Temp
    
    * Temp
    
    * Temp
    
    * Refactored Checkbox and Radio
    
    * Refactored checkbox and radio
    
    * Refactored checkbox and radio
    
    * Temp commit
    
    * Refactor checkbox and radio button
    
    * Clean up code
    
    * Fix test fdrm
    
    Co-authored-by: DianeLiu2019 <51336707+DianeLiu2019@users.noreply.github.com>

--- f2/src/components/formik/test/TESTFORM.js
+++ f2/src/components/formik/test/TESTFORM.js
@@ -1,21 +1,62 @@
 import React from 'react'
 import { useStateValue } from '../../../utils/state'
 import { Form, Container, Row } from 'react-bootstrap'
-import { Formik } from 'formik'
-import {
-  DefaultButton,
-  SubmitButton,
-  CancelButton,
-  UploadButton,
-  SkipButton,
-  FeedbackButton,
-  NextCancelButtons,
-  MidFormFeedbackButton,
-} from '../button'
+import { Formik, FieldArray, Field } from 'formik'
+import { CheckBoxRadio } from '../checkboxRadio'
+//import { Radio } from '../radio'
+import { TextArea } from '../textArea'
+import { Input } from '../input'
+
 
 export const TestForm = (props) => {
   const [data] = useStateValue()
 
+  const questionData = [
+    {
+      name: 'Radio 1',
+      label: 'Phone',
+      value: 'phone',
+      id: 'selection-phone',
+      followUp: 'What was the phone number?',
+      helpText: 'Please enter the phone number including area code',
+    },
+    {
+      name: 'Radio 2',
+      label: 'Online',
+      value: 'online',
+      id: 'selection-online',
+      followUp: 'What was the email address?',
+    },
+    {
+      name: 'Radio 3',
+      label: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+      value: 'other',
+      id: 'selection-other',
+      followUp: 'Additional details',
+      helpText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    },
+    {
+      name: 'Radio 4',
+      label: 'Other1',
+      value: 'other1',
+      id: 'selection-other1',
+      followUp: 'Additional details',
+    },
+    {
+      name: 'Radio 4',
+      label: 'Other2',
+      value: 'other2',
+      id: 'selection-other2',
+      followUp: 'Additional details',
+      helpText: 'Please provide any information regarding the situation',
+    },
+  ]
   return (
     <React.Fragment>
       <Formik
@@ -30,22 +71,70 @@ export const TestForm = (props) => {
           <Form onSubmit={handleSubmit}>
             <Container>
               <Row>
-                <DefaultButton label="Test Button" />
-                <SubmitButton label="Submit" />
-                <FeedbackButton label="Feedback" />
-                <CancelButton label="Cancel" />
-                <UploadButton label="Upload" />
-                <SkipButton label="Skip" />
-              </Row>
-              <Row>
-                <NextCancelButtons
-                  label="Next Page: Test"
-                  submit="Continue"
-                  cancel="Cancel"
+                <FieldArray
+                  name="howDidTheyReachYou"
+                  render={() =>
+                    questionData.map((question) => {
+                      return (
+                        <React.Fragment key={question.name + 'checkbox'}>
+                          <Field
+                            name="howDidTheyReachYou"
+                            label={question.label}
+                            component={CheckBoxRadio}
+                            value={question.value}
+                            helpText={question.helpText}
+                            type="checkbox"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            id={'checkbox-' + question.id}
+                          >
+                            <Field
+                              name={question.name + 'CheckGroup'}
+                              label={question.followUp}
+                              helpText={question.helpText}
+                              component={Input}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                            />
+                          </Field>
+                        </React.Fragment>
+                      )
+                    })
+                  }
                 />
               </Row>
               <Row>
-                <MidFormFeedbackButton label="Is something wrong on this page?" />
+                <FieldArray
+                  name="radioGroup"
+                  render={() =>
+                    questionData.map((question) => {
+                      return (
+                        <React.Fragment key={question.name + 'radio'}>
+                          <Field
+                            name="radioGroup"
+                            label={question.label}
+                            component={CheckBoxRadio}
+                            value={question.value}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            helpText={question.helpText}
+                            type="radio"
+                            id={question.id}
+                          >
+                            <Field
+                              name={question.name + 'RadioGroup'}
+                              label={question.followUp}
+                              helpText={question.helpText}
+                              component={TextArea}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                            />
+                          </Field>
+                        </React.Fragment>
+                      )
+                    })
+                  }
+                />
               </Row>
             </Container>
           </Form>
