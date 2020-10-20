fc9e52af9f38314ac0b3218a6cab1c28284a824b
ying-chen-cds <68352206+ying-chen-cds@users.noreply.github.com>
2020-10-20 14:36:10 -0400

    Pdf Report  (#2353)
    * Pdf Report Download
    
    * update
    
    * French translation
    
    * update
    
    * update
    
    * update
    
    Co-authored-by: ngosset <norman.gosset@cds-snc.ca>

---
+++ f2/src/pdf/AnonymousView.js
@@ -0,0 +1,20 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { Text, View, Image } from '@react-pdf/renderer'
+import { pdfStyles } from './pdfStyles'
+import line from '../images/line.png'
+
+export const AnonymousView = (props) => {
+  const lang = props.lang
+  return (
+    <View style={pdfStyles.section}>
+      <Text style={pdfStyles.title}>
+        {lang['confirmationPage.anonymous.title']}
+      </Text>
+      <Text style={pdfStyles.sectionContent}>
+        {lang['confirmationPage.anonymous.nag']}
+      </Text>
+      <Image style={pdfStyles.sectionSeparator} src={line} />
+    </View>
+  )
+}
