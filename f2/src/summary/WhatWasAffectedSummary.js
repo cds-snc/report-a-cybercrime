a6c692dde30168de6d57cc0a100ae341ad220e0d
ying-chen-cds <68352206+ying-chen-cds@users.noreply.github.com>
2020-10-29 23:31:33 -0400

    Reformat confirmation page and pdf report (#2442)
    * Reformat confirmation page and pdf report
    
    * update

--- f2/src/summary/WhatWasAffectedSummary.js
+++ f2/src/summary/WhatWasAffectedSummary.js
@@ -11,6 +11,7 @@ import { H2 } from '../components/header'
 import { useLingui } from '@lingui/react'
 import { Text } from '../components/text'
 import { formatList } from '../utils/formatList'
+import { DescriptionListItem } from '../components/DescriptionListItem'
 
 export const WhatWasAffectedSummary = (props) => {
   const { i18n } = useLingui()
@@ -37,6 +38,7 @@ export const WhatWasAffectedSummary = (props) => {
         <div>
           {/*: mark the proper ids for lingui */}
           <Trans id="confirmationPage.ImpactTitle.edit" />
+          <Trans id="confirmationPage.whatWasAffected.format" />
         </div>
       ) : null}
       <Stack
@@ -57,13 +59,10 @@ export const WhatWasAffectedSummary = (props) => {
         </Flex>
         {containsData(impact) ? (
           <Stack as="dl" spacing={4}>
-            <Text>
-              <Trans id="confirmationPage.whatWasAffected.format" />
-              &nbsp;
-              <Text as="span" textTransform="lowercase">
-                {summaryLine}
-              </Text>
-            </Text>
+            <DescriptionListItem
+              descriptionTitle="confirmationPage.whatWasAffected.format"
+              description={summaryLine}
+            />
           </Stack>
         ) : (
           <Text>
