12d9529b24755af1034c5073dd0cf0e823afc24a
justinr86 <62246403+justinr86@users.noreply.github.com>
2020-10-30 13:15:56 -0400

    Update DOM on Language Change (#2428)
    * Update package-lock.json
    
    * Update title on language change
    
    * Update html lang on language change
    
    * Undo catalog change
    
    * Undo catalog change
    
    * Use React hook to update DOM instead.

--- f2/src/LocaleSwitcher.js
+++ f2/src/LocaleSwitcher.js
@@ -1,17 +1,18 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
+import { useEffect } from 'react'
 import { useLingui } from '@lingui/react'
 import { locales, activate } from './i18n.config'
 import { Box } from '@chakra-ui/core'
 import { A } from './components/link'
 
-const Toggler = props => {
+const Toggler = (props) => {
   const { locale } = props
   return (
     <A
       key={locale}
       padding={0}
-      onClick={e => {
+      onClick={(e) => {
         e.preventDefault()
         activate(locale)
       }}
@@ -25,6 +26,14 @@ const Toggler = props => {
 
 export function LocaleSwitcher() {
   const { i18n } = useLingui()
+
+  useEffect(() => {
+    const title = i18n.locale === 'fr' ? 'signaler une fraude' : 'Report a scam'
+
+    document.title = title
+    document.documentElement.setAttribute('lang', i18n.locale)
+  })
+
   return (
     <Box>
       {i18n.locale === 'en' && <Toggler locale="fr" />}
